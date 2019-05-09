c529df690ef4c8bdc808a2c43c5bb40dabaa6adf
Mohammad Bahoosh <moisrex@protonmail.com>
2019-05-09 13:04:56 +0430

    fixing the project structure

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
 #include <boost/program_options.hpp>
+#include <iostream>
 
-auto main(int argc, char const ** argv) {
-	std::cout << "working" << std::endl;
-	return EXIT_SUCCESS;
+auto main(int argc, char const** argv) -> int {
+    std::cout << "working" << std::endl;
+    return EXIT_SUCCESS;
 }
