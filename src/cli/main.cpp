0638b87c84e90481af35771a664d706645ad0389
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2019-05-09 13:24:32 +0430

    fixing boost not linking

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -2,6 +2,26 @@
 #include <iostream>
 
 auto main(int argc, char const** argv) -> int {
-    std::cout << "working" << std::endl;
+
+    using namespace boost::program_options;
+    using namespace std;
+
+    options_description desc("webpp cli");
+    desc.add_options()(
+        "help,h", bool_switch()->default_value(false)->implicit_value(true),
+        "print this help");
+
+    variables_map wm;
+    store(command_line_parser(argc, argv).options(desc).run(), wm);
+    notify(wm);
+
+    if (wm.count("help")) {
+        cout << desc << endl;
+        return EXIT_SUCCESS;
+    }
+
+    // default action
+    cout << desc << endl;
+
     return EXIT_SUCCESS;
 }
