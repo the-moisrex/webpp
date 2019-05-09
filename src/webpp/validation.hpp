997ae6bd8d1df47a245a7a6cb57a3b9d5bb40d88
Mohammad Bahoosh <moisrex@protonmail.com>
2019-05-09 11:58:51 +0430

    some changes

--- src/webpp/validation.hpp
+++ src/webpp/validation.hpp
@@ -7,220 +7,212 @@
 
 namespace webpp {
 
-namespace is {
+    namespace is {
 
-/**
- * @brief check if the string is empty or not
- * @return true if the string is empty
- */
-bool empty(std::string const& str) noexcept {
-  return str.empty();
-}
+        /**
+         * @brief check if the string is empty or not
+         * @return true if the string is empty
+         */
+        bool empty(std::string const& str) noexcept { return str.empty(); }
 
-/**
- * @brief check if these two are equal
- */
-template <typename T1, typename T2>
-bool equals(T1 const& first, T2 const& second) noexcept {
-  return first == second;
-}
+        /**
+         * @brief check if these two are equal
+         */
+        template <typename T1, typename T2>
+        bool equals(T1 const& first, T2 const& second) noexcept {
+            return first == second;
+        }
 
-/**
- * @brief check if str contains seed
- * @param str
- * @param seed
- * @return true if it does contain it
- */
-bool contains(std::string const& str, std::string const& seed) noexcept {
-  return str.find(seed) == std::string::npos;
-}
+        /**
+         * @brief check if str contains seed
+         * @param str
+         * @param seed
+         * @return true if it does contain it
+         */
+        bool contains(std::string const& str,
+                      std::string const& seed) noexcept {
+            return str.find(seed) == std::string::npos;
+        }
 
-/**
- * @brief check if the container contains value
- */
-template <typename T>
-bool contains(std::initializer_list<T> const& container,
-              T const& value) noexcept {
-  return std::find(std::begin(container), std::end(container), value) !=
-         std::end(container);
-}
+        /**
+         * @brief check if the container contains value
+         */
+        template <typename T>
+        bool contains(std::initializer_list<T> const& container,
+                      T const& value) noexcept {
+            return std::find(std::begin(container), std::end(container),
+                             value) != std::end(container);
+        }
 
-/**
- * @brief check if the container contains key
- */
-template <template <class, class...> class Container, class T1, class... Args>
-bool contains_key(Container<T1, Args...> const& container,
-                  T1 const& key) noexcept {
-  return container.find(key) != std::end(container);
-}
+        /**
+         * @brief check if the container contains key
+         */
+        template <template <class, class...> class Container, class T1,
+                  class... Args>
+        bool contains_key(Container<T1, Args...> const& container,
+                          T1 const& key) noexcept {
+            return container.find(key) != std::end(container);
+        }
 
-/**
- * @brief check if the container contains the value
- */
-template <template <class, class, class...> class Container,
-          class T1,
-          class T2,
-          class... Args>
-bool contains_value(Container<T1, T2, Args...> const& container,
-                    T2 const& value) noexcept {
-  for (auto pair : container)
-    if (pair.second == value)
-      return true;
-  return false;
-}
+        /**
+         * @brief check if the container contains the value
+         */
+        template <template <class, class, class...> class Container, class T1,
+                  class T2, class... Args>
+        bool contains_value(Container<T1, T2, Args...> const& container,
+                            T2 const& value) noexcept {
+            for (auto pair : container)
+                if (pair.second == value)
+                    return true;
+            return false;
+        }
 
-/**
- * @brief check if the specified character is a whitespace
- * @param c the character to check
- * @return true if c is a whitespace
- */
-bool whitespace(char const& c) {
-  return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == '\f' ||
-         c == '\v';
-}
+        /**
+         * @brief check if the specified character is a whitespace
+         * @param c the character to check
+         * @return true if c is a whitespace
+         */
+        bool whitespace(char const& c) {
+            return c == ' ' || c == '\n' || c == '\r' || c == '\t' ||
+                   c == '\f' || c == '\v';
+        }
 
-/**
- * @brief check if str is right trimmed
- * @param str
- * @return true if there's no whitespaces in the right side of input
- */
-bool rtrimmed(std::string const& str) noexcept {
-  return !whitespace(*str.rbegin());
-}
+        /**
+         * @brief check if str is right trimmed
+         * @param str
+         * @return true if there's no whitespaces in the right side of input
+         */
+        bool rtrimmed(std::string const& str) noexcept {
+            return !whitespace(*str.rbegin());
+        }
 
-/**
- * @brief check if str is left trimmed
- * @param str
- * @return true if there's no whitespaces in the left side of input
- */
-bool ltrimmed(std::string const& str) noexcept {
-  return !whitespace(str[0]);
-}
+        /**
+         * @brief check if str is left trimmed
+         * @param str
+         * @return true if there's no whitespaces in the left side of input
+         */
+        bool ltrimmed(std::string const& str) noexcept {
+            return !whitespace(str[0]);
+        }
 
-/**
- * @brief check if str is right and left trimmed
- * @param str
- * @return true if there's no whitespaces in the right and left side of input
- */
-bool trimmed(std::string const& str) noexcept {
-  return ltrimmed(str) && rtrimmed(str);
-}
+        /**
+         * @brief check if str is right and left trimmed
+         * @param str
+         * @return true if there's no whitespaces in the right and left side of
+         * input
+         */
+        bool trimmed(std::string const& str) noexcept {
+            return ltrimmed(str) && rtrimmed(str);
+        }
 
-/**
- * @brief check if the specified character is a valid number or not
- * @param character
- * @return true if the specified input is an integer
- */
-bool digit(char const& c) noexcept {
-  return c >= '0' && c <= '9';
-}
+        /**
+         * @brief check if the specified character is a valid number or not
+         * @param character
+         * @return true if the specified input is an integer
+         */
+        bool digit(char const& c) noexcept { return c >= '0' && c <= '9'; }
 
-/**
- * @brief check if the specified string is a number (including floating points)
- * @param str
- * @return true if the specified string is a number
- */
-bool number(std::string const& str) noexcept {
-  bool is_first = true;
-  for (auto const& c : str) {
-    if (!digit(c)) {
-      if (is_first && c == '.') {
-        is_first = false;
-        continue;
-      }
-      return false;
-    }
-  }
-  return true;
-}
+        /**
+         * @brief check if the specified string is a number (including floating
+         * points)
+         * @param str
+         * @return true if the specified string is a number
+         */
+        bool number(std::string const& str) noexcept {
+            bool is_first = true;
+            for (auto const& c : str) {
+                if (!digit(c)) {
+                    if (is_first && c == '.') {
+                        is_first = false;
+                        continue;
+                    }
+                    return false;
+                }
+            }
+            return true;
+        }
 
-/**
- * @brief check if the specified string is an integer
- * @param str
- * @return true if the specified string is an integer
- */
-bool integer(std::string const& str) noexcept {
-  for (auto const& c : str)
-    if (!digit(c))
-      return false;
-  return true;
-}
+        /**
+         * @brief check if the specified string is an integer
+         * @param str
+         * @return true if the specified string is an integer
+         */
+        bool integer(std::string const& str) noexcept {
+            for (auto const& c : str)
+                if (!digit(c))
+                    return false;
+            return true;
+        }
 
-/**
- * @brief check if the specified str is an email or not
- * @param str
- * @return true if the specified str is an email
- */
-bool email(std::string const& str) noexcept {
-  static const std::regex pattern{
-      "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*("
-      "\\.[A-Za-z]{2,})$"};
-  return std::regex_match(str, pattern);
-}
+        /**
+         * @brief check if the specified str is an email or not
+         * @param str
+         * @return true if the specified str is an email
+         */
+        bool email(std::string const& str) noexcept {
+            static const std::regex pattern{
+                "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-"
+                "z0-9]+)*(\\.[A-Za-z]{2,})$"};
+            return std::regex_match(str, pattern);
+        }
 
-// bool phone(std::string const& str) noexcept {
-//  static const std::regex pattern{
-//      "^(?:(?:\\+?1\\s*(?:[.-]\\s*)?)?(?:\\(\\s*([2-9]1[02-9]|[2-9][02-8]1|[2-"
-//      "9][02-8][02-9])\\s*\\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))"
-//      "\\s*(?:[.-]\\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\\s*(?:[.-]"
-//      "\\s*)?([0-9]{4})(?:\\s*(?:#|x\\.?|ext\\.?|extension)\\s*(\\d+))?$"};
-//  return std::regex_match(str, pattern);
-//}
-bool FQDN(std::string const& str) noexcept;
-bool url(std::string const& str) noexcept;
+        bool FQDN(std::string const& str) noexcept;
+        bool url(std::string const& str) noexcept;
 
-/**
- * @brief checks if the specified str is an ipv4
- * @param str
- * @return true if str is a valid ipv4
- */
-bool ipv4(std::string const& str) noexcept {
-  static const std::regex pattern{
-      "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]"
-      "?[0-9][0-9]?)$"};
-  return std::regex_match(str, pattern);
-}
+        /**
+         * @brief checks if the specified str is an ipv4
+         * @param str
+         * @return true if str is a valid ipv4
+         */
+        bool ipv4(std::string const& str) noexcept {
+            static const std::regex pattern{
+                "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+                "\\.){3}(25[0-5]|2[0-4][0-9]|[01]"
+                "?[0-9][0-9]?)$"};
+            return std::regex_match(str, pattern);
+        }
 
-bool ipv6(std::string const& str) noexcept;
-bool ip(std::string const& str) noexcept;
-bool ip_range(std::string const& str) noexcept;
-bool ipv4_range(std::string const& str) noexcept;
-bool ipv6_range(std::string const& str) noexcept;
-// bool isImage(something) noexcept;
-bool color(std::string const& str) noexcept;
-bool hex_color(std::string const& str) noexcept;
-bool name_color(std::string const& str) noexcept;
-bool mimetype(std::string const& str) noexcept;
-bool UUID(std::string const& str) noexcept;
-bool port(std::string const& str) noexcept;
-bool mongoid(std::string const& str) noexcept;
-bool lowercase(std::string const& str) noexcept;
-bool uppercase(std::string const& str) noexcept;
+        bool ipv6(std::string const& str) noexcept;
+        bool ip(std::string const& str) noexcept;
+        bool ip_range(std::string const& str) noexcept;
+        bool ipv4_range(std::string const& str) noexcept;
+        bool ipv6_range(std::string const& str) noexcept;
+        // bool isImage(something) noexcept;
+        bool color(std::string const& str) noexcept;
+        bool hex_color(std::string const& str) noexcept;
+        bool name_color(std::string const& str) noexcept;
+        bool mimetype(std::string const& str) noexcept;
+        bool UUID(std::string const& str) noexcept;
+        bool port(std::string const& str) noexcept;
+        bool mongoid(std::string const& str) noexcept;
+        bool lowercase(std::string const& str) noexcept;
+        bool uppercase(std::string const& str) noexcept;
 
-// you may want to change the string to a date of some sort or add both
-bool today(std::string const& str) noexcept;
-bool tomorrow(std::string const& str) noexcept;
-bool yesterday(std::string const& str) noexcept;
-bool this_year(std::string const& str) noexcept;
-bool next_year(std::string const& str) noexcept;
-bool prev_year(std::string const& str) noexcept;
-bool this_month(std::string const& str) noexcept;
-bool next_month(std::string const& str) noexcept;
-bool prev_month(std::string const& str) noexcept;
-bool this_week(std::string const& str) noexcept;
-bool next_week(std::string const& str) noexcept;
-bool prev_week(std::string const& str) noexcept;
-bool between(std::string const& str,
-             std::string const& from,
-             std::string const& after) noexcept;
-bool after(std::string const& str, std::string const& pointintime) noexcept;
-bool before(std::string const& str, std::string const& pointintime) noexcept;
+        // you may want to change the string to a date of some sort or add both
+        bool today(std::string const& str) noexcept;
+        bool tomorrow(std::string const& str) noexcept;
+        bool yesterday(std::string const& str) noexcept;
+        bool this_year(std::string const& str) noexcept;
+        bool next_year(std::string const& str) noexcept;
+        bool prev_year(std::string const& str) noexcept;
+        bool this_month(std::string const& str) noexcept;
+        bool next_month(std::string const& str) noexcept;
+        bool prev_month(std::string const& str) noexcept;
+        bool this_week(std::string const& str) noexcept;
+        bool next_week(std::string const& str) noexcept;
+        bool prev_week(std::string const& str) noexcept;
+        bool between(std::string const& str, std::string const& from,
+                     std::string const& after) noexcept;
+        bool after(std::string const& str,
+                   std::string const& pointintime) noexcept;
+        bool before(std::string const& str,
+                    std::string const& pointintime) noexcept;
 
-bool base64(std::string const& str) noexcept;
-bool escaped(std::string const& str) noexcept;
+        bool base64(std::string const& str) noexcept;
+        bool escaped(std::string const& str) noexcept;
 
-bool username(std::string const& str) noexcept;
-}  // namespace is
-}  // namespace webpp
+        bool username(std::string const& str) noexcept;
+    } // namespace is
+} // namespace webpp
 
-#endif  // VALIDATION_H
+#endif // VALIDATION_H
--- tests/validations.cpp
+++ tests/validations.cpp
@@ -9,54 +9,46 @@ using namespace webpp::is;
 using namespace std;
 
 TEST(ValidationsTest, EmptyFunction) {
-  EXPECT_TRUE(empty(""));
-  EXPECT_FALSE(empty("not empty"));
-  string str = "";
-  EXPECT_TRUE(empty(str));
-  str = "not empty";
-  EXPECT_FALSE(empty(str));
+    EXPECT_TRUE(empty(""));
+    EXPECT_FALSE(empty("not empty"));
+    string str = "";
+    EXPECT_TRUE(empty(str));
+    str = "not empty";
+    EXPECT_FALSE(empty(str));
 }
 
 TEST(ValidationsTest, ContainsFunctions) {
-  map<int, string> data;
-  data[0] = "hello world";
-  data[10] = "testing";
+    map<int, string> data;
+    data[0] = "hello world";
+    data[10] = "testing";
 
-  EXPECT_TRUE(contains_key(data, 10));
-  EXPECT_TRUE(contains_value(data, string("hello world")));
-  EXPECT_FALSE(contains_key(data, 100));
-  EXPECT_FALSE(contains_value(data, string("it's not gonna be there")));
+    EXPECT_TRUE(contains_key(data, 10));
+    EXPECT_TRUE(contains_value(data, string("hello world")));
+    EXPECT_FALSE(contains_key(data, 100));
+    EXPECT_FALSE(contains_value(data, string("it's not gonna be there")));
 }
 
 TEST(ValidationsTest, TrimmedFunctions) {
-  EXPECT_TRUE(trimmed("trimmed string"));
-  EXPECT_TRUE(rtrimmed(" right trimmed"));
-  EXPECT_TRUE(ltrimmed("left trimmed "));
+    EXPECT_TRUE(trimmed("trimmed string"));
+    EXPECT_TRUE(rtrimmed(" right trimmed"));
+    EXPECT_TRUE(ltrimmed("left trimmed "));
 }
 
 TEST(ValidationsTest, IPFunctions) {
-  EXPECT_TRUE(ipv4("255.255.255.255"));
-  EXPECT_FALSE(ipv4("256.1.1.1"));
-  EXPECT_TRUE(ipv4("127.0.0.1"));
-  EXPECT_TRUE(ipv4("0.0.0.0"));
-  EXPECT_TRUE(ipv4("192.168.0.0"));
-  EXPECT_FALSE(ipv4("192.168.1.256"));
-  EXPECT_TRUE(ipv4("192.168.0.255"));
+    EXPECT_TRUE(ipv4("255.255.255.255"));
+    EXPECT_FALSE(ipv4("256.1.1.1"));
+    EXPECT_TRUE(ipv4("127.0.0.1"));
+    EXPECT_TRUE(ipv4("0.0.0.0"));
+    EXPECT_TRUE(ipv4("192.168.0.0"));
+    EXPECT_FALSE(ipv4("192.168.1.256"));
+    EXPECT_TRUE(ipv4("192.168.0.255"));
 }
 
 TEST(ValidationsTest, EmailFunction) {
-  EXPECT_TRUE(email("moisrex@gmail.com"))
-      << "moisrex@gmail.com should be valid";
-  EXPECT_TRUE(email("moisrex.test@gmail.com"))
-      << "moisrex.test@gmail.com should be valid";
-  EXPECT_FALSE(email("not an.email@123.com"))
-      << "spaces are not allowd in emails";
+    EXPECT_TRUE(email("moisrex@gmail.com"))
+        << "moisrex@gmail.com should be valid";
+    EXPECT_TRUE(email("moisrex.test@gmail.com"))
+        << "moisrex.test@gmail.com should be valid";
+    EXPECT_FALSE(email("not an.email@123.com"))
+        << "spaces are not allowd in emails";
 }
-
-// TEST(ValidationsTest, PhoneFunction) {
-//  EXPECT_TRUE(phone("1-234-567-8901"));
-//  EXPECT_TRUE(phone("+989175569874"));
-//  EXPECT_TRUE(phone("09172256987"));
-//  EXPECT_TRUE(phone("1 (234) 567-8901"));
-//  EXPECT_TRUE(phone("1-234-567-8901 ext1234"));
-//}
