601e4e65fc5331e6fd2e35e79e78323ed32251cf
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2019-05-10 21:06:33 +0430

    working on routes

--- src/webpp/router.hpp
+++ src/webpp/router.hpp
@@ -1,31 +1,65 @@
 #ifndef WEBPP_ROUTER_H
 #define WEBPP_ROUTER_H
 
+#include <algorithm>
 #include <functional>
+#include <memory>
 #include <string>
+#include <type_traits>
 #include <vector>
 
 namespace webpp {
 
-    class route {
-      public:
-        bool active = false;
-        ::std::string method;
-        ::std::string slug;
-        ::std::vector<route> subroutes;
+    using matcher_t = std::function<bool(std::string const&)>;
+    using migration_t = std::function<void(std::ostream&)>;
 
-        route(::std::string method, ::std::string slug)
-            : method(method), slug(slug) {}
+    namespace matchers {
+        class path {
+          private:
+            std::string slug;
+
+          public:
+            path(std::string slug) : slug(std::move(slug)) {}
+            bool operator()(std::string const& slug) {
+                return slug == this->slug;
+            }
+        };
+    } // namespace matchers
+
+    /**
+     * @brief This route class contains one single root route and it's children
+     */
+    class route {
+      private:
+        bool active = false;
+        std::string method = "GET";
+        std::vector<std::shared_ptr<route>> subroutes;
+        matcher_t matcher;
+        std::vector<migration_t> migrations;
+        std::shared_ptr<route> parent;
+
+      public:
+        route(std::string method,
+              std::shared_ptr<route> const& parent = nullptr,
+              bool active = false)
+            : method(std::move(method)), parent(parent), active(active) {}
+
+        inline bool is_active() const noexcept {
+            return active && !migrations.empty();
+        }
+
+        inline void activate() noexcept { active = true; }
+        inline void diactivate() noexcept { active = false; }
+
+        bool match(std::string const& path) const noexcept {}
+
+        route* find(std::string const& path) noexcept {}
     };
 
     class router {
-        ::std::vector<route> m_routes;
+        std::vector<std::shared_ptr<route>> root_routes;
 
       public:
-        using callback = ::std::function<void()>;
-        using middleware = ::std::function<void()>;
-        router& use(route new_route, callback _callback,
-                    ::std::vector<middleware> _middlewares = {});
     };
 
 }; // namespace webpp
