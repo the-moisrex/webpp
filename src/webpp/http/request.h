a83bdde55d792f5188c3decaeddd9995f2db558b
Mohammad Bahoosh <moisrex@protonmail.com>
2019-05-23 02:50:45 +0430

    New Docs strategy and adding some methods to cookies

--- src/webpp/http/request.h
+++ src/webpp/http/request.h
@@ -1,20 +1,26 @@
 #ifndef REQUEST_H
 #define REQUEST_H
 
-
 #include "headers.hpp"
 
 namespace webpp {
 
-        class request
-        {
-            private:
-                unsigned int status_code = 200;
+    class request {
+      private:
+        webpp::headers request_headers;
 
-            public:
-                request() = default;
-                request(headers request_headers, unsigned int status_code = 200) noexcept;
+      public:
+        request() = default;
+        request(headers request_headers) noexcept;
 
-        };
-}
+        inline webpp::headers const& headers() const noexcept {
+            return request_headers;
+        }
+
+        inline auto header(std::string const& key) const noexcept {
+            return request_headers[key];
+        }
+    };
+} // namespace webpp
+
 #endif // REQUEST_H
