e8b0ae2958c9c8c793199b88aa1634b89d2b4c59
Mohammad Bahoosh <moisrex@protonmail.com>
2019-05-23 12:17:40 +0430

    trying to add date

---
+++ src/webpp/extra/date.cpp
@@ -0,0 +1,6 @@
+#include "date.h"
+
+date::date()
+{
+
+}
---
+++ src/webpp/extra/date.h
@@ -0,0 +1,12 @@
+#ifndef DATE_H
+#define DATE_H
+
+namespace webpp {
+
+        class date {
+            public:
+                date();
+        };
+
+}
+#endif // DATE_H
