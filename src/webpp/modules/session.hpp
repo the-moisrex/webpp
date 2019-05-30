cb75787d56211c37d90238b40ac53eabfbc1f6c3
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2019-05-30 23:44:44 +0430

    added constructor for session

--- src/webpp/modules/session.hpp
+++ src/webpp/modules/session.hpp
@@ -30,6 +30,8 @@
 
 #include "session/client-adapter/cookie_adapter.h"
 #include "session/server-adapter/memory_adapter.h"
+#include <memory>
+#include <type_traits>
 
 namespace webpp {
 
@@ -37,8 +39,27 @@ namespace webpp {
               class ClientAdapter = webpp::cookie_adapter,
               class CacheServerAdapter = webpp::memory_adapter>
     class session {
+      protected:
+        bool _cache_enabled =
+            !std::is_same<ServerAdapter, CacheServerAdapter>::value;
+        std::unique_ptr<ServerAdapter> _server_adapter;
+        std::unique_ptr<ClientAdapter> _client_adapter;
+        std::unique_ptr<CacheServerAdapter> _cache_adapter;
+
       public:
-        session();
+        session(decltype(_server_adapter)&& server_adapter = nullptr,
+                decltype(_client_adapter)&& client_adapter = nullptr,
+                decltype(_cache_adapter)&& cache_adapter = nullptr) noexcept
+            : _server_adapter{std::move(server_adapter)},
+              _client_adapter{std::move(client_adapter)},
+              _cache_adapter{std::move(cache_adapter)},
+              _cache_enabled{_cache_enabled && _cache_adapter != nullptr} {
+            if (!_server_adapter)
+                _server_adapter = std::make_shared<ServerAdapter>();
+
+            if (!_client_adapter)
+                _client_adapter = std::make_shared<ClientAdapter>();
+        }
     };
 
 } // namespace webpp
