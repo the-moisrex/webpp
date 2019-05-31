77ff56e6db7d5c73288251f3f74f7f0412cc3746
Mohammad Bahoosh <moisrex@protonmail.com>
2019-06-01 00:12:12 +0430

    going to make it multithread

--- src/webpp/modules/session.hpp
+++ src/webpp/modules/session.hpp
@@ -31,34 +31,54 @@
 #include "session/client-adapter/cookie_adapter.h"
 #include "session/server-adapter/memory_adapter.h"
 #include <memory>
+#include <string>
+#include <thread>
 #include <type_traits>
 
 namespace webpp {
 
     template <class ServerAdapter = webpp::memory_adapter,
-              class ClientAdapter = webpp::cookie_adapter,
-              class CacheServerAdapter = webpp::memory_adapter>
+              class ClientAdapter = webpp::cookie_adapter>
     class session {
+      public:
+        using key_t = std::string;
+
       protected:
-        bool _cache_enabled =
-            !std::is_same<ServerAdapter, CacheServerAdapter>::value;
-        std::unique_ptr<ServerAdapter> _server_adapter;
-        std::unique_ptr<ClientAdapter> _client_adapter;
-        std::unique_ptr<CacheServerAdapter> _cache_adapter;
+        /**
+         * @brief The cache for the ServerAdapter in sessions.
+         */
+        class cache_proxy_t {
+          private:
+            std::thread tr;
+            bool _cache_enabled =
+                !std::is_same<ServerAdapter, memory_adapter>::value;
+
+          public:
+            cache_proxy_t(
+                std::unique_ptr<ServerAdapter>&& server_adapter) noexcept
+                : tr(server_adapter) {}
+
+            ~cache_proxy_t() noexcept {
+                if (tr.joinable())
+                    tr.join();
+            }
+        } proxy;
 
       public:
         session(decltype(_server_adapter)&& server_adapter = nullptr,
-                decltype(_client_adapter)&& client_adapter = nullptr,
-                decltype(_cache_adapter)&& cache_adapter = nullptr) noexcept
+                decltype(_client_adapter)&& client_adapter = nullptr) noexcept
             : _server_adapter{std::move(server_adapter)},
-              _client_adapter{std::move(client_adapter)},
-              _cache_adapter{std::move(cache_adapter)},
-              _cache_enabled{_cache_enabled && _cache_adapter != nullptr} {
+              _client_adapter{std::move(client_adapter)} {
             if (!_server_adapter)
                 _server_adapter = std::make_shared<ServerAdapter>();
 
             if (!_client_adapter)
                 _client_adapter = std::make_shared<ClientAdapter>();
+        }
+
+        template <typename T>
+        T& get(session::key_t key) noexcept {
+            return proxy.get(key);
         }
     };
 
