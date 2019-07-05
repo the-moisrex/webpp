b9ea492477916a91914707a2b6b2b2b18873dd52
Mohammad Bahoosh <moisrex@protonmail.com>
2019-07-05 17:35:25 +0430

    made getenv secure
    so many comments I hvae to write later.

--- src/webpp/interfaces/cgi.h
+++ src/webpp/interfaces/cgi.h
@@ -12,14 +12,16 @@ namespace webpp {
         cgi();
         char const* header(char const* const) const noexcept;
         ::webpp::headers headers() const noexcept;
+        char const* server_addr() const noexcept;
+        int server_port() const noexcept;
         char const* remote_addr() const noexcept;
-        char const* remote_port() const noexcept;
+        int remote_port() const noexcept;
         char const* server_name() const noexcept;
         char const* scheme() const noexcept;
         char const* server_protcol() const noexcept;
         char const* method() const noexcept;
         void run(::webpp::router const& _router) noexcept;
-        char const* env(char const* const& name = nullptr) const noexcept;
+        char const* env(char const* const &name) const noexcept;
     };
 
 } // namespace webpp
