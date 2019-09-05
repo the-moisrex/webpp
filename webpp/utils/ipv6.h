5d23b1c41b1bea7ffae09cd62a8d3eb9478eb397
Mohammad Bahoosh <moisrex@protonmail.com>
2019-09-06 04:08:50 +0430

    some changes

--- webpp/utils/ipv6.h
+++ webpp/utils/ipv6.h
@@ -11,21 +11,143 @@ namespace webpp {
     class ipv6 {
       private:
         static constexpr auto IPV6_ADDR_SIZE = 16; // Bytes
+        using fields_t = std::array<uint8_t, IPV6_ADDR_SIZE>;
 
-        union fields_t {
-            uint8_t m8[IPV6_ADDR_SIZE];                      // 8-bit
-            uint16_t m16[IPV6_ADDR_SIZE / sizeof(uint16_t)]; // 16-bit
-            uint32_t m32[IPV6_ADDR_SIZE / sizeof(uint32_t)]; // 32-bit
-            uint64_t m64[IPV6_ADDR_SIZE / sizeof(uint64_t)]; // 64-bit
-        };
-
-        std::variant<std::string_view, fields_t> data;
+        // I didn't go with a union because in OpenThread project they did and
+        // they had to deal with endianess of their data. I rather use shifts
+        // and host's byte order instead of getting my hands dirty with host's
+        // byte order. Network's byte order is big endian btw, but here we just
+        // have to worry about the host's byte order because we are not sending
+        // these data over the network.
+        mutable std::variant<std::string_view, fields_t> data;
 
       public:
         constexpr fields_t fields() const noexcept {
             if (std::holds_alternative<fields_t>(data))
                 return std::get<fields_t>(data);
-            // todo: parse
+            auto _data = std::get<std::string_view>(data);
+            fields_t _fields = {}; // all zero
+
+            uint16_t val = 0;
+            uint8_t count = 0;
+            bool first = true;
+            bool hasIp4 = false;        // contains ipv4
+            char ch = 0;                // each character
+            uint8_t d = 0;              // numeric representation
+            auto iter = _data.begin();  // iterator
+            auto endp = _fields.end();  // finish line
+            auto dst = _fields.begin(); // something I can't explain :)
+            decltype(dst) colonp = _fields.end();
+            const char* colonc = nullptr;
+
+            dst--;
+
+            for (;;) {
+                ch = *iter++;
+                d = ch & 0xf;
+
+                // read Hexadecimals
+                if (('a' <= ch && ch <= 'f') || ('A' <= ch && ch <= 'F')) {
+                    d += 9;
+                } else if (ch == ':' || ch == '\0' || ch == ' ') {
+                    // read seperators
+                    if (count) {
+                        if (dst + 2 != endp) {
+                            data.emplace<fields_t>(); // fill with zeros
+                            return std::get<fields_t>(data);
+                        }
+
+                        *(dst + 1) = static_cast<uint8_t>(val >> 8);
+                        *(dst + 2) = static_cast<uint8_t>(val);
+                        dst += 2;
+                        count = 0;
+                        val = 0;
+                    } else if (ch == ':') {
+
+                        // verify or throw up in the user's face :)
+                        if (colonp == nullptr || first) {
+                            data.emplace<fields_t>(); // fill with zeros
+                            return std::get<fields_t>(data);
+                        }
+                        colonp = dst;
+                    }
+
+                    if (ch == '\0' || ch == ' ') {
+                        break;
+                    }
+
+                    colonc = aBuf;
+
+                    continue;
+                } else if (ch == '.') {
+                    hasIp4 = true;
+
+                    // Do not count bytes of the embedded IPv4 address.
+                    endp -= kIp4AddressSize;
+
+                    VerifyOrExit(dst <= endp, error = OT_ERROR_PARSE);
+
+                    break;
+                } else {
+                    VerifyOrExit('0' <= ch && ch <= '9',
+                                 error = OT_ERROR_PARSE);
+                }
+
+                first = false;
+                val = static_cast<uint16_t>((val << 4) | d);
+                VerifyOrExit(++count <= 4, error = OT_ERROR_PARSE);
+            }
+
+            if (colonp || dst == endp) {
+                data.emplace<fields_t>(); // fill with zeros
+                return std::get<fields_t>(data);
+            }
+
+            while (colonp && dst > colonp) {
+                *endp-- = *dst--;
+            }
+
+            while (endp > dst) {
+                *endp-- = 0;
+            }
+
+            if (hasIp4) {
+                val = 0;
+
+                // Reset the start and end pointers.
+                dst = reinterpret_cast<uint8_t*>(mFields.m8 + 12);
+                endp = reinterpret_cast<uint8_t*>(mFields.m8 + 15);
+
+                for (;;) {
+                    ch = *colonc++;
+
+                    if (ch == '.' || ch == '\0' || ch == ' ') {
+                        VerifyOrExit(dst <= endp, error = OT_ERROR_PARSE);
+
+                        *dst++ = static_cast<uint8_t>(val);
+                        val = 0;
+
+                        if (ch == '\0' || ch == ' ') {
+                            // Check if embedded IPv4 address had exactly four
+                            // parts.
+                            VerifyOrExit(dst == endp + 1,
+                                         error = OT_ERROR_PARSE);
+                            break;
+                        }
+                    } else {
+                        VerifyOrExit('0' <= ch && ch <= '9',
+                                     error = OT_ERROR_PARSE);
+
+                        val = (10 * val) + (ch & 0xf);
+
+                        // Single part of IPv4 address has to fit in one byte.
+                        VerifyOrExit(val <= 0xff, error = OT_ERROR_PARSE);
+                    }
+                }
+            }
+
+        exit:
+            return error;
         }
 
         /**
