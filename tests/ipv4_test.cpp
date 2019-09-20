b148e5753809468d5eca2f97c8713b374596f4b8
Mohammad Bahoosh <moisrex@protonmail.com>
2019-09-20 16:06:12 +0430

    ipv4 tests added

--- tests/ipv4_test.cpp
+++ tests/ipv4_test.cpp
@@ -1,10 +1,27 @@
 #include <gtest/gtest.h>
+#include <string_view>
 #include <webpp/utils/ipv4.h>
 
 using namespace webpp;
 
 TEST(IPv4Tests, Creation) {
-    constexpr ipv4 one({192, 168, 1, 1});
-    constexpr ipv4 two("192.168.1.1");
-    EXPECT_TRUE(one.integer() == two.integer());
+    constexpr ipv4 one{192, 168, 1, 1};
+    ipv4 two("192.168.1.1");
+    EXPECT_EQ(one, two);
+    EXPECT_EQ(one.integer(), two.integer());
+}
+
+TEST(IPv4Tests, Methods) {
+    ipv4 ip{192, 168, 1, 1};
+    EXPECT_TRUE(ip.is_private());
+    EXPECT_FALSE(ip.is_public());
+    EXPECT_FALSE(ip.is_all_zero());
+    EXPECT_TRUE(ip.is_valid());
+    EXPECT_EQ(ip.str(), "192.168.1.1");
+
+    auto octets = ip.octets();
+    EXPECT_EQ(octets[0], 192);
+    EXPECT_EQ(octets[1], 168);
+    EXPECT_EQ(octets[2], 1);
+    EXPECT_EQ(octets[3], 1);
 }
