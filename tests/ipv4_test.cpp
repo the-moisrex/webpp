db7f8ed2f5cf9e068c8f00d80eae1d4929b80bd1
Mohammad Bahoosh <moisrex@protonmail.com>
2019-09-20 05:57:44 +0430

    preparing env to write tests for ip classes

--- tests/ipv4_test.cpp
+++ tests/ipv4_test.cpp
@@ -3,7 +3,7 @@
 
 using namespace webpp;
 
-TEST(IpTests, IPv4Creation) {
+TEST(IPv4Tests, Creation) {
     constexpr ipv4 one({192, 168, 1, 1});
     constexpr ipv4 two("192.168.1.1");
     EXPECT_TRUE(one.integer() == two.integer());
--- tests/ipv6_test.cpp
+++ tests/ipv6_test.cpp
@@ -1,10 +1,8 @@
 #include <gtest/gtest.h>
-#include <webpp/utils/ipv4.h>
+#include <webpp/utils/ipv6.h>
 
 using namespace webpp;
 
-TEST(IpTests, IPv4Creation) {
-    constexpr ipv4 one({192, 168, 1, 1});
-    constexpr ipv4 two("192.168.1.1");
-    EXPECT_TRUE(one.integer() == two.integer());
+TEST(IPv6Tests, Creation) {
+
 }
