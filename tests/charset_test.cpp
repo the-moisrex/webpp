af23efb973a0415dd910dbc5d0e3203359fd175f
Mohammad Bahoosh <moisrex@protonmail.com>
2019-09-29 21:16:40 +0330

    charset test fix

--- tests/charset_test.cpp
+++ tests/charset_test.cpp
@@ -4,12 +4,12 @@
 using namespace webpp;
 
 TEST(CharsetTest, CharsetCreation) {
-    constexpr auto chars = charset('a', 'z');
-    EXPECT_TRUE(chars.contains('a'));
-    EXPECT_TRUE(chars.contains('b'));
-    EXPECT_TRUE(chars.contains('z'));
-    EXPECT_FALSE(chars.contains('A'));
-    EXPECT_FALSE(chars.contains('Z'));
-    EXPECT_FALSE(chars.contains('&'));
-    EXPECT_FALSE(chars.contains('\0'));
+    constexpr auto chars = charset<'a', 'z'>();
+    EXPECT_TRUE(chars.contains('a')) << "contains 'a'";
+    EXPECT_TRUE(chars.contains('b')) << "contains 'b'";
+    EXPECT_TRUE(chars.contains('z')) << "contains 'z'";
+    EXPECT_FALSE(chars.contains('A')) << "shouldn't contain 'A'";
+    EXPECT_FALSE(chars.contains('Z')) << "shouldn't contain 'Z'";
+    EXPECT_FALSE(chars.contains('&')) << "shouldn't contain '&'";
+    EXPECT_FALSE(chars.contains('\0')) << "shouldn't contain '\\0'";
 }
