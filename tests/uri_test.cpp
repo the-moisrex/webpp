d2c39cb956905fb2391e472a83e8a76f43f53a3a
Mohammad Bahoosh <moisrex@protonmail.com>
2019-10-10 16:59:43 +0000

    God damn it

--- tests/uri_test.cpp
+++ tests/uri_test.cpp
@@ -83,3 +83,47 @@ TEST(URITests, IPv6HostName) {
     u.clear_path();
     EXPECT_EQ(u.str(), "//[::1]:8080/?name=value&name2=value2#str");
 }
+
+TEST(URITests, WieredURIs) {
+    // some examples from https://rosettacode.org/wiki/URL_parser
+    auto _uris = {
+        "ftp://ftp.is.co.za/rfc/rfc1808.txt",
+        "http://www.ietf.org/rfc/rfc2396.txt",
+        "ldap://[2001:db8::7]/c=GB?objectClass?one",
+        "mailto:John.Doe@example.com",
+        "news:comp.infosystems.www.servers.unix",
+        "tel:+1-816-555-1212",
+        "telnet://192.0.2.16:80/",
+        "urn:oasis:names:specification:docbook:dtd:xml:4.1.2",
+        "foo://example.com:8042/over/there?name=ferret#nose",
+        "urn:example:animal:ferret:nose",
+        "jdbc:mysql://test_user:test@test.com:3306/sakila?profileSQL=true",
+        "ftp://ftp.is.co.za/rfc/rfc1808.txt",
+        "http://www.ietf.org/rfc/rfc2396.txt#header1",
+        "ldap://[2001:db8::7]/c=GB?objectClass=one&objectClass=two",
+        "mailto:[email protected]",
+        "news:comp.infosystems.www.servers.unix",
+        "tel:+1-816-555-1212",
+        "telnet://192.0.2.16:80/",
+        "urn:oasis:names:specification:docbook:dtd:xml:4.1.2",
+        "ssh://test@test.com",
+        "https://bob:pass@test.com/place",
+        "http://example.com/?a=1&b=2+2&c=3&c=4&d=%65%6e%63%6F%64%65%64"};
+
+    for (auto const& _uri : _uris) {
+        EXPECT_TRUE(const_uri(_uri).is_valid());
+    }
+}
+
+TEST(URITests, URN) {
+    const_uri a("urn:example:a123,z456");
+    const_uri b = "URN:example:a123,z456";
+    const_uri c = "urn:EXAMPLE:a123,z456";
+
+    EXPECT_EQ(a, b);
+    EXPECT_EQ(a, c);
+    EXPECT_EQ(b, c);
+    EXPECT_TRUE(a.is_urn());
+    EXPECT_TRUE(b.is_urn());
+    EXPECT_TRUE(c.is_urn());
+}
