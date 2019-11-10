e5455dfddf5d5c6403144aaad818b947a3945ce0
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2019-11-10 17:52:46 +0000

    comments added and examples written
    the comments and examples are not 100% and probably will change.

--- examples/01-hello-world/main.cpp
+++ examples/01-hello-world/main.cpp
@@ -3,18 +3,30 @@
 using namespace webpp;
 
 using request = request_t<cgi>;
-using response = request_t<cgi>;
 
 response about_page(request req) { return file("about.html"); }
 
 int main() {
 
   webpp<cgi> app;
-    app.router.on(GET & "/home"_path, [](auto& req, auto& res) {
+    app.router.on(get & "/home"_path, [](auto& req, auto& res) {
         res << "hello world";
         res << '\n';
     });
-    app.router.on(GET and "/about"_path, about_page);
+  app.router.on(get and "/about"_path, about_page);
+  app.router.on("/user/{user_id}"_tpath, [](auto const& req, auto& res) {
+      auto headers = res.headers();
+      headers["User-Id"] = req.url().segments()["user_id"];
 
-    return app.run();
+      app.router.on(ends_with("/profile"_tpath),
+                    []() { return "welcome to your profile"; });
+
+      app.router.on(post and ends_with("/contact"_path),
+                    [](auto& req, auto& res) {
+                        auto jdata = req.body().json();
+                        // ...
+                    });
+  });
+
+  return app.run();
 }
--- webpp/http/body.h
+++ webpp/http/body.h
@@ -6,9 +6,83 @@
 #include <string_view>
 
 /**
- * This class will not load, cache, or parse the body until the user needs it.
- * This means that we need access to the Interface so we can use it to read the
- * data of the body, then we parse it to the thing that the user needs.
+ * There are two types of bodies:
+ *   - The request body
+ *   - The response body
+ *
+ * Common things between the two:
+ *
+ * Request body:
+ *   This class will not load, cache, or parse the body until the user needs it.
+ *   This means that we need access to the Interface so we can use it to read
+ *   the data of the body, then we parse it to the thing that the user needs.
+ *
+ *   - Parsing formats:
+ *     - [ ] JSON that returns:
+ *       - [ ] JSONObject or some sort of custom class
+ *       - [ ] Array
+ *       - [ ] std::vector
+ *       - [ ] std::multimap
+ *     - [ ] blob
+ *     - [ ] GraphQL Object
+ *     - [ ] Form inputs
+ *       - [ ] format 1 (I don't remember the type now)
+ *       - [ ] format 2 TODO
+ *
+ * Response Body:
+ *   - Features of the response body:
+ *     - Should not be a template or we have to implement free functions
+ *     - Owns it's data
+ *
+ *   - Caching system
+ *     - In memory and on hard cache:
+ *       - [ ] In memory cache with use counts
+ *         - so it's possible to migrate the in memory cache into hard disk when
+ *           memory is about to finish
+ *       - [ ] In hard disk cache (completely remove-able)
+ *       - [ ] Permanent cache
+ *         - We shouldn't need this. Think about it. TODO
+ *     - Static responses:
+ *       - [ ] Static files -> html, css, js, images, ...
+ *     - Half dynamic responses:
+ *       - [ ] The HTML templates
+ *     - Full dynamic responses:
+ *       - [ ] Forward caching
+ *         - Means that the user will predict the user's future request based
+ *           on the current and the past requests he/she made and then generates
+ *           a response a head of time if the system is not busy.
+ *
+ *   - Minifying/Compressing the responses:
+ *     - [ ] GZip
+ *     - [ ] Data themselves:
+ *       - [ ] HTML
+ *       - [ ] CSS
+ *       - [ ] JS
+ *       - [ ] Images
+ *         - [ ] JPEG
+ *         - [ ] GIF
+ *         - [ ] PNG
+ *
+ *   - Writing the data in different formats
+ *     - [ ] String
+ *     - [ ] JSON from:
+ *       - [ ] Strings
+ *       - [ ] Iterate-ables: arrays, vectors, maps, ...
+ *     - Media
+ *       - Images
+ *         - [ ] JPEG
+ *         - [ ] PNG
+ *         - [ ] GIF
+ *         - [ ] research more for standards
+ *       - Videos
+ *         - [ ] mp4
+ *         - [ ] flv
+ *         - [ ] research more for standards
+ *       - Audios
+ *         - [ ] mp3
+ *         - [ ] research more for standards
+ *     - [ ] GraphQL
+ *     - [ ] Downloadable files of any type (blobs)
  *
  */
 namespace webpp {
@@ -17,9 +91,10 @@ namespace webpp {
       private:
 
       public:
+        auto json() const;
+        auto json(std::string_view const& data);
 
-
-        void* const& json() const;
+        auto file(std::string_view const& filepath);
 
         /**
          * @brief get the whole body as a string
