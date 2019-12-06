49bb82925db5d9e95b31acdcf841472ff025a9b2
Mohammad Bahoosh <moisrex@protonmail.com>
2019-12-06 13:07:13 +0000

    move function_ref to functional source file

---
+++ webpp/utils/functional.h
@@ -0,0 +1,38 @@
+#ifndef WEBPP_FUNCTIONAL_H
+#define WEBPP_FUNCTIONAL_H
+
+// Created by moisrex on 12/6/19.
+
+namespace webpp {
+    template <typename Signature>
+    class function_ref;
+
+    template <typename Return, typename... Args>
+    class function_ref<Return(Args...)> final {
+      private:
+        using signature_type = Return(void*, Args...);
+
+        void* _ptr;
+        Return (*_erased_fn)(void*, Args...);
+
+      public:
+        template <typename T,
+                  typename = std::enable_if_t<
+                      std::is_invocable<T&, Args...>{} &&
+                      !std::is_same<std::decay_t<T>, function_ref>{}>>
+        constexpr function_ref(T&& x) noexcept
+            : _ptr{(void*)std::addressof(x)} {
+            _erased_fn = [](void* ptr, Args... xs) -> Return {
+                return (*reinterpret_cast<std::add_pointer_t<T>>(ptr))(
+                    std::forward<Args>(xs)...);
+            };
+        }
+
+        constexpr decltype(auto) operator()(Args... xs) const
+            noexcept(noexcept(_erased_fn(_ptr, std::forward<Args>(xs)...))) {
+            return _erased_fn(_ptr, std::forward<Args>(xs)...);
+        }
+    };
+} // namespace webpp
+
+#endif // WEBPP_FUNCTIONAL_H
