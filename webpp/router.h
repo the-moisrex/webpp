d6ebbac826da90ed8d78ab334730489d64780ddb
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2019-12-06 13:07:13 +0000

    move function_ref to functional source file

--- webpp/router.h
+++ webpp/router.h
@@ -9,35 +9,7 @@
 
 namespace webpp {
 
-    template <typename Signature>
-    class function_ref;
 
-    template <typename Return, typename... Args>
-    class function_ref<Return(Args...)> final {
-      private:
-        using signature_type = Return(void*, Args...);
-
-        void* _ptr;
-        Return (*_erased_fn)(void*, Args...);
-
-      public:
-        template <typename T,
-                  typename = std::enable_if_t<
-                      std::is_invocable<T&, Args...>{} &&
-                      !std::is_same<std::decay_t<T>, function_ref>{}>>
-        constexpr function_ref(T&& x) noexcept
-            : _ptr{(void*)std::addressof(x)} {
-            _erased_fn = [](void* ptr, Args... xs) -> Return {
-                return (*reinterpret_cast<std::add_pointer_t<T>>(ptr))(
-                    std::forward<Args>(xs)...);
-            };
-        }
-
-        constexpr decltype(auto) operator()(Args... xs) const
-            noexcept(noexcept(_erased_fn(_ptr, std::forward<Args>(xs)...))) {
-            return _erased_fn(_ptr, std::forward<Args>(xs)...);
-        }
-    };
 
     /**
      * @brief This route class contains one single root route and it's children
