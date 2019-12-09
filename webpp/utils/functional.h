02a7a738f2a5aed66c7159a6da64330d900d61b2
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2019-12-09 16:00:39 +0000

    added the class structure for debounced class

--- webpp/utils/functional.h
+++ webpp/utils/functional.h
@@ -74,6 +74,55 @@ namespace webpp {
         }
     }
 
+    /**
+     * Creates a debounced function that delays invoking func until after wait
+     * milliseconds have elapsed since the last time the debounced function was
+     * invoked. The debounced function comes with a cancel method to cancel
+     * delayed func invocations and a flush method to immediately invoke them.
+     * Provide options to indicate whether func should be invoked on the leading
+     * and/or trailing edge of the wait timeout. The func is invoked with the
+     * last arguments provided to the debounced function. Subsequent calls to
+     * the debounced function return the result of the last func invocation.
+     *
+     * Note: If leading and trailing options are true, func is invoked on the
+     * trailing edge of the timeout only if the debounced function is invoked
+     * more than once during the wait timeout.
+     *
+     * If wait is 0 and leading is false, func invocation is deferred until to
+     * the next tick, similar to setTimeout with a timeout of 0 (in javascript).
+     *
+     * @see https://css-tricks.com/debouncing-throttling-explained-examples/
+     * @see https://lodash.com/docs#debounce
+     * @see
+     * https://github.com/lodash/lodash/blob/15e1557b2a97c8bbee22d873832d90ed3ba50ba7/debounce.js
+     * @tparam Callable
+     */
+    template <typename Callable>
+    class debounce {
+        Callable func;
+        std::chrono::steady_clock::rep wait;
+        std::chrono::time_point<std::chrono::steady_clock> last_invoke_time;
+        bool leading = false;
+        bool trailing = true;
+
+      public:
+        debounce(Callable callable, decltype(wait) wait = 0) noexcept
+            : func(std::move(callable)), wait(wait) {
+            static_assert(std::is_invocable_v<Callable>,
+                          "The Callable specified is not actually callable.");
+        }
+
+        template <typename... Args>
+        auto operator()(Args... args) noexcept {
+            func(args...);
+        }
+
+        void cancel() noexcept {}
+
+        auto flush() noexcept {}
+
+        auto pending() noexcept {}
+    };
 } // namespace webpp
 
 #endif // WEBPP_FUNCTIONAL_H
