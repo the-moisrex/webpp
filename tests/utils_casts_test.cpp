ecf8d9099b96b2396d3abae5a9b1f9db50fb0e1e
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-01-27 03:10:46 +0000

    tests added for casts

--- tests/utils_casts_test.cpp
+++ tests/utils_casts_test.cpp
@@ -13,4 +13,27 @@ TEST(Casts, ToInt) {
     EXPECT_EQ(to_uint("-10"), 10);
     EXPECT_EQ(to<int>("-10"), -10);
     EXPECT_EQ(to<unsigned long long>("+1025153153"), 1025153153);
+
+    auto invalids = {
+        "one",
+        "+two",
+        "12a",
+        "a11",
+        "+a11",
+        "111-751",
+        "123,321",
+        "123.123",
+        "123+123"
+    };
+
+    for (auto const& c : invalids) {
+        EXPECT_EQ(to<int>(c), 0);
+        EXPECT_EQ(to_uint64(c), 0);
+        try {
+            to<int, true, true>(c);
+            EXPECT_TRUE(false) << c;
+        } catch (std::exception const& err) {
+            EXPECT_TRUE(true);
+        }
+    }
 }
