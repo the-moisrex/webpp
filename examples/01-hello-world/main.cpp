4e4e962a2eaa7610796196a15c8b508a797e8878
Mohammad Bahoosh <moisrex@protonmail.com>
2020-01-27 02:40:42 +0000

    file reformat

--- examples/01-hello-world/main.cpp
+++ examples/01-hello-world/main.cpp
@@ -8,25 +8,25 @@ response about_page(request req) { return file("about.html"); }
 
 int main() {
 
-  webpp<cgi> app;
+    webpp<cgi> app;
     app.router.on(get & "/home"_path, [](auto& req, auto& res) {
         res << "hello world";
         res << '\n';
     });
-  app.router.on(get and "/about"_path, about_page);
-  app.router.on("/user/{user_id}"_tpath, [](auto const& req, auto& res) {
-      auto headers = res.headers();
-      headers["User-Id"] = req.url().segments()["user_id"];
+    app.router.on(get and "/about"_path, about_page);
+    app.router.on("/user/{user_id}"_tpath, [](auto const& req, auto& res) {
+        auto headers = res.headers();
+        headers["User-Id"] = req.url().segments()["user_id"];
 
-      app.router.on(ends_with("/profile"_tpath),
-                    []() { return "welcome to your profile"; });
+        app.router.on(ends_with("/profile"_tpath),
+                      []() { return "welcome to your profile"; });
 
-      app.router.on(post and ends_with("/contact"_path),
-                    [](auto& req, auto& res) {
-                        auto jdata = req.body().json();
-                        // ...
-                    });
-  });
+        app.router.on(post and ends_with("/contact"_path),
+                      [](auto& req, auto& res) {
+                          auto jdata = req.body().json();
+                          // ...
+                      });
+    });
 
-  return app.run();
+    return app.run();
 }
