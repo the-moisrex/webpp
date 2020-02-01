a2ab92d32ce42f6bc08bd7aa7f6d7b84adf8c88d
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-02-01 05:39:35 +0000

    Lots of changes; Debugging

--- tests/functional_test.cpp
+++ tests/functional_test.cpp
@@ -11,18 +11,27 @@ void test(int limit) {
     EXPECT_TRUE(i < limit - 1) << "i is: " << i << "; limit: " << limit;
 }
 
+struct MyCallable {
+    void operator()(int limit) {
+        static auto i = 0;
+        i++;
+        EXPECT_TRUE(i < limit - 1) << "i is: " << i << "; limit: " << limit;
+    }
+};
+
 TEST(FunctionalTests, DebouncedFunctions) {
     using namespace std::chrono;
 
-    constexpr auto checking_deduction_for_function_pointers = debounce(test);
-    constexpr auto debounced_test = debounce(test, milliseconds(10));
     constexpr auto limit = 1000;
+
+    constexpr auto checking_deduction_for_function_pointers = debounce_t(test);
+    constexpr auto debounced_test = debounce_t(milliseconds(10), test);
     for (int i = 0; i < limit; i++)
         debounced_test(limit);
 
     // lambdas
 
-    constexpr auto lambda_test = debounce([](int limit) {
+    constexpr auto lambda_test = debounce_t([](int limit) {
         static auto i = 0;
         i++;
         EXPECT_LT(i, limit);
@@ -30,4 +39,10 @@ TEST(FunctionalTests, DebouncedFunctions) {
 
     for (int i = 0; i < limit; i++)
         lambda_test(limit);
+
+    // class
+
+    constexpr debounce_t<MyCallable> debounced_class;
+    for (int i = 0; i < limi; i++)
+        debounced_class(limit);
 }
