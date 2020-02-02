686f1a6bb61cba7f41da84c067043979ae1ac74c
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-02-02 12:01:06 +0000

    fix valve

--- tests/valves_test.cpp
+++ tests/valves_test.cpp
@@ -1,10 +1,26 @@
 // Created by moisrex on 11/7/19.
 #include <gtest/gtest.h>
 #include <webpp/valves/valve.h>
+#include <webpp/http/request.h>
+#include <string>
 
 using namespace webpp;
 using namespace webpp::valves;
 
+namespace webpp {
+    class fake_cgi_GET;
+
+    template<>
+    class request_t<fake_cgi_GET> {
+    public:
+        std::string request_method() const noexcept {
+            return "GET";
+        }
+    };
+}
+
 TEST(ValvesTests, Creation) {
-    constexpr auto v = method("GET") or method("POST");
+    auto v = method("GET") or method("POST");
+    EXPECT_TRUE(v(request_t<fake_cgi_GET>()));
+
 }
