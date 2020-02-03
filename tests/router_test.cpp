2b7dc4325d6aa4f7fb27c245b391aaff69ad510d
Mohammad Bahoosh <moisrex@protonmail.com>
2020-02-03 07:24:56 +0000

    Code clean-up
    tooooooo much noise!

--- tests/router_test.cpp
+++ tests/router_test.cpp
@@ -7,6 +7,8 @@ using namespace webpp;
 
 TEST(Router, RouteCreation) {
     using request = request_t<cgi>;
+
+    // this will happen with a help of a little "user-defined template deduction guide"
     constexpr route<cgi> about_page([](response &res) noexcept {
         res << "About page\n";
     });
