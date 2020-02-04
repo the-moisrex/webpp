90be715675376280366e9e9a0f7c3046e7f15cd5
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-02-04 03:45:44 +0000

    response tests added

---
+++ tests/response_test.cpp
@@ -0,0 +1,16 @@
+// Created by moisrex on 2/4/20.
+
+#include <gtest/gtest.h>
+#include <webpp/http/response.h>
+
+using namespace webpp;
+
+TEST(Response, Init) {
+    auto res = response();
+    auto res2 = response();
+    EXPECT_EQ(std::string(res.body.str("")), "");
+    res2 << "Code";
+    EXPECT_EQ(std::string(res2.body.str()), "Code");
+    res = res2;
+    EXPECT_EQ(std::string(res.body.str()), "Code");
+}
