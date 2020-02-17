8e3aa972d4ae95fa077977b138d9b0268b75746a
Mohammad Bahoosh <moisrex@protonmail.com>
2020-02-17 21:50:50 +0000

    linking is now correct

--- cmake/header_functions.cmake
+++ cmake/header_functions.cmake
@@ -1,16 +1,36 @@
 
 # This function will links other extensions to their corresponding header
-function(link_extensions INPUT_DIR OUTPUT_DIR INCLUDE_DIRNAME ALL_EXTENSIONS)
+function(install_headers INPUT_DIR OUTPUT_DIR ALL_EXTENSIONS)
+  message(STATUS "Header source files     : ${INPUT_DIR}")
+  message(STATUS "Installing headers into : ${OUTPUT_DIR}")
 
   # copy .h extensions
-  file(INSTALL "${INPUT_DIR}" DESTINATION "${OUTPUT_DIR}")
+  file(GLOB INCLUDE_ROOT_FILES_AND_DIRS 
+    LIST_DIRECTORIES true 
+    RELATIVE "${INPUT_DIR}"
+    "${INPUT_DIR}/*")
+  foreach(F IN LISTS INCLUDE_ROOT_FILES_AND_DIRS)
+    file(INSTALL "${INPUT_DIR}/${F}" DESTINATION "${OUTPUT_DIR}")
+  endforeach()
 
   # get the file names
-  file(
-    GLOB_RECURSE LIB_FILES "${OUTPUT_DIR}/${INCLUDE_DIRNAME}"
-    LIST_DIRECTORIES false
-    *.h
-  )
+  set(LIB_FILES "")
+  foreach(F IN LISTS INCLUDE_ROOT_FILES_AND_DIRS)
+    if (IS_DIRECTORY "${OUTPUT_DIR}/${F}")
+      file(
+        GLOB_RECURSE LIB_FILES__ 
+        LIST_DIRECTORIES false
+        "${OUTPUT_DIR}/${F}/*.h"
+      )
+      foreach(FF IN LISTS LIB_FILES__)
+        if(NOT IS_DIRECTORY "${FF}")
+          list(APPEND LIB_FILES "${FF}")
+        endif()
+      endforeach()
+    else()
+      list(APPEND LIB_FILES "${OUTPUT_DIR}/${F}")
+    endif()
+  endforeach()
   message(STATUS "Founded files with .h extension: ${LIB_FILES}")
 
 
@@ -19,6 +39,7 @@ function(link_extensions INPUT_DIR OUTPUT_DIR INCLUDE_DIRNAME ALL_EXTENSIONS)
     foreach(F IN LISTS LIB_FILES)
       string(REPLACE ".h" "${NEW_EXT}" NEW_FILE "${F}")
       if(NOT IS_DIRECTORY "${NEW_FILE}")
+        message(STATUS "Linking file \"${F}\" to \"${NEW_FILE}\"")
         file(CREATE_LINK "${F}" "${NEW_FILE}" COPY_ON_ERROR)
       endif()
     endforeach()
