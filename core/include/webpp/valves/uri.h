07739b68eb86169c962cdd4c1a2456dfda9359e4
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-02-20 17:11:25 +0000

    tpath added, which may change a lot soon

--- core/include/webpp/valves/uri.h
+++ core/include/webpp/valves/uri.h
@@ -2,6 +2,8 @@
 #define WEBPP_VALVE_URI_H
 
 #include "valve.h"
+#include <cstddef> // for std::size_t
+#include <string_view>
 
 namespace webpp::valves {
 
@@ -24,6 +26,38 @@ namespace webpp::valves {
     struct path : public valve<path_condition> {
         using valve<path_condition>::valve;
     };
+
+    constexpr path operator""_path(const char* str, std::size_t len) {
+        return path{std::string_view{str, len}};
+    }
+
+    // templated path
+
+    struct tpath_condition {
+      protected:
+        std::string_view tpath_str;
+
+      public:
+        constexpr tapth_condition(std::string_view str) noexcept
+            : tpath_str(str) {}
+
+        constexpr tpath_condition() noexcept = default;
+
+        template <typename RequestType>
+        [[nodiscard]] bool operator()(RequestType const& req) const noexcept {
+            // TODO: change this algorithm
+            return req.request_uri() == tpath_str;
+        }
+    };
+
+
+    struct tpath : public valve<tpath_condition> {
+        using valve<tpath_condition>::valve;
+    };
+
+    constexpr tpath operator""_tpath(const char* str, std::size_t len) {
+        return tpath{std::string_view{str, len}};
+    }
 
 } // namespace webpp::valves
 
