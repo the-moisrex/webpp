d5ac60e7c181632e57b1a467591d6cf8dbd6a6ae
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-02-23 12:02:16 +0000

    made the hello world example compile, but don't work

--- examples/01-hello-world/main.cpp
+++ examples/01-hello-world/main.cpp
@@ -1,6 +1,8 @@
 #include <webpp/router>
 #include <webpp/server>
 #include <webpp/http/request>
+#include <webpp/valves/methods>
+#include <webpp/valves/uri>
 #include <webpp/interfaces/cgi>
 #include "config.h"
 
@@ -9,29 +11,28 @@ using namespace webpp::valves;
 
 using request = request_t<cgi>;
 
-response about_page(request req) { return file("about.html"); }
+// response about_page(request req) { return file("about.html"); }
 
 int main() {
 
     server<cgi> app;
-    app.router.on(valves::get & "/home"_path, [](auto const& req, auto& res) noexcept {
+    app.router.on(get and "/cgi-bin/webpp_helloworld"_path, [](auto const& req, auto& res) noexcept {
         res << "hello world";
-        res << '\n';
     });
-    app.router.on(get and "/about"_path, about_page);
-    app.router.on("/user/{user_id}"_tpath, [](auto const& req, auto& res) {
-        auto headers = res.headers();
-        headers["User-Id"] = req.url().segments()["user_id"];
-
-        app.router.on(ends_with("/profile"_tpath),
-                      []() { return "welcome to your profile"; });
-
-        app.router.on(post and ends_with("/contact"_path),
-                      [](auto& req, auto& res) {
-                          auto jdata = req.body().json();
+    // app.router.on(get and "/about"_path, about_page);
+    // app.router.on("/user/{user_id}"_tpath, [](auto const& req, auto& res) {
+        // auto headers = res.headers();
+        // headers["User-Id"] = req.url().segments()["user_id"];
+//
+        // app.router.on(ends_with("/profile"_tpath),
+                      // []() { return "welcome to your profile"; });
+//
+        // app.router.on(post and ends_with("/contact"_path),
+                      // [](auto& req, auto& res) {
+                          // auto jdata = req.body().json();
                           // ...
-                      });
-    });
-
+                      // });
+    // });
+//
     return app.run();
 }
