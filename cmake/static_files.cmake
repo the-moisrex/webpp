5bb26c691bd548f6ae76437bd7e7daf33e6d55eb
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-02-28 15:08:08 +0000

    fix a couple of bugs in the static file generation function

--- cmake/static_files.cmake
+++ cmake/static_files.cmake
@@ -14,24 +14,33 @@ function(generate_config_file config_file)
     # read the file
     file(READ "${_file}" content)
 
+    # configure the file to replace the variables
+    #string(CONFIGURE "${_content}" content)
+
     # calculate the length of the file (in bytes)
     string(LENGTH "${content}" content_length)
     string(LENGTH "${_file}" filename_length)
 
-    # append the string to the final file
-    string(APPEND all_files "\t{std::string_view{R\"${random_prefix}(${_file})${random_prefix}\", ${filename_length}}, std::string_view{R\"${random_prefix}(${content})${random_prefix}\", ${content_length}}},\n")
-
     # append as a separate variable
     string(MAKE_C_IDENTIFIER "${_file}" var_ident)
-    string(APPEND sep_files "constexpr std::string_view file_${var_ident}{R\"${random_prefix}(${content})${random_prefix}\", ${content_length}};\n")
+    string(PREPEND var_ident "file_")
+    string(APPEND sep_files "constexpr std::string_view ${var_ident}{R\"${random_prefix}(${content})${random_prefix}\", ${content_length}};\n")
+
+    # append the string to the final file
+    string(APPEND all_files "\t{std::string_view{R\"${random_prefix}(${_file})${random_prefix}\", ${filename_length}}, ${var_ident}},\n")
+
 
   endforeach()
 
   string(APPEND all_files "\n}};\n\n")
 
-  set(file_content "#include <string_view>\n#include <array>\n\n")
-  string(APPEND file_content "${all_files}\n")
-  string(APPEND file_content "${sep_files}")
+  string(TIMESTAMP datetime UTC)
+
+  set(file_content "")
+  string(APPEND file_content "// Auto generated with cmake at: ${datetime}\n// Written by: Mohammad Bahoosh\n\n")
+  string(APPEND file_content "#include <string_view>\n#include <array>\n\n")
+  string(APPEND file_content "${sep_files}\n\n")
+  string(APPEND file_content "${all_files}")
 
   # write the file_content to the config.h.in
   file(WRITE "${config_file}" "${file_content}")
