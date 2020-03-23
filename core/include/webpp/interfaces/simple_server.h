6a424bf3aeefc9e02a8afe24075e25ba73f2e0f5
Mohammad Bahoosh <moisrex@protonmail.com>
2020-03-23 15:41:49 -0400

    simple server header added

---
+++ core/include/webpp/interfaces/simple_server.h
@@ -0,0 +1,19 @@
+#ifndef WEBPP_INTERFACE_SSERVER_H
+#define WEBPP_INTERFACE_SSERVER_H
+
+#include "basic_interface.h"
+
+namespace webpp {
+
+    class simple_server : public basic_interface<simple_server> {
+      private:
+
+      public:
+
+
+    };
+
+}
+
+
+#endif
