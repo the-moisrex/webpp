57290e79f6965a3672d9a913ce9e4fbf60161b57
Mohammad Bahoosh <moisrex@protonmail.com>
2020-03-24 00:13:12 -0400

    connection class is evolving slowly

---
+++ core/src/webpp/interfaces/sserver/server.cpp
@@ -0,0 +1,19 @@
+#include "connection.hpp"
+#include <set>
+
+namespace webpp::sserver {
+
+  /**
+   * This class is the server and the conection manager.
+   */
+  class server {
+    private:
+      std::set<connection> connections;
+
+
+    public:
+
+
+  };
+
+}
