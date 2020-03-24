ede05f1c7b9cd5da725f0f82357aef8977edfdfe
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-03-24 02:45:01 -0400

    connection::read

--- core/src/webpp/interfaces/sserver/connection.hpp
+++ core/src/webpp/interfaces/sserver/connection.hpp
@@ -1,29 +1,49 @@
 #include "constants.hpp"
+#include <memory>
+
+/**
+ * The reason that this file is here and not in the include directory is because
+ * we want to hide every boost related library from the final users of this
+ * framework. Hiding implementaions are good ways to do this.
+ */
 
 namespace webpp::sserver {
 
+    class connection : public std::enable_shared_from_this<connection> {
+      private:
+        socket_t socket;
+        std::array<char, buffer_size> buffer;
 
-  class connection {
-    private:
-      socket_t socket;
-      std::array<char, buffer_size> buffer;
+        void read() noexcept;
+        void write() noexcept;
 
-    public:
-      connection(connection const&) = delete;
-      connection& operator=(connection const&) = delete;
+      public:
+        connection(socket_t socket) noexcept : socket(std::move(socket)) {}
+        connection(connection const&) = delete;
+        connection& operator=(connection const&) = delete;
 
-      /**
-       * Start async operations
-       */
-      void start() noexcept;
+        /**
+         * Start async operations
+         */
+        void start() noexcept;
 
-      /**
-       * We're shutting down everything, kepp up!
-       */
-      void stop() noexcept;
+        /**
+         * We're shutting down everything, kepp up!
+         */
+        void stop() noexcept;
+    };
 
-  };
+    void connection::read() noexcept {
+        // we share ourselves, so the connection keeps itself alive.
+        auto self{shared_from_this()};
+        socket.async_read_some(
+            net::buffer(buffer),
+            [this, self](error_code_t const& err,
+                         std::size_t bytes_transferred) noexcept {
+                if (!err) {
+                    // we need to parse, store, read more, or write something
+                }
+            });
+    }
 
-
-
-}
+} // namespace webpp::sserver
--- core/src/webpp/interfaces/sserver/constants.hpp
+++ core/src/webpp/interfaces/sserver/constants.hpp
@@ -1,6 +1,8 @@
 
 #include <boost/asio.hpp>
 
-using socket_t = boost::asio::ip::tcp::socket;
+namespace net = boost::asio;
+using socket_t = net::ip::tcp::socket;
+using error_code_t = boost::system::error_code;
 constexpr std::size_t buffer_size = 1024 * 1024;
 
