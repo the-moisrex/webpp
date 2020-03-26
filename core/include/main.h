19cdea5d4b9f887d916068586054a7b6bfe21e83
Mohammad Bahoosh <moisrex@protonmail.com>
2020-03-26 17:22:26 -0400

    make the library runnable

---
+++ core/include/main.h
@@ -0,0 +1,16 @@
+#ifndef WEBPP_MAIN_H
+#define WEBPP_MAIN_H
+
+#ifdef __unix__
+extern "C" {
+  // details: https://polentino911.wordpress.com/2013/08/08/make-your-own-executable-shared-library-on-linux/
+#ifdef ELF_INTERPRETER_PATH
+  extern const char elf_interpreter[] __attribute__((section(".interp"))) = ELF_INTERPRETER_PATH;
+#else // ELF_INTERPRETER_PATH
+  extern const char elf_interpreter[] __attribute__((section(".interp"))) = "/lib64/ld-linux-x86-64.so.2";
+#endif // ELF_INTERPRETER_PATH
+  extern void __lib_main(void) __attribute__ ((noreturn));
+}
+#endif // __unix__
+
+#endif // WEBPP_MAIN_H
--- core/src/webpp/interfaces/communication/server.cpp
+++ core/src/webpp/interfaces/communication/server.cpp
@@ -1,33 +1,37 @@
 #include "connection.hpp"
+#include <memory>
 #include <set>
 
 namespace webpp::sserver {
 
-  /**
-   * This class is the server and the conection manager.
-   */
-  class server {
-    private:
-      std::set<connection> connections;
-      net::io_context io;
-      net::ip::tcp::acceptor acceptor;
+    /**
+     * This class is the server and the conection manager.
+     */
+    class server : public std::enable_shared_from_this<server> {
+      private:
+        std::set<connection> connections;
+        net::io_context io;
+        net::ip::tcp::acceptor acceptor;
 
-      void accept() noexcept {
-        acceptor.async_accept([] (error_code_t const& ec, socket_t socket) {
-                // Check whether the server was stopped by a signal before this completion handler had a chance to run
-                if (!acceptor.is_open()) {
-                    return;
-                }
+        void accept() noexcept {
+            auto self{shared_from_this()};
+            acceptor.async_accept(
+                [this, self](error_code_t const& ec, socket_t socket) {
+                    // Check whether the server was stopped by a signal before
+                    // this completion handler had a chance to run
+                    if (!acceptor.is_open()) {
+                        return;
+                    }
 
-                if (!ec) {
-                    connections.emplace_back(std::move(socket));
-                }
-            });
-      }
+                    if (!ec) {
+                        connections.emplace(std::move(socket));
+                    }
 
-    public:
+                    accept();
+                });
+        }
 
+      public:
+    };
 
-  };
-
-}
+} // namespace webpp::sserver
