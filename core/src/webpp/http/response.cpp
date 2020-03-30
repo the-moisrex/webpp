b429c74b3938f00ad3f5c697fefeaa2fc5603321
Mohammad Bahoosh <moisrex@protonmail.com>
2020-03-30 19:49:01 -0400

    going for C++17

--- core/src/webpp/http/response.cpp
+++ core/src/webpp/http/response.cpp
@@ -1,7 +1,6 @@
 #include "../../../include/webpp/http/response.h"
 #include <fstream>
 
-
 using namespace webpp;
 
 response& response::operator<<(std::string_view str) noexcept {
@@ -10,11 +9,11 @@ response& response::operator<<(std::string_view str) noexcept {
 }
 
 void response::calculate_default_headers() noexcept {
-    // It's C++20
-    if (!header.contains("Content-Type"))
+    // todo: use C++20 header.contains instead when possible
+    if (header.find("Content-Type") == header.cend())
         header.emplace("Content-Type", "text/html; charset=utf-8");
 
-    if (!header.contains("Content-Length"))
+    if (header.find("Content-Length") == header.cend())
         header.emplace("Content-Length",
                        std::to_string(body.str().size() * sizeof(char)));
 }
@@ -66,39 +65,40 @@ response::operator std::string() const noexcept {
     return std::string(body.str());
 }
 
-
 #ifdef CONFIG_FILE
-#  if CONFIG_FILE != ""
-#    include CONFIG_FILE
-#  else
-     extern std::string_view get_static_file(std::string_view const&) noexcept;
-#  endif
+#if CONFIG_FILE != ""
+#include CONFIG_FILE
+#else
+extern std::string_view get_static_file(std::string_view const&) noexcept;
+#endif
 #endif
 
 response response::file(std::filesystem::path const& _file) noexcept {
-  response res;
+    response res;
 #ifdef CONFIG_FILE
-  if (auto content = ::get_static_file(filepath); !content.empty()) {
-    res.body.replace_string(content);
-    return res;
-  }
+    if (auto content = ::get_static_file(filepath); !content.empty()) {
+        res.body.replace_string(content);
+        return res;
+    }
 #endif
 
-  // TODO: performance tests
-  // TODO: change the replace_string with replace_string_view if the file is cached
+    // TODO: performance tests
+    // TODO: change the replace_string with replace_string_view if the file is
+    // cached
 
-  if (std::ifstream in{_file.c_str(), std::ios::binary | std::ios::ate}) {
-    // details on this matter: https://stackoverflow.com/questions/11563963/writing-a-binary-file-in-c-very-fast/39097696#39097696
-    // std::unique_ptr<char[]> buffer{new char[buffer_size]};
-    // in.rdbuf()->pubsetbuf(buffer.get(), buffer_size); // speed boost, I think
-    auto size = in.tellg();
-    std::unique_ptr<char[]> result(new char[size]);
-    in.seekg(0);
-    in.read(result.get(), size);
-    res.body.replace_string(std::string{result.get(), static_cast<std::string_view::size_type>(size)});
-  }
+    if (std::ifstream in{_file.c_str(), std::ios::binary | std::ios::ate}) {
+        // details on this matter:
+        // https://stackoverflow.com/questions/11563963/writing-a-binary-file-in-c-very-fast/39097696#39097696
+        // std::unique_ptr<char[]> buffer{new char[buffer_size]};
+        // in.rdbuf()->pubsetbuf(buffer.get(), buffer_size); // speed boost, I
+        // think
+        auto size = in.tellg();
+        std::unique_ptr<char[]> result(new char[size]);
+        in.seekg(0);
+        in.read(result.get(), size);
+        res.body.replace_string(std::string{
+            result.get(), static_cast<std::string_view::size_type>(size)});
+    }
 
-  return res;
+    return res;
 }
-
-
