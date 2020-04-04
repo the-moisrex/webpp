f25e96bde856c6aa9047291b2344e7f4912bdfdb
Mohammad Bahoosh <moisrex@protonmail.com>
2020-04-04 11:44:13 -0400

    net-ts files added to std directory

--- core/include/webpp/std/buffer.h
+++ core/include/webpp/std/buffer.h
@@ -4,19 +4,18 @@
 // currently experimental version of internet is buggy. Hopefully that will be
 // fixed later.
 
-#if __has_include(<internet>)
-#include <internet>
-#elif __has_include(<boost/asio/ts/internet.hpp>)
-#include <boost/asio/ts/internet.hpp>
+#if __has_include(<buffer>)
+#include <buffer>
+#elif __has_include(<boost/asio/ts/buffer.hpp>)
+#include <boost/asio/ts/buffer.hpp>
 namespace std {
     namespace net = boost::asio;
 }
-#elif __has_include(<experimental/internet>)
-#include <experimental/internet>
+#elif __has_include(<experimental/buffer>)
+#include <experimental/buffer>
 namespace std {
     namespace net = experimental::net;
 }
 #else
 #error "We don't even have access to networking TS."
 #endif
-
--- core/include/webpp/std/socket.h
+++ core/include/webpp/std/socket.h
@@ -1,22 +1,21 @@
 // Created by moisrex on 4/4/20.
 
 // we rather use boost::asio instead of experimental version of std because
-// currently experimental version of internet is buggy. Hopefully that will be
+// currently experimental version of socket is buggy. Hopefully that will be
 // fixed later.
 
-#if __has_include(<internet>)
-#include <internet>
-#elif __has_include(<boost/asio/ts/internet.hpp>)
-#include <boost/asio/ts/internet.hpp>
+#if __has_include(<socket>)
+#include <socket>
+#elif __has_include(<boost/asio/ts/socket.hpp>)
+#include <boost/asio/ts/socket.hpp>
 namespace std {
     namespace net = boost::asio;
 }
-#elif __has_include(<experimental/internet>)
-#include <experimental/internet>
+#elif __has_include(<experimental/socket>)
+#include <experimental/socket>
 namespace std {
     namespace net = experimental::net;
 }
 #else
 #error "We don't even have access to networking TS."
 #endif
-
