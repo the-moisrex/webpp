371caece12b239356df76c9ec7e8d0b4a9940e84
Mohammad Bahoosh <moisrex@protonmail.com>
2020-04-04 11:25:59 -0400

    creating std directory
     for stuff that will be added in the future of std library and we can implement and use it today.

---
+++ core/include/webpp/std/internet.h
@@ -0,0 +1,26 @@
+// Created by moisrex on 4/4/20.
+
+#ifndef WEBPP_INTERNET_H
+#define WEBPP_INTERNET_H
+
+// we rather use boost::asio instead of experimental version of std because
+// currently experimental version of internet is buggy. Hopefully that will be
+// fixed later.
+
+#if __has_include(<internet>)
+#include <internet>
+#elif __has_include(<boost/asio/ts/internet.hpp>)
+#include <boost/asio/ts/internet.hpp>
+namespace std {
+    namespace net = boost::asio;
+}
+#elif __has_include(<experimental/internet>)
+#include <experimental/internet>
+namespace std {
+    namespace net = experimental::net;
+}
+#else
+#error "We don't even have access to networking TS."
+#endif
+
+#endif // WEBPP_INTERNET_H
