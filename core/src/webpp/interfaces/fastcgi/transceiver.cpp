5106abbadd853bc1e2c37c89b1aec44acbcdc266
Mohammad Bahoosh <moisrex@protonmail.com>
2020-04-03 20:05:48 -0400

    trying to remove boost

--- core/src/webpp/interfaces/fastcgi/transceiver.cpp
+++ core/src/webpp/interfaces/fastcgi/transceiver.cpp
@@ -1,1 +1,5 @@
-#include "../../../../include/webpp/interfaces/fastcgi/transceiver.h"
+
+// todo: remove these if we don't need it in this file. but make sure they're included somewhere.
+#include "../common/server.hpp"
+#include "../common/constants.hpp"
+#include "../common/connection.hpp"
