7b346b758f725e00d7674b360ba6523c4b0585fa
Mohammad Bahoosh <moisrex@protonmail.com>
2020-04-05 17:43:17 -0400

    put it in a while

--- core/include/webpp/std/std.h
+++ core/include/webpp/std/std.h
@@ -9,5 +9,6 @@
 #define STLLIB_EXPERIMENTAL 1
 #define STLLIB_BOOST 2
 #define STLLIB_NETWORKING_ERROR "We don't even have access to networking TS."
+#define BOOST_ASIO_NO_DEPRECATED
 
 #endif // WEBPP_STD_STD_H
