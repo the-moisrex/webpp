964576b539a5a797c5c2f009c8e986d2694b4713
Mohammad Bahoosh <moisrex@protonmail.com>
2020-04-05 10:54:55 -0400

    ipv6 benchmark added

--- benchmarks/utils_address.cpp
+++ benchmarks/utils_address.cpp
@@ -59,6 +59,65 @@ BENCHMARK(WebppV4Random);
 
 
 
+///////////////////// IPv6 ///////////////////////////
+
+
+
+static void AsioV6(benchmark::State& state) {
+  for (auto _ : state) {
+    auto addr = ip::make_address_v6("::1");
+    benchmark::DoNotOptimize(addr);
+  }
+}
+BENCHMARK(AsioV6);
+
+static void WebppV6(benchmark::State& state) {
+  for (auto _ : state) {
+    auto addr = webpp::ipv6("::1");
+    benchmark::DoNotOptimize(addr);
+  }
+}
+BENCHMARK(WebppV6);
+
+
+/////////////////// IPv6 Random ///////////////////////
+
+auto ipv6_rands() {
+  vector<string> ipv6_rand;
+  for (int i = 0 ; i < 10; i++) {
+    string str = "2a00:1450::200";
+    str.append(to_string(i));
+    ipv6_rand.push_back(std::move(str));
+  }
+  return ipv6_rand;
+}
+auto ipv6_data() {
+  static auto data = ipv6_rands();
+  static auto it = data.begin();
+  ++it;
+  if (it == data.end())
+    it = data.begin();
+  return *it;
+}
+
+static void AsioV6Random(benchmark::State& state) {
+  ipv6_data();
+  for (auto _ : state) {
+    auto addr = ip::make_address_v6(ipv6_data());
+    benchmark::DoNotOptimize(addr);
+  }
+}
+BENCHMARK(AsioV6Random);
+
+static void WebppV6Random(benchmark::State& state) {
+  ipv6_data();
+  for (auto _ : state) {
+    auto addr = webpp::ipv6(ipv6_data());
+    benchmark::DoNotOptimize(addr);
+  }
+}
+BENCHMARK(WebppV6Random);
+
 
 
 
