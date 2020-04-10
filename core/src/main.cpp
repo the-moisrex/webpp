44e79c524d14ac65e976e95ad294a7674f18c073
Mohammad Bahoosh <moisrex@protonmail.com>
2020-04-10 00:21:53 -0400

    comments added

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -4,6 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * This function is used for printing information about the project
+ * when running the dynamic library.
+ *
+ * Even though we could do some really amazing stuff here but we're not
+ * gonna get very clever about it just yet because we don't want to
+ * make the library heavy.
+ */
 extern "C" void __lib_main(void) {
   printf("web++ framework in the flesh.\n");
   exit(0);
