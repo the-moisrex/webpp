cecaccf97e4aafe96b2928f4be0bb3cc477abc03
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-04-19 13:19:27 -0400

    trying to implement tpath but I think I can do some of it in the compile time
    that's why I'm gonna change the charset.h to be more advance
    probabely going to change the charset name to fixed_string
    it's going to be based on the CTRE library

--- core/include/webpp/valves/uri.h
+++ core/include/webpp/valves/uri.h
@@ -1,8 +1,10 @@
 #ifndef WEBPP_VALVE_URI_H
 #define WEBPP_VALVE_URI_H
 
+#include "../../../include/webpp/common/meta.h"
 #include "../../../include/webpp/utils/uri.h"
 #include "valve.h"
+#include <cassert>
 #include <cstddef> // for std::size_t
 #include <string_view>
 
@@ -41,8 +43,89 @@ namespace webpp::valves {
      * @param _path
      * @return
      */
-    [[nodiscard]] bool tpath_check(std::string_view const& templated_path,
-                                   std::string_view const& _path) noexcept;
+    template <typename CharT>
+    [[nodiscard]] bool
+    tpath_check(std::basic_string_view<CharT> const& templated_path,
+                std::basic_string_view<CharT> const& _path) noexcept {
+
+        auto tit = templated_path.data(); // templated iterator
+        auto pit = _path.data();          // path iterator
+        decltype(tit) seg_start = nullptr;
+        std::size_t seg_starts = 0;
+        decltype(pit) pseg_start = nullptr;
+        for (;;) {
+            switch (*tit) {
+            case '\0':
+
+                // error: there's an open segment rule and we can't just leave
+                // it unclosed. I should throw an error in the user's face but
+                // I'm nice and I'm just gonna assert
+                assert(("the specified templated path is not valid.",
+                        seg_start == nullptr));
+                if (seg_start) {
+                    return false;
+                }
+                break;
+            case '{': // might be start of a segment
+
+                if (seg_start) {
+                    // the { char is part of the segment rule here
+                    ++seg_starts;
+                    break;
+                }
+
+                // pinpoint the segment start for when we reach the end of it
+                seg_start = tit + 1;
+                pseg_start = pit;
+                break;
+            case '}': { // might be the end of a segment
+
+                // a new } char with no starting { char
+                assert(("The specified templated path is not valid.",
+                        seg_starts != 0));
+                if (seg_starts == 0)
+                    return false;
+
+                --seg_starts;
+                if (seg_starts != 0)
+                    break; // this } char is part of the segment rule itself
+
+#if CXX20
+                std::basic_string_view<CharT> seg_rule(seg_start, tit);
+#elif CXX17
+                std::basic_string_view<CharT> seg_rule(seg_start,
+                                                       tit - seg_start);
+#endif
+
+                // going to find the segment in the path string too
+                for (auto it = pit;; ++it) {
+                    switch (*it) {
+                    case '\0':
+                        goto after_loop;
+                    case '/':
+                    }
+                }
+            after_loop:
+                parse_seg(seg_rule, );
+            } break;
+            case '\\': // escape character
+                // we just don't care what the next char is. even if it's { or }
+                // which they have meanings in this algorithm.
+                ++tit;
+                if (*tit != '}' && *tit != '{')
+                    --tit;
+                break;
+            default:
+                // it's not an error, it's normal to return false here.
+                if (!seg_start && *tit != *pit)
+                    return false;
+            }
+
+            ++tit;
+            pit += seg_start != nullptr; // only add 1 if we're not in a segment
+        }
+        return false;
+    }
 
     struct tpath_condition {
       protected:
@@ -55,8 +138,8 @@ namespace webpp::valves {
         constexpr tpath_condition() noexcept = default;
 
         template <typename RequestType>
-        [[nodiscard]] inline bool operator()(RequestType const& req) const
-            noexcept {
+        [[nodiscard]] inline bool
+        operator()(RequestType const& req) const noexcept {
             return tpath_check(tpath_str, req.request_uri());
         }
     };
