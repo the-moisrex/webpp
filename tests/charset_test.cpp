2b71c40faa4c984773f6debd5b290880695224a1
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-04-20 14:05:23 -0400

    charset_t 2.0
    It's not the best thing, but it work (hopefully)!

--- tests/charset_test.cpp
+++ tests/charset_test.cpp
@@ -5,7 +5,7 @@
 using namespace webpp;
 
 TEST(CharsetTest, CharsetCreation) {
-    constexpr auto chars = charset<'a', 'z'>();
+    constexpr auto chars = charset<char, 'a', 'z'>();
     EXPECT_TRUE(chars.contains('a')) << "contains 'a'";
     EXPECT_TRUE(chars.contains('b')) << "contains 'b'";
     EXPECT_TRUE(chars.contains('z')) << "contains 'z'";
@@ -14,5 +14,6 @@ TEST(CharsetTest, CharsetCreation) {
     EXPECT_FALSE(chars.contains('&')) << "shouldn't contain '&'";
     EXPECT_FALSE(chars.contains('\0')) << "shouldn't contain '\\0'";
 
-    EXPECT_EQ(5, charset('1', '2', '3', '4', '5').size());
+    charset_t chars2{'1', '2', '3', '4', '5'};
+    EXPECT_EQ(5, chars2.size());
 }
