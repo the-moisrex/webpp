ecc2586de623aab6a6ded0757d6e67bf015ed907
Mohammad Bahoosh <moisrex@protonmail.com>
2020-04-21 08:52:33 -0400

    uri is now able to support character types other than char

--- core/src/webpp/utils/uri.cpp
+++ core/src/webpp/utils/uri.cpp
@@ -2,11 +2,3 @@
 #include "../../../include/webpp/utils/uri.h"
 
 #include <webpp/valves/uri.h>
-
-bool webpp::operator==(uri const& one, const_uri const& two) noexcept {
-    return one.operator==(two.str());
-}
-
-bool webpp::operator==(const_uri const& one, uri const& two) noexcept {
-    return one.operator==(two.str());
-}
