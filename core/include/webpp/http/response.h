98935c786ca8b2e0036a63854cecab8e437110f6
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-05-08 09:17:25 -0400

    Trying to make the syntax highlighting work

--- core/include/webpp/http/response.h
+++ core/include/webpp/http/response.h
@@ -89,45 +89,45 @@ namespace webpp {
     };
 
 
+    /*
 
+    #ifdef CONFIG_FILE
+    #    if CONFIG_FILE != ""
+    #        include CONFIG_FILE
+    #    else
+        extern std::string_view get_static_file(std::string_view const&)
+    noexcept; #    endif #endif
 
-#ifdef CONFIG_FILE
-#    if CONFIG_FILE != ""
-#        include CONFIG_FILE
-#    else
-    extern std::string_view get_static_file(std::string_view const&) noexcept;
-#    endif
-#endif
+        response_t response_t::file(std::filesystem::path const& _file) noexcept
+    { response_t res; #ifdef CONFIG_FILE if (auto content =
+    ::get_static_file(filepath); !content.empty()) {
+                res.body.replace_string(content);
+                return res;
+            }
+    #endif
 
-    response_t response_t::file(std::filesystem::path const& _file) noexcept {
-        response_t res;
-#ifdef CONFIG_FILE
-        if (auto content = ::get_static_file(filepath); !content.empty()) {
-            res.body.replace_string(content);
+            // TODO: performance tests
+            // TODO: change the replace_string with replace_string_view if the file is cached
+
+            if (std::ifstream in{_file.c_str(), std::ios::binary |
+    std::ios::ate}) {
+                // details on this matter:
+                // https://stackoverflow.com/questions/11563963/writing-a-binary-file-in-c-very-fast/39097696#39097696
+                // std::unique_ptr<char[]> buffer{new char[buffer_size]};
+                // in.rdbuf()->pubsetbuf(buffer.get(), buffer_size); // speed boost,
+                // I think
+                auto                    size = in.tellg();
+                std::unique_ptr<char[]> result(new char[size]);
+                in.seekg(0);
+                in.read(result.get(), size);
+                res.body.replace_string(std::string{
+                  result.get(),
+    static_cast<std::string_view::size_type>(size)});
+            }
+
             return res;
         }
-#endif
-
-        // TODO: performance tests
-        // TODO: change the replace_string with replace_string_view if the file is cached
-
-        if (std::ifstream in{_file.c_str(), std::ios::binary | std::ios::ate}) {
-            // details on this matter:
-            // https://stackoverflow.com/questions/11563963/writing-a-binary-file-in-c-very-fast/39097696#39097696
-            // std::unique_ptr<char[]> buffer{new char[buffer_size]};
-            // in.rdbuf()->pubsetbuf(buffer.get(), buffer_size); // speed boost,
-            // I think
-            auto                    size = in.tellg();
-            std::unique_ptr<char[]> result(new char[size]);
-            in.seekg(0);
-            in.read(result.get(), size);
-            res.body.replace_string(std::string{
-              result.get(), static_cast<std::string_view::size_type>(size)});
-        }
-
-        return res;
-    }
-
+    */
     template <typename ResponseType, typename = void>
     struct is_response : ::std::false_type {};
 
