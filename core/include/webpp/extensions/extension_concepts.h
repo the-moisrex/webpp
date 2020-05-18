875150ee9f7c0e46edc90a382cfc06bca4d89950
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-05-18 03:58:28 -0400

    Extensions concepts added

--- core/include/webpp/extensions/extension_concepts.h
+++ core/include/webpp/extensions/extension_concepts.h
@@ -3,4 +3,33 @@
 #ifndef WEBPP_EXTENSION_CONCEPTS_H
 #define WEBPP_EXTENSION_CONCEPTS_H
 
+#include "../std/tuple.h"
+
+#include <type_traits>
+
+namespace webpp {
+
+    template <typename T>
+    using is_extension = std::conjunction<
+      ::std::is_default_constructible<T>, ::std::is_move_constructible<T>,
+      ::std::is_move_assignable<T>, ::std::is_copy_assignable<T>>;
+
+    template <typename T>
+    concept Extension = is_extension<T>::value;
+
+    template <template <typename...> typename E, typename... T>
+    concept ExtensionList = stl::TupleOf<is_extension, E, T...> || requires {
+        (Extension<T> && ...);
+    };
+
+
+    template <typename T>
+    concept ExtensionWithDependencies = requires {
+        Extension<T>;
+        typename T::required_extensions;
+        Extension<typename T::required_extensions>;
+    };
+
+} // namespace webpp
+
 #endif // WEBPP_EXTENSION_CONCEPTS_H
