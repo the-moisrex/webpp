9fdb71082c057c613c93e4ae5fcc0a56aa593f83
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-05-26 07:20:59 -1000

    application concept updated

--- core/include/webpp/http/application_concepts.h
+++ core/include/webpp/http/application_concepts.h
@@ -4,12 +4,19 @@
 #define WEBPP_APPLICATION_CONCEPTS_H
 
 #include "../std/concepts.h"
+#include "./request_concepts.h"
+#include "./response_concepts.h"
+#include "./routes/context.h"
 
 namespace webpp {
 
     template <typename T>
-    concept Application = ::std::default_initializable<T>;
+    concept Application =
+      ::std::default_initializable<T>&& requires(T app, basic_context<> ctx) {
+        { app(ctx) }
+        ->Response;
+    };
 
-}
+} // namespace webpp
 
 #endif // WEBPP_APPLICATION_CONCEPTS_H
