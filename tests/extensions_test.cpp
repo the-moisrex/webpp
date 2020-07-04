d83eb12c9aff7f532a7cee6d3e626fd93998412b
Mohammad Bahoosh <moisrex@protonmail.com>
2020-07-04 02:26:31 -1000

    Fixed another series of bugs in the extension system

--- tests/extensions_test.cpp
+++ tests/extensions_test.cpp
@@ -14,25 +14,28 @@ TEST(ExtensionsTests, ExtensionConcepts) {
 }
 
 struct one {
-    static constexpr bool item = true;
+    static constexpr bool item     = true;
+    static constexpr bool item_one = true;
     template <typename tt>
     struct type {
-        bool one = true;
+        static constexpr bool value_one = true;
     };
 };
 struct two {
-    static constexpr bool item = true;
+    static constexpr bool item     = true;
+    static constexpr bool item_two = true;
     template <typename tt>
     struct type {
-        bool two = true;
+        static constexpr bool value_two = true;
     };
 };
 struct three {
-    static constexpr bool item = true;
+    static constexpr bool item       = true;
+    static constexpr bool item_three = true;
 
     template <typename tt>
     struct type {
-        bool three = true;
+        static constexpr bool value_three = true;
     };
 };
 
@@ -58,17 +61,18 @@ struct fake_descriptor {
 
     template <typename ExtensionListType, typename TraitsType, typename EList>
     struct mid_level_extensie_type : public EList {
-        bool mid_level = true;
+        static constexpr bool mid_level = true;
     };
 
     template <typename ExtensionListType, typename TraitsType, typename EList>
     struct final_extensie_type : public EList {
-        bool final_level = true;
+        static constexpr bool final_level = true;
     };
 };
 
 TEST(ExtensionsTests, ExtensionPackStuff) {
-    using pack = extension_pack<one, two, three>;
+    using pack   = extension_pack<one, two, three>;
+    using expack = extension_pack<exes>;
     EXPECT_TRUE(pack::template is_all<has_item>::value);
 
     static_assert(stl::same_as<typename pack::mother_extensions, pack>,
@@ -84,7 +88,13 @@ TEST(ExtensionsTests, ExtensionPackStuff) {
       stl::same_as<typename extension_pack<exes, exes, exes>::merge_extensions<fake_descriptor>, pack>,
       "epack is failing at making the extensions unique");
 
-    using etype = typename extension_pack<exes>::template extensie_type<std_traits, fake_descriptor>;
+
+    using iexpack = typename expack::template merge_extensions<
+      fake_descriptor>::mother_extensions::template mother_inherited<std_traits>;
+    auto ii = iexpack{};
+    EXPECT_TRUE(ii.value_one && ii.value_two && ii.value_three) << "Inherited is not working";
+
+    using etype = typename expack::template extensie_type<std_traits, fake_descriptor>;
     etype e{};
     EXPECT_TRUE(e.mid_level);
     EXPECT_TRUE(e.final_level);
