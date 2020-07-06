170fc68b7caaeb8560220d6a6900a6319c6b856e
Mohammad Bahoosh <moisrex@protonmail.com>
2020-07-06 10:31:53 -1000

    Fix some cookie bugs

--- tests/cookies_test.cpp
+++ tests/cookies_test.cpp
@@ -14,11 +14,9 @@ TEST(Cookie, ResponseCookiesCreation) {
     c.name("   test   ").value("  value  ");
     EXPECT_EQ(c.name(), "test");
     EXPECT_EQ(c.value(), "value");
-    EXPECT_EQ(res_cookie_t("  test  ", "  value ").name(),
-              res_cookie_t("test", "value").name())
+    EXPECT_EQ(res_cookie_t("  test  ", "  value ").name(), res_cookie_t("test", "value").name())
       << "cookies should be trimmed";
-    EXPECT_EQ(res_cookie_t("  test  ", "  value  ").name(),
-              res_cookie_t().name("  test  ").name())
+    EXPECT_EQ(res_cookie_t("  test  ", "  value  ").name(), res_cookie_t().name("  test  ").name())
       << "name should trim it too.";
 }
 
@@ -46,18 +44,18 @@ TEST(ResponseCookies, CookieJar) {
     jar.emplace_back("one", "value");
     jar.emplace_back(" one ", "value 2"); // this should replace the other one
     jar.emplace_back("two", "value");
+    jar.remove_duplicates();
 
-    EXPECT_TRUE(jar.size() == 2)
-      << "cookies with the same name should be replaced with the older ones";
+    EXPECT_EQ(jar.size(), 2) << "cookies with the same name should be replaced with the older ones";
 
     res_cookie_jar_t jar2;
     jar2.emplace_back("one", "value 1");
     jar2.emplace_back("two", "value 2");
     jar2.emplace_back(" one ",
                       "value 1-2"); // this should replace the other one
+    jar2.remove_duplicates();
 
-    EXPECT_TRUE(jar2.size() == 2)
-      << "The order that cookies get added to cookie jar does not matter";
+    EXPECT_EQ(jar2.size(), 2) << "The order that cookies get added to cookie jar does not matter";
 
     jar2.emplace_back("two", "value 2-2");
 
@@ -76,13 +74,13 @@ TEST(ResponseCookies, CookieJar) {
     jar2.push_back(res_cookie_t("one", "value 1-8"));
     jar2.push_back(res_cookie_t("two", "value 2-4"));
 
-    EXPECT_TRUE(jar2.size() == 2)
-      << "Cookie jar should have the same size when we're emplacing a cookie "
-         "with the same name";
+    jar2.remove_duplicates();
+    EXPECT_EQ(jar2.size(), 2) << "Cookie jar should have the same size when we're emplacing a cookie "
+                                 "with the same name";
 
     auto found = jar2.find("two");
-    EXPECT_TRUE(found->name() == "two");
-    EXPECT_TRUE(found->value() == "value 2-4")
+    EXPECT_EQ(found->name(), "two");
+    EXPECT_EQ(found->value(), "value 2-4")
       << "The value we found is not the same as the value it should have "
          "(found: "
       << found->value() << "; expected: "
@@ -92,9 +90,8 @@ TEST(ResponseCookies, CookieJar) {
     for (auto const& a : jar2) {
         auto found = jar2.find(a.name());
         EXPECT_TRUE(found->value() == a.value())
-          << "Checking all the values in the cookie jar (cookie name: "
-          << a.name() << "=" << a.value() << "; found: " << found->name() << "="
-          << found->value() << ")";
+          << "Checking all the values in the cookie jar (cookie name: " << a.name() << "=" << a.value()
+          << "; found: " << found->name() << "=" << found->value() << ")";
     }
 }
 
@@ -102,41 +99,34 @@ TEST(ResponseCookies, CookieJarUniqeness) {
     res_cookie_jar_t cs;
     cs.push_back(res_cookie_t().name("one").value("test").domain("google.com"));
     cs.push_back(res_cookie_t().name("one").value("test").domain("bing.com"));
+    cs.remove_duplicates();
 
-    EXPECT_TRUE(cs.size() == 2)
-      << "Different domains should not be considered the same";
+    EXPECT_TRUE(cs.size() == 2) << "Different domains should not be considered the same";
 
     cs.push_back(res_cookie_t().name("one").value("test").domain("google.com"));
     cs.push_back(res_cookie_t().name("one").value("test").domain("bing.com"));
+    cs.remove_duplicates();
 
-    EXPECT_TRUE(cs.size() == 2)
-      << "Inserting already push_backed cookies that are 'same_as' the other "
-         "one should be ignored";
+    EXPECT_EQ(cs.size(), 2) << "Inserting already push_backed cookies that are 'same_as' the other "
+                               "one should be ignored";
 
     // now we check if changing the name, path, or domain to a new value that
     // already exists will remove the value or not
-    cs.push_back(res_cookie_t()
-                   .name("two")
-                   .value("test")
-                   .domain("bing.com")
-                   .comment("hello"));
+    cs.push_back(res_cookie_t().name("two").value("test").domain("bing.com").comment("hello"));
     EXPECT_EQ(cs.size(), 3);
     EXPECT_EQ(cs.find("two")->comment(), "hello");
     cs.name("two", "one");
+    cs.remove_duplicates();
 
-    EXPECT_EQ(cs.size(), 2)
-      << "One of the cookies should now be removed so the whole cookie jar "
-         "have unique cookies";
+    EXPECT_EQ(cs.size(), 2) << "One of the cookies should now be removed so the whole cookie jar "
+                               "have unique cookies";
     EXPECT_EQ(cs.find("one")->comment(), "hello")
       << "The old cookie should be removed instead of the new one. The new "
          "cookie should be replace the old one while renaming.";
 
-    cs.push_back(res_cookie_t()
-                   .name("one")
-                   .value("test")
-                   .domain("duckduckgo.com")
-                   .comment("hello"));
-    auto p = std::find_if(cs.begin(), cs.end(), [](auto const& cookie) {
+    cs.push_back(res_cookie_t().name("one").value("test").domain("duckduckgo.com").comment("hello"));
+    cs.remove_duplicates();
+    auto p = std::find_if(cs.rbegin(), cs.rend(), [](auto const& cookie) {
         return cookie.name() == "one";
     });
     EXPECT_EQ(cs.size(), 3);
@@ -144,9 +134,8 @@ TEST(ResponseCookies, CookieJarUniqeness) {
     cs.domain(p, "google.com");
     EXPECT_EQ(p->domain(), "google.com");
 
-    EXPECT_EQ(cs.size(), 2)
-      << "One of the cookies should now be removed so the whole cookie jar "
-         "have unique cookies";
+    EXPECT_EQ(cs.size(), 2) << "One of the cookies should now be removed so the whole cookie jar "
+                               "have unique cookies";
     EXPECT_EQ(cs.find("one")->comment(), "hello")
       << "The old cookie should be removed instead of the new one. The new "
          "cookie should be replace the old one in the changing the domain "
