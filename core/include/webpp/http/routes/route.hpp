512e3153ff9533fe8c96445134279b8c5cec0675
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-08-03 06:54:25 -1000

    Taking the madness to the next level

--- core/include/webpp/http/routes/route.hpp
+++ core/include/webpp/http/routes/route.hpp
@@ -152,7 +152,7 @@ namespace webpp {
         enum class logical_operators { none, AND, OR, XOR };
 
         template <typename RouteType, logical_operators Op, typename NextRoute>
-        struct basic_route : public make_inheritable<RouteType> {
+        struct basic_route : protected make_inheritable<RouteType> {
             using next_route_type = NextRoute;
             using super_t         = make_inheritable<RouteType>;
 
@@ -183,10 +183,11 @@ namespace webpp {
             consteval basic_route(basic_route&& v) noexcept      = default;
 
             using super_t::operator=;
+            using super_t::operator();
         };
 
         template <typename RouteType>
-        struct basic_route<RouteType, logical_operators::none, void> : public make_inheritable<RouteType> {
+        struct basic_route<RouteType, logical_operators::none, void> : protected make_inheritable<RouteType> {
             using super_t = make_inheritable<RouteType>;
 
             template <typename... Args>
@@ -196,6 +197,7 @@ namespace webpp {
             consteval basic_route(basic_route&&) noexcept      = default;
 
             using super_t::operator=;
+            using super_t::operator();
         };
 
         template <>
@@ -313,6 +315,25 @@ namespace webpp {
                 }
             }
 
+            //            template <logical_operators TheOp, typename NewRouteType>
+            //            [[nodiscard]] consteval auto set_next_bool_op(NewRouteType&& new_route) const
+            //            noexcept {
+            //                if constexpr (stl::is_void_v<route_type>) {
+            //
+            //                    if constexpr (stl::is_void_v<next_route_type>) {
+            //                        return route<NewRouteType, TheOp, void>(
+            //                          stl::forward<NewRouteType>(new_route));
+            //                    } else {
+            //                        return route<NewRouteType, TheOp, next_route_type>(
+            //                          stl::forward<NewRouteType>(new_route), super_t::next);
+            //                    }
+            //                } else {
+            //                    route<next_route_type, TheOp, NewRouteType> n{super_t::next,
+            //                                                                  stl::forward<NewRouteType>(new_route)};
+            //                    return route<route_type, op, decltype(n)>{*this, n};
+            //                }
+            //            }
+            //
           public:
             [[nodiscard]] consteval auto operator&&(PotentialRoute auto&& new_route) const noexcept {
                 return set_next<logical_operators::AND>(stl::forward<decltype(new_route)>(new_route));
@@ -424,7 +445,6 @@ namespace webpp {
                         } else if constexpr (Response<n_res_t>) {
 
                         } else if constexpr (Context<n_res_t>) {
-
                         }
                     } else if constexpr (logical_operators::AND == op) {
                         // don't rely on operator && for not executing the next route, because the user may
@@ -488,28 +508,98 @@ namespace webpp {
                 }
             }
 
-            [[nodiscard]] inline bool call_next_route_in_bool(Context auto&&      ctx,
-                                                              Request auto const& req) const noexcept {
-                using context_type = stl::remove_cvref_t<decltype(ctx)>;
-                auto res           = call_next_route(ctx, req);
-                using res_t        = stl::remove_cvref_t<decltype(res)>;
-                if constexpr (stl::same_as<res_t, bool>) {
-                    return res;
-                } else if constexpr (stl::is_void_v<res_t>) {
-                    return false;
-                } else {
-                    // even if it's a response, we're going to treat it as a bool and just ignore the result
-                    // that's sounds harsh but a developer that's using "and", "or", "xor" in it's router and
-                    // returns a non-bool result is probably drunk.
-                    if constexpr (Logger<context_type>) {
-                        ctx.warning("This sub-route should return a boolean "
-                                    "because you've used it in a 'and', 'or', or 'xor' operation.");
+          public:
+
+            /**
+             * This member function will run any route that returns bool and checks their results to see if
+             * the routes in this route-chain can be run (the rest of the non-bool routes).
+             *
+             * The operator() is guaranteed not to run those routes that return bool so you can first check
+             * this function, then if it returns true, you can call the route.
+             */
+            template <typename ContextType>
+            requires(Context<stl::remove_cvref_t<ContextType>>)
+              [[nodiscard]] constexpr bool matches(ContextType&&       ctx,
+                                                   Request auto const& req) const noexcept {
+                using context_type  = decltype(ctx);
+                using request_type  = decltype(req);
+                using ret_type      = ;
+                using next_ret_type = ;
+
+                constexpr auto is_next_route_match = [this](auto&& ctx,
+                                                        auto const& req) -> bool {
+                    if constexpr (requires(next_route_type _route) {
+                                      {_route.matches(stl::forward<decltype(ctx)>(ctx), req)};
+                                  }) {
+                        // if it's a route itself (nexted route), it'll use that matches member function
+                        return super_t::next.matches(stl::forward<decltype(ctx)>(ctx), req);
+                    } else if constexpr (stl::same_as<next_ret_type, bool>) {
+                        // call the route because it returns a bool, this route won't be run again in the operator()
+                        return call_next_route(stl::forward<decltype(ctx)>(ctx), req);
+                    } else { // Request, ConvertibleToRequest, Entry-Route level Context Switching, ...
+                        // there's not going to be a Sub-Route level Context Switching here, because if there's one,
+                        // it's a "route" and thus has a ".matches" member function.
+                        return true;
                     }
-                    return true;
+                };
+
+                if constexpr (!is_route_valid) {
+                    if constexpr (!is_next_route_valid) {
+                        return false;
+                    } else {
+                        return is_next_route_match(super_t::next, stl::forward<decltype(ctx)>(ctx), req);
+                    }
+                } else if constexpr (requires(route_type _route) {
+                        {_route.matches(stl::forward<decltype(ctx)>(ctx), req)};
+                    }) {
+                    // It's a nested route
+                    auto res = super_t::matches(stl::forward<decltype(ctx)>(ctx), req);
+                    if constexpr (logical_operators::none == op || logical_operators::AND == op) {
+                        if (!res) // not relying on && to not execute the next route
+                            return false;
+                        return is_next_route_match(ctx, req);
+                    } else if constexpr (logical_operators::OR == op) {
+                        if (res)
+                            return true;
+                        return is_next_route_match(ctx, req);
+                    } else if constexpr (logical_operators::XOR == op) {
+                        return res ^ is_next_route_match(ctx, req);
+                    } else {
+                        return false; // this should never happen
+                    }
+                } else if constexpr (stl::same_as<ret_type, bool>) {
+                    if constexpr (logical_operators::none == op || logical_operators::AND == op) {
+                        // don't rely on operator && for not executing the next route, because the user may
+                        // have overloaded the operator &&
+                        if (!call_this_route(ctx, req))
+                            return false;
+                        return is_next_route_match(ctx, req);
+                    } else if constexpr (logical_operators::OR == op) {
+                        // Same as "and", we will not use operator ||
+                        if (call_this_route(ctx, req))
+                            return true;
+                        return is_next_route_match(ctx, req);
+                    } else if constexpr (logical_operators::XOR == op) {
+                        // In operator xor, the next route will be called no matter the result of the current
+                        // route so there's no need for doing the same thing that we did above, but since they
+                        // may have changed the meaning of the operator ^, it's not a bad idea to do so, but
+                        // I'm too lazy :)
+                        return call_this_route(ctx, req) ^ is_next_route_match(ctx, req);
+                    } else {
+                        return false; // should not be possible
+                    }
+                } else if constexpr (Context<ret_type>) {
+                    // entry/sub-route level context switching
+                    // switch the context and try the next route if available
+                    // todo
+                    // switched_ctx >>= ((get and "testing" >>= switching_ctx >>= page1) || (post and "testing" >>= page2))
+                    // [switched_ctx none [[get and ["testing" none [switching_ctx none page1]]] or [post and ["testing" none page2]]]]
+                } else { // void, Response, ConvertibleToResponse, ...
+                    return true; // we don't care about the next route, whatever it is, this route has to run
                 }
             }
 
-          public:
+
             template <typename ContextType>
             requires(Context<stl::remove_cvref_t<ContextType>>) [[nodiscard]] bool
             operator()(ContextType&& ctx, Request auto const& req) const noexcept {
