50c309c93471dd487362b75900dca1c244f6735f
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-11-07 18:18:10 -1000

    Port stuff

--- core/include/webpp/uri/uri_string.hpp
+++ core/include/webpp/uri/uri_string.hpp
@@ -54,14 +54,10 @@ namespace webpp::uri {
      *  [protocol"://"[username[":"password]"@"]hostname[":"port]"/"?][path]["?"querystring]["#"fragment]
      */
     template <Traits TraitsType, bool Mutable = true>
-    struct uri_string
-      : public allocator_holder<typename TraitsType::template allocator<typename TraitsType::char_type>> {
+    struct uri_string {
       public:
         using traits_type = TraitsType;
         using char_type   = typename traits_type::char_type;
-        using alloc_holder_type =
-          allocator_holder<typename TraitsType::template allocator<typename TraitsType::char_type>>;
-        using allocator_type = typename alloc_holder_type::allocator_type;
 
         /**
          * Getting the appropriate string type to use.
@@ -138,126 +134,6 @@ namespace webpp::uri {
          */
         [[nodiscard]] bool decode_to(istl::String auto& output) const noexcept {
             return decode_uri_component(str(), output, ALLOWED_CHARACTERS_IN_URI);
-        }
-
-
-
-        /**
-         * Get the default port for the specified scheme
-         * TODO: make this function a free function
-         */
-        [[nodiscard]] uint16_t default_port() const noexcept {
-            auto _scheme = scheme();
-            if (_scheme == "http")
-                return 80u;
-            if (_scheme == "https")
-                return 443u;
-            if (_scheme == "ftp")
-                return 21u;
-            if (_scheme == "ssh")
-                return 22u;
-            if (_scheme == "telnet")
-                return 23u;
-            if (_scheme == "ftps")
-                return 990u;
-
-            // TODO: add more protocols here
-            return 0u;
-        }
-
-        /**
-         * @brief port number of the uri;
-         * @return port number
-         * @default 80
-         */
-        [[nodiscard]] uint16_t port_uint16() const noexcept {
-            if (has_port()) {
-                return static_cast<uint16_t>(to_uint(port()));
-            }
-            return default_port();
-        }
-
-        /**
-         * Get the port in a string_view format; there's no default value.
-         * @return string_view
-         */
-        [[nodiscard]] str_view_t port() const noexcept {
-            parse_port();
-
-            // there's no port
-            if (port_start == data.size())
-                return {};
-
-            // don't worry authority_end will be the end of the string anyway
-            return substr(port_start + 1, (authority_end == data.size() ? authority_end - 1 : authority_end) -
-                                            (port_start + 1));
-        }
-
-        /**
-         * @brief checks if the uri has explicitly specified a port number or
-         * not
-         * @return bool
-         */
-        [[nodiscard]] bool has_port() const noexcept {
-            parse_port();
-            return port_start != data.size();
-        }
-
-        /**
-         * @brief set port
-         * @param new_port
-         */
-        auto& port(uint16_t new_port) noexcept {
-            return port(to_str_copy<traits_type>(new_port));
-        }
-
-        /**
-         * Set new port value
-         * @param new_port
-         */
-        uri_string& port(str_view_t new_port) noexcept {
-            if (ascii::starts_with(new_port, ':'))
-                new_port.remove_prefix(1);
-            if (!ascii::is::digit(new_port))
-                throw stl::invalid_argument("The specified port is not valid");
-            parse_port();
-            if (port_start == data.size()) {
-                // there's no port, I have to insert it myself:
-                parse_host();
-
-                if (authority_end != data.size()) {
-                    // found it at the end of the line
-                    replace_value(authority_end, 0, ":" + str_t(new_port));
-                } else if (user_info_end != data.size()) {
-                    // there's authority and there might be a host
-                    replace_value(user_info_end + 1, user_info_end + 1, ":" + str_t(new_port));
-                } else if (authority_start != data.size()) {
-                    // there's a authority_start at least
-                    replace_value(authority_start + 1, 0, ":" + str_t(new_port));
-                } else {
-                    // there's no authority at all.
-                    if (scheme_end == data.size()) {
-                        // there's no scheme either
-                        replace_value(0, 0, "//:" + str_t(new_port));
-                    } else {
-                        // there's scheme
-                        replace_value(scheme_end + 1, 0, "//:" + str_t(new_port));
-                    }
-                }
-            } else {
-                // there's a port and we are going to just replace it
-                replace_value(port_start + 1,
-                              authority_end == data.size() ? authority_end - 1 : authority_end, new_port);
-            }
-            return *this;
-        }
-
-        /**
-         * @brief clear the port number from the uri and defaults to 80
-         * @return self
-         */
-        uri_string& clear_port() noexcept {
-            return port({});
         }
 
         /**
