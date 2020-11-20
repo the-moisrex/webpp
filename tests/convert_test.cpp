da9b03ceb4c37b35b80c5e85a69ac9b4f3d0303a
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2020-11-20 02:49:56 -1000

    Fixed to lexical::cast tests; added todo for them

--- tests/convert_test.cpp
+++ tests/convert_test.cpp
@@ -7,8 +7,9 @@ using namespace webpp;
 TEST(Convert, LexicalCast) {
     EXPECT_EQ(20, lexical::cast<int>("20"));
     EXPECT_EQ(-43, lexical::cast<int>("-43"));
-    EXPECT_EQ(43, lexical::cast<unsigned>(-43));
-    EXPECT_EQ(43, lexical::cast<unsigned>("-43"));
+    // todo: make these two or at least the second one work
+    EXPECT_EQ(static_cast<unsigned>(-43), lexical::cast<unsigned>(-43)) << "static_casting -43 to unsigned is not 43";
+    EXPECT_EQ(static_cast<unsigned>(-43), lexical::cast<unsigned>("-43")) << "-43 cannot be converted to unsigned int that easily";
     EXPECT_EQ("-43", lexical::cast<std::string_view>("-43"));
     EXPECT_EQ("-43", lexical::cast<std::string>("-43"));
     EXPECT_EQ("-43", lexical::cast<std::string>("-43"));
