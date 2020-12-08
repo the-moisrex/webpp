8843989cba6137551d6e7a6793da3c8c87f6c7ff
Mohammad Bahoosh <moisrex@protonmail.com>
2020-12-08 11:07:56 -1000

    Going back. no TraitsPacks!

--- core/include/webpp/traits/traits.hpp
+++ core/include/webpp/traits/traits.hpp
@@ -43,66 +43,8 @@ namespace webpp {
         tp.dispatch(lambda);
     };
 
-
-    /**
-     * A Pack is a series of types that potentially do the same thing and have the same APIs but they might
-     * perform better in specific situations. For example you can have an "ASCII String" type which is good
-     * for places where only ASCII characters are going to be handled and also have a "Unicode String" type
-     * that is required for working with Unicode strings. Even though the "Unicode String" type can handle
-     * the "ASCII String"'s job, it's probably going to have performance issues.
-     *
-     * Features:
-     *   So in order to solve this problem, I came up with this "Packaging" the types and giving them
-     *   "Features". Each type has a set of features that will help the other types in the project to choose
-     *   the best type based on those features.
-     *
-     * Possible Features:
-     *   Every package does have a different set of "Possible Features". For example String types require
-     *   different type of features than Allocator types.
-     *
-     */
-    template <typename T>
-    concept Pack = requires {
-
-    };
-
-
-    /**
-     * TraitsPacks are types that the final user is going to pass to the entry-point templated structures.
-     * They are designed to contain a `Pack` of types with different options to choose from.
-     * These packs of types will help this project to "choose" between those types and create a new `traits`
-     * type that contains only one of those types.
-     *
-     * This process of choosing is going to be based on the `features` that each of those types in the pack
-     * have.
-     *
-     * For example: Allocators
-     *   Allocators are a good example of having a pack of them instead of just one single type that does
-     *   it all.
-     */
-    template <typename T>
-    concept TraitsPack = requires {
-        // char type
-        requires istl::CharType<typename T::char_type>;
-
-        // char traits:
-        // requires istl::CharTraits<typename T::char_traits>;
-
-        // allocator pack
-        requires AllocatorPack<typename T::alloc_pack>;
-
-        // typename T::template allocator<typename T::char_traits>;
-        typename T::template string<typename T::char_type,
-                                    typename T::alloc_pack::template local<typename T::char_type>>;
-        typename T::template string_view<typename T::char_type>;
-        typename T::logger_type;
-        // todo: add String<typename T::string_type>; without adding a circular dependency
-        // todo: add StringView<typename T::string_view_type>; without adding a circular dependency
-    };
-
     /**
      * Included traits:
-     *   - [ ] char_type: character type
      *   - [ ] allocator: allocator type
      *         which should satisfy the Allocator requirements specified in:
      *         https://en.cppreference.com/w/cpp/named_req/Allocator
@@ -113,21 +55,23 @@ namespace webpp {
      *         https://en.cppreference.com/w/cpp/named_req/AllocatorAwareContainer
      *         https://en.cppreference.com/w/cpp/named_req/SequenceContainer
      *         https://en.cppreference.com/w/cpp/named_req/ContiguousContainer
-     *   - [ ] Thread Pool
-     *
-     * Proposed types:
-     *   - [ ] Encryption algorithms and their keys
-     *   - [ ] Date and Time systems: stl::chrono types
      *   - [ ] JSON types
      *   - [ ] Error Types
+     *   - [ ] Unicode String Type
+     *
+     * Proposed types:
+     *   - [ ] Thread Pool
+     *   - [ ] Encryption algorithms and their keys
+     *   - [ ] Date and Time systems: stl::chrono types
      *   - [ ] String formatting types (fmt/printf/...)
      *   - [ ] Config file types
      *   - [ ] Cache types
      *   - [ ] Task Scheduling types
      *   - [ ] Image processing types
-     *   - [ ] char_traits: character traits
+     *   - [ ] char_traits: character traits (removed)
      *         which should satisfy the CharTraits requirements specified in:
      *         https://en.cppreference.com/w/cpp/named_req/CharTraits
+     *   - [ ] char_type: character type (removed)
      *
      * Rules:
      *   CharType:
@@ -140,33 +84,88 @@ namespace webpp {
      *     solution for everywhere. This is the reason behind `TraitsPack` and `Traits` being two different
      *     things.
      *
+     * Pack:
+     *   A Pack is a series of types that potentially do the same thing and have the same APIs but they might
+     *   perform better in specific situations. For example you can have an "ASCII String" type which is good
+     *   for places where only ASCII characters are going to be handled and also have a "Unicode String" type
+     *   that is required for working with Unicode strings. Even though the "Unicode String" type can handle
+     *   the "ASCII String"'s job, it's probably going to have performance issues.
+     *
+     * Features:
+     *   So in order to solve this problem, I came up with this "Packaging" the types and giving them
+     *   "Features". Each type has a set of features that will help the other types in the project to choose
+     *   the best type based on those features.
+     *
+     * Possible Features:
+     *   Every package does have a different set of "Possible Features". For example String types require
+     *   different type of features than Allocator types.
+     *
+     * Required Features:
+     *   These are the features that is "required" while searching for the best feature in the list of
+     *   available types in the pack.
+     *
+     * Optional Features:
+     *   These are the features that are not required but if the type has it, it's going to get a better
+     *   "rank" while searching.
+     *
+     * Feature Rank:
+     *   While searching for the best type, they will be ranked to see which one has the best rank.
+     *
+     *
+     * Types:
+     *
+     *   string<CharType, AllocatorType>:
+     *     Usage: ASCII only
+     *     The CharType and AllocatorType has nothing to do with "alloc_pack" and "char_type". Even though
+     *     you probably should use the CharType and AllocatorType, but you are free not to use them in your
+     *     type and we're okay with that. For example you might use QString without using any allocator; but
+     *     you have to then use a wrapper for QString to make sure the constructors are a match to the
+     *     std::basic_string's constructors.
+     *
+     *   string_view<CharType>:
+     *     Usage: ASCII only
+     *     A string_view match for the `string` type above.
+     *
+     *   logger_type:
+     *     The logging system.
+     *
+     *   thread_pool:
+     *     The Thread Pool system.
+     *     todo: we don't really need this for CGI. See how you can remove this.
+     *
+     *   alloc_pack:
+     *     A Pack of allocators.
+     *
+     *   json:
+     *     todo: implement this
+     *
+     *   error:
+     *     The error system to use. Possible solutions:
+     *       - Exceptions
+     *       - Error Code solutions
+     *     todo: find common syntax that can be used to implement all of them and write a concept for it
+     *
+     *   unicode_string:
+     *   unicode_string_view:
+     *     todo: implement these two
      */
     template <typename T>
     concept Traits = requires {
-        requires TraitsPack<typename T::traits_pack_type>; // parent traits type
-        requires istl::CharType<typename T::char_type>;    // character type
-        requires Allocator<typename T::allocator_type>;    // selected allocator type
-        requires Logger<typename T::logger_type>;          // selected logger type
-        requires ThreadPool<typename T::thread_pool>;      // the selected thread pool
+        requires AllocatorPack<typename T::alloc_pack>; // allocator pack
+        requires Logger<typename T::logger_type>;       // logger type
+        requires ThreadPool<typename T::thread_pool>;   // thread pool
 
         typename T::template string<typename T::char_type,
                                     typename T::template allocator<typename T::char_type>>;
         typename T::template string_view<typename T::char_type>;
+        // todo: add String<typename T::string_type>; without adding a circular dependency
+        // todo: add StringView<typename T::string_view_type>; without adding a circular dependency
     };
 
-
-    template <typename T>
-    concept TraitsPackEnabled = requires (T t) {
-
-    };
 
     /**
      * A traits enabled type is a type that supports everything that a traits type has to offer.
      * This will probably have a run-time cost to instantiate.
-     *
-     * Two most used are:
-     *   - allocators
-     *   - loggers
      */
     template <typename T>
     concept EnabledTraits = requires(T t) {
@@ -179,8 +178,8 @@ namespace webpp {
     };
 
 
-    template <typename From, typename To>
-    using to_alloc = typename stl::allocator_traits<From>::template rebind_alloc<To>;
+    template <typename AllocType, typename To>
+    using to_alloc = typename stl::allocator_traits<AllocType>::template rebind_alloc<To>;
 
     /**
      * Middle man type aliases.
@@ -207,24 +206,22 @@ namespace webpp {
         template <Traits TT>
         using logger = typename TT::logger_type;
 
-        /// traits packs ///
-
-        template <TraitsPack TT, typename T>
+        template <Traits TT, typename T>
         using local_allocator = typename TT::alloc_pack::template local<T>;
 
-        template <TraitsPack TT>
+        template <Traits TT>
         using local_char_allocator = local_allocator<TT, char_type<TT>>;
 
-        template <TraitsPack TT, typename T>
+        template <Traits TT, typename T>
         using general_allocator = typename TT::alloc_pack::template general<T>;
 
-        template <TraitsPack TT>
+        template <Traits TT>
         using general_char_allocator = general_allocator<TT, char_type<TT>>;
 
-        template <TraitsPack TT, typename T>
+        template <Traits TT, typename T>
         using alloc_list = typename TT::alloc_pack::template list<T>;
 
-        template <TraitsPack TT>
+        template <Traits TT>
         using char_alloc_list = alloc_list<TT, char_type<TT>>;
 
         template <Traits TT>
