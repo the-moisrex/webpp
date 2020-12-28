4a25bf0f1b98f08abb42f282c814cbc9074ad276
Mohammad Bahoosh <moisrex@protonmail.com>
2020-12-28 04:44:16 -1000

    added test for next_char and prev_char in UTF16 mode

--- tests/unicode_test.cpp
+++ tests/unicode_test.cpp
@@ -7,7 +7,7 @@ using namespace webpp;
 using namespace std;
 
 
-TEST(Unicode, UnChecked) {
+TEST(Unicode, U8UnChecked) {
     using namespace unicode::unchecked;
 
     const char8_t* str = u8"این یک تست است.";
@@ -16,5 +16,17 @@ TEST(Unicode, UnChecked) {
     EXPECT_EQ(static_cast<int>(u8"ن"[0]), static_cast<int>(*next_char_copy(str + 3)));
     EXPECT_EQ(static_cast<int>(u8"ی"[0]), static_cast<int>(*prev_char_copy(str + 3)));
     EXPECT_EQ(static_cast<int>(u8"ن"[0]), static_cast<int>(*prev_char_copy(str + 5)));
+
+}
+
+TEST(Unicode, U16UnChecked) {
+    using namespace unicode::unchecked;
+
+    const char16_t* str = u"این یک تست است.";
+    // have to use static_cast<int> because the google-test doesn't seem to understand char8_t
+    EXPECT_EQ(static_cast<int>(u"ی"[0]), static_cast<int>(*next_char_copy(str)));
+    EXPECT_EQ(static_cast<int>(u"ن"[0]), static_cast<int>(*next_char_copy(str + 1)));
+    EXPECT_EQ(static_cast<int>(u"ی"[0]), static_cast<int>(*prev_char_copy(str + 5)));
+    EXPECT_EQ(static_cast<int>(u"ن"[0]), static_cast<int>(*prev_char_copy(str + 3)));
 
 }
