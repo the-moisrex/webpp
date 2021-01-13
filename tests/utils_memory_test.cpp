b39fcfa5aded3409e55ff212bc5fabfc0488a0fd
Mohammad Bahoosh <moisrex@protonmail.com>
2021-01-13 10:30:38 -1000

    Fixed default allocator problem

--- tests/utils_memory_test.cpp
+++ tests/utils_memory_test.cpp
@@ -2,24 +2,33 @@
 
 #include "../core/include/webpp/memory/allocator_pack.hpp"
 #include "../core/include/webpp/memory/available_memory.hpp"
+#include "../core/include/webpp/memory/object.hpp"
 #include "../core/include/webpp/memory/std_allocator_pack.hpp"
 #include "../core/include/webpp/memory/std_pmr_allocator_pack.hpp"
-#include "../core/include/webpp/memory/object.hpp"
 #include "./common_pch.hpp"
 
 
 using namespace webpp;
 using namespace webpp::alloc;
-using namespace std;
+using namespace webpp::stl;
 
 TEST(MemoryTest, AllocatorPackTest) {
     static_assert(Allocator<stl::pmr::polymorphic_allocator<char>>);
     allocator_pack<stl::pmr::allocator_descriptors> alloc_pack;
-    auto str = make<string, monotonic_features>(alloc_pack, placeholder{});
+    auto str    = make<string, monotonic_features>(alloc_pack, "hello world", placeholder{});
     using str_t = remove_cvref_t<decltype(str)>;
-    static_assert(stl::same_as<str_t, pmr::string>);
+    static_assert(same_as<str_t, stl::pmr::string>);
+    EXPECT_EQ(str, "hello world");
 
-    object::local<std::string> str2(alloc_pack);
+    object::local<string> str2(alloc_pack, "hello world");
+    ASSERT_EQ(str2, "hello world");
+
+    // nested allocator-rebinding: both string and vector should use the specified allocator
+    object::local<vector<string>> str_vec{alloc_pack};
+    using vec_t     = typename remove_cvref_t<decltype(str_vec)>::object_type;
+    using vec_str_t = typename vec_t::value_type;
+    static_assert(same_as<vec_str_t, stl::pmr::string>, "Nested allocator-rebinding don't work");
+    static_assert(same_as<vec_t, stl::pmr::vector<stl::pmr::string>>, "Allocator-rebinding don't work");
 }
 
 
