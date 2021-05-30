44a9001d0469412d3e7975f526bdace9e6124c31
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-05-30 10:19:29 -0700

    updated command file

--- sdk/cmds/command.hpp
+++ sdk/cmds/command.hpp
@@ -1,20 +1,29 @@
 #ifndef WEBPP_SDK_CMDS_CMD
 #define WEBPP_SDK_CMDS_CMD
 
+#include "../../../core/include/webpp/std/string.hpp"
+#include "../../../core/include/webpp/std/string_view.hpp"
+
 
 namespace webpp::sdk::cmd {
 
-	struct command {
+    /**
+     * Command abstraction
+     *
+     * This class is the abstract class and the helper class
+     * that all the commands will be extend from.
+     */
+    struct command {
 
-		// the name of the command
-		virtual constexpr stl::string_view name() noexcept const = 0;
+        // the name of the command
+        virtual constexpr stl::string_view name() noexcept const = 0;
 
 
-		// description of the command
-		virtual constexpr stl::string desc() noexcept const = 0;
-	};
+        // description of the command
+        virtual constexpr stl::string desc() noexcept const = 0;
+    };
 
-}
+} // namespace webpp::sdk::cmd
 
 
 #endif // WEBPP_SDK_CMDS_CMD
