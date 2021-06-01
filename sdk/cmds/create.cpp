b7ed016bd0fcb7adb66782e008c38ff861031743
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-06-01 09:03:58 -0700

    Adding handle abstraction to command

--- sdk/cmds/create.cpp
+++ sdk/cmds/create.cpp
@@ -3,3 +3,12 @@
 using namespace webpp;
 using namespace webpp::sdk::cmd;
 
+/**
+ * Create files:
+ *
+ * There are multiple types of create types:
+ *   - A file in an already established project
+ *   - Multiple files for a project
+ *   - A whole new project
+ */
+void create::handle() {}
