21804f6f0886d98603ba78c7836a9ddf10a33ef0
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-06-02 05:32:22 -0700

    Added project name and template methods

--- sdk/cmds/create_project.hpp
+++ sdk/cmds/create_project.hpp
@@ -20,6 +20,14 @@ namespace webpp::sdk::cmd {
         // A batch is a series of files and directories that have
         // a knowm name.
         virtual stl::string batch_name() const = 0;
+
+	// Get the selected project name
+	// Empty when it's not specified.
+	stl::string project_name() const;
+
+	// Get the selected template name
+	// There's a default value
+	stl::string template_name() const;
     };
 
 } // namespace webpp::sdk::cmd
