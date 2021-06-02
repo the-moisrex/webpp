21aa6d0a9ec14dd0cfbdb156920c8db96684a133
The Moisrex <moisrex@protonmail.com>
2021-06-02 05:32:22 -0700

    Added project name and template methods

--- sdk/cmds/create_project.cpp
+++ sdk/cmds/create_project.cpp
@@ -5,7 +5,13 @@ using namespace webpp::sdk::cmd;
 
 
 void create_project::handle() override {
-	
+
+	if (const stl::string proj_name = project_name(); !proj_name.empty()) {
+		// the project has a valid name
+	} else {
+		print_error("Please specify a valid name for the project");
+	}
+
 }
 
 
