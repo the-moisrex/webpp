872f0472f139466a78d670baa98cb175d05e6352
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-06-02 06:22:14 -0700

    Create Project new idea

--- sdk/cmds/create_project.cpp
+++ sdk/cmds/create_project.cpp
@@ -3,13 +3,21 @@
 using namespace webpp;
 using namespace webpp::sdk::cmd;
 
+struct project_template {
+
+};
 
 void create_project::handle() override {
+	const stl::string proj_name = project_name();
+	if (proj_name.empty()) {
+		print_error("Please specify a valid name for the project.");
+		return;
+	}
 
-	if (const stl::string proj_name = project_name(); !proj_name.empty()) {
-		// the project has a valid name
-	} else {
-		print_error("Please specify a valid name for the project");
+	const project_tempplate tmpl = get_project_template();
+	if (!tmpl.is_valid()) {
+		print_error("The specified template is not valid.");
+		return;
 	}
 
 }
