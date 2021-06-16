9ca4fcfdb26d64e68e694a26a0f6bfcc54f07501
The Moisrex <moisrex@protonmail.com>
2021-06-16 05:52:07 -0700

    Reformat + comment

--- core/include/webpp/strings/charset.hpp
+++ core/include/webpp/strings/charset.hpp
@@ -159,7 +159,9 @@ namespace webpp {
      * Type deduction. I stole this from a type deduction from std::array
      */
     template <typename Tp, typename... Up>
-    requires((stl::same_as<Tp, Up> && ...)) charset(Tp, Up...)->charset<Tp, 1 + sizeof...(Up)>;
+    requires((stl::same_as<Tp, Up> && ...)) // T and Us should be same
+      charset(Tp, Up...)
+    ->charset<Tp, (1 + sizeof...(Up))>;
 
     template <istl::CharType CharT = char, stl::size_t N>
     charset(const CharT (&)[N]) -> charset<stl::remove_cvref_t<CharT>, N - 1>;
