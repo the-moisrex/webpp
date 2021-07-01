0bd862f8d2eaabd4ce820cd08327662271e215ab
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-07-01 03:38:16 -1000

    Back to constexpr in format

--- core/include/webpp/strings/charset.hpp
+++ core/include/webpp/strings/charset.hpp
@@ -62,7 +62,7 @@ namespace webpp {
 
       public:
         // we use +1, so we don't copy the null terminator character as well
-        constexpr charset(const value_type (&str)[N + 1]) noexcept
+        consteval charset(const value_type (&str)[N + 1]) noexcept
           : super{to_array(stl::make_index_sequence<N>(), str)} {}
 
         template <typename... T>
@@ -78,7 +78,7 @@ namespace webpp {
          *     These are the character sets to include.
          */
         template <stl::size_t N1, stl::size_t N2, stl::size_t... NN>
-        constexpr charset(charset<value_type, N1> const& set1,
+        consteval charset(charset<value_type, N1> const& set1,
                           charset<value_type, N2> const& set2,
                           charset<value_type, NN> const&... c_sets) noexcept
           : super{merge<N1, N2, NN...>(set1, set2, c_sets...)} {
