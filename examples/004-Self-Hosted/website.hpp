2bb5f209ca787f867747b99298ae63b1bf24447c
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-07-01 21:26:53 -1000

    blog example template added

---
+++ examples/004-Self-Hosted/website.hpp
@@ -0,0 +1,49 @@
+// Created by moisrex on 5/5/20.
+
+#ifndef WEBPP_EXAMPLE_BLOG_H
+#define WEBPP_EXAMPLE_BLOG_H
+
+#include "../../core/include/webpp/http/http.hpp"
+
+
+namespace website {
+
+    using namespace webpp::http;
+
+    struct blog {
+
+        auto home(auto const& ctx) noexcept {
+            return ctx.string("Home page");
+        }
+
+        auto about(auto const& ctx) {
+            return ctx.string("About page");
+        }
+
+        auto operator()(auto&& req) {
+            using extensions = webpp::extension_pack<string_response>;
+            const auto admin = []() {
+                return "Nice page.";
+            };
+            router _router{extensions{},
+                           (get and (root / "home")) >>=
+                           [this](Context auto& ctx) {
+                               return this->home(ctx);
+                           },
+                           get & (root / "about") >>=
+                           [this](Context auto& ctx) {
+                               return this->about(ctx);
+                           },
+                           root / "admin" >>= admin};
+
+            // for debugging purposes
+            std::cerr << _router.to_string(req) << std::endl;
+
+            return _router(req);
+        }
+    };
+
+} // namespace website
+
+
+#endif // WEBPP_EXAMPLE_BLOG_H
