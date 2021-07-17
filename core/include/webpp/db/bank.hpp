eb58a10a9485385d33fe535c6912426dfccac54b
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-07-17 06:40:01 -0700

    The idea of database bank has been born

---
+++ core/include/webpp/db/bank.hpp
@@ -0,0 +1,18 @@
+#ifndef WEBPP_DATABASE_BANK
+#define WEBPP_DATABASE_BANK
+
+#include "../traits/traits.hpp"
+
+namespace webpp::database {
+
+    /**
+     * This clasa is designed to be used inside a
+     * "protocol" so the final user can configure the
+     * databases they want from here.
+     */
+    template <Traits TraitsType>
+    struct bank : stl::vector<...> {};
+
+} // namespace webpp::database
+
+#endif
