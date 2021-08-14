c6e3a94688f52d24ebc300557a63d8b387f80c90
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-08-14 06:34:55 -0700

    Updating string variant to string splits
    Should I use string_pieces or string_views?

--- core/include/webpp/strings/string_vector.hpp
+++ core/include/webpp/strings/string_vector.hpp
@@ -4,27 +4,38 @@
 
 #include "../std/string.hpp"
 #include "../std/string_view.hpp"
-#include "../std/variant.hpp"
-
+#include "../std/type_traits.hpp"
 
 namespace webpp {
 
 
     /**
-     * String Variant
+     * String Splits
      *
      * Holds string_views of the same string efficiantly.
      */
     template <istl::CharType CharT, auto... Names>
-    struct basic_string_variant {
+    struct basic_string_splits {
         using char_type                          = CharT;
-        static constexpr stl::size_t field_count = sizeof...(Names);
-        using tuple_type = istl::repeat_type<stl::tuple, char_type const*, field_count>;
+        using str_ptr                            = char_type const*;
+        static constexpr stl::size_t piece_count = sizeof...(Names);
+        using tuple_type                         = istl::repeat_type<stl::tuple, str_ptr, piece_count + 1>;
 
       private:
         tuple_type data;
 
       public:
+        constexpr basic_string_splits() noexcept = default;
+        constexpr basic_string_splits(str_ptr ptr, stl::size_t len) noexcept
+          : data{ptr, (Names, ptr + len), ...} {}
+
+        template <stl::size_t Index, istl::StringView StrV = stl::string_view>
+        constexpr StrV view() const noexcept {
+            auto const start_ptr = stl::get<Index>(data);
+            auto const end_ptr   = stl::get<Index + 1>(data);
+            auto const len       = end_ptr - start_ptr;
+            return StrV{start_ptr, len};
+        }
     };
 
 
