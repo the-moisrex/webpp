849a46f5f6ce56d4ab3be62a06240e11e1780c3c
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-09-04 08:44:10 -0700

    Added version function

--- core/include/webpp/db/sqlite/sqlite_connection.hpp
+++ core/include/webpp/db/sqlite/sqlite_connection.hpp
@@ -1,6 +1,7 @@
 #ifndef WEBPP_DATABASE_SQLITE_CONNECTION_HPP
 #define WEBPP_DATABASE_SQLITE_CONNECTION_HPP
 
+#include "../../libs/sqlite.hpp"
 #include "../../traits/traits.hpp"
 
 #include <utility>
@@ -9,6 +10,9 @@ namespace webpp::database {
 
     template <Traits TraitsType>
     struct sqlite_connection {
+        using traits_type = TraitsType;
+        using string_type = traits::string<traits_type>;
+
       private:
         sqlite3* handle{nullptr};
 
@@ -29,6 +33,36 @@ namespace webpp::database {
             close();
         }
     };
+
+
+
+
+    // source: https://raw.githubusercontent.com/sqlitebrowser/sqlitebrowser/master/src/sqlitedb.cpp
+    void sqlite_version(string_type& sqlite, string_type& sqlcipher) {
+        sqlite = string_type(SQLITE_VERSION);
+
+        // The SQLCipher version must be queried via a pragma and for a pragma we need a database
+        // connection. Because we want to be able to query the SQLCipher version without opening a
+        // database file first, we open a separate connection to an in-memory database here.
+        sqlcipher = string_type();
+#ifdef ENABLE_SQLCIPHER
+        sqlite3* dummy;
+        if (sqlite3_open(":memory:", &dummy) == SQLITE_OK) {
+            sqlite3_stmt* stmt;
+            if (sqlite3_prepare_v2(dummy, "PRAGMA cipher_version", -1, &stmt, nullptr) == SQLITE_OK) {
+                if (sqlite3_step(stmt) == SQLITE_ROW)
+                    sqlcipher = QByteArray(static_cast<const char*>(sqlite3_column_blob(stmt, 0)),
+                                           sqlite3_column_bytes(stmt, 0));
+
+                sqlite3_finalize(stmt);
+            }
+
+            sqlite3_close(dummy);
+        }
+#endif
+    }
+
+
 
 } // namespace webpp::database
 
