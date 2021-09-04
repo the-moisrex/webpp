fea218705965a4dd3f98b2ceaba0132fb8e5c337
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-09-03 23:10:47 -0700

    Added sqlite3 header file

---
+++ core/include/webpp/libs/sqlite.hpp
@@ -0,0 +1,25 @@
+#ifndef WEBPP_SQLITE_HPP
+#define WEBPP_SQLITE_HPP
+
+// some of the source is from: https://raw.githubusercontent.com/sqlitebrowser/sqlitebrowser/master/src/sqlite.h
+
+#ifdef ENABLE_SQLCIPHER
+#    define SQLITE_TEMP_STORE 2
+#    define SQLITE_HAS_CODEC
+#    ifdef Q_OS_WIN32
+#        include <sqlite3.h>
+#    else
+#        include <sqlcipher/sqlite3.h>
+#    endif
+#else
+#    include <sqlite3.h>
+#endif
+
+// For older versions of the SQLite library which do not have the SQLITE_DETERMINISTIC flag
+// yet (which introduced in this commit: https://www.sqlite.org/src/info/5716fc2341ddd8cf), we
+// define it here with a value of 0. Because it is ORed with other constants a value of 0 is a no-op.
+#ifndef SQLITE_DETERMINISTIC
+#    define SQLITE_DETERMINISTIC 0
+#endif
+
+#endif
