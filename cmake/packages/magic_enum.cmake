e0dba208856a93720c40c9ae644baf18afe0974a
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-10-06 21:42:14 -1000

    Fixed magic_enum bug

--- cmake/packages/magic_enum.cmake
+++ cmake/packages/magic_enum.cmake
@@ -1,4 +1,8 @@
-CPMAddPackage("gh:Neargye/magic_enum@0.7.3")
+CPMAddPackage(
+        NAME magic_enum
+        URL https://github.com/Neargye/magic_enum/archive/refs/tags/v0.7.3.tar.gz
+        VERSION 0.7.3
+)
 set(MAGIC_ENUM_OPT_BUILD_EXAMPLES OFF CACHE BOOL "Build magic_enum examples" FORCE)
 set(MAGIC_ENUM_OPT_BUILD_TESTS OFF CACHE BOOL "Build and perform magic_enum tests" FORCE)
 set(MAGIC_ENUM_OPT_INSTALL ON CACHE BOOL "Generate and install magic_enum target" FORCE)
