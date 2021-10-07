533fbc912e45f04a40135b9883fba23e0e3004f4
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-10-07 06:27:17 -1000

    Clang doesn't support -march=native

--- cmake/packages/brotli.cmake
+++ cmake/packages/brotli.cmake
@@ -1,5 +1,5 @@
-find_package(Brotli QUIET)
-if (NOT Brotli_FOUND)
+find_package(brotli QUIET)
+if (NOT brotli_FOUND)
     CPMAddPackage(
             NAME brotli
             URL "https://github.com/google/brotli/archive/refs/tags/v1.0.9.zip"
