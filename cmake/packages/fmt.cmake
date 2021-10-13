c323da7cf4e88dc197c61b8586f364dbdf0a74e7
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-10-13 02:33:13 -1000

    Variable Re-Order

--- cmake/packages/fmt.cmake
+++ cmake/packages/fmt.cmake
@@ -1,8 +1,4 @@
 include(CPM)
-CPMAddPackage(
-        NAME fmt
-        URL https://github.com/fmtlib/fmt/releases/download/8.0.1/fmt-8.0.1.zip
-)
 set(FMT_DOC OFF CACHE BOOL "Generate the doc target." FORCE)
 set(FMT_INSTALL ON CACHE BOOL "Generate the install target." FORCE)
 set(FMT_TEST OFF CACHE BOOL "Generate the test target." FORCE)
@@ -10,3 +6,7 @@ set(FMT_FUZZ OFF CACHE BOOL "Generate the fuzz target." FORCE)
 set(FMT_CUDA_TEST OFF CACHE BOOL "Generate the cuda-test target." FORCE)
 set(FMT_OS ON CACHE BOOL "Include core requiring OS (Windows/Posix) " FORCE)
 
+CPMAddPackage(
+        NAME fmt
+        URL https://github.com/fmtlib/fmt/releases/download/8.0.1/fmt-8.0.1.zip
+)
--- cmake/packages/magic_enum.cmake
+++ cmake/packages/magic_enum.cmake
@@ -1,9 +1,9 @@
 
 include(CPM)
+set(MAGIC_ENUM_OPT_BUILD_EXAMPLES OFF CACHE BOOL "Build magic_enum examples" FORCE)
+set(MAGIC_ENUM_OPT_BUILD_TESTS OFF CACHE BOOL "Build and perform magic_enum tests" FORCE)
+set(MAGIC_ENUM_OPT_INSTALL ON CACHE BOOL "Generate and install magic_enum target" FORCE)
 CPMAddPackage(
         NAME magic_enum
         URL https://github.com/Neargye/magic_enum/archive/refs/tags/v0.7.3.tar.gz
 )
-set(MAGIC_ENUM_OPT_BUILD_EXAMPLES OFF CACHE BOOL "Build magic_enum examples" FORCE)
-set(MAGIC_ENUM_OPT_BUILD_TESTS OFF CACHE BOOL "Build and perform magic_enum tests" FORCE)
-set(MAGIC_ENUM_OPT_INSTALL ON CACHE BOOL "Generate and install magic_enum target" FORCE)
