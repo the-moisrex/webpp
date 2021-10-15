7d6aca00dfca2e5e0fd98121ac9fc64372f48476
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-10-15 06:13:19 -1000

    eve template added

---
+++ cmake/packages/eve.cmake
@@ -0,0 +1,9 @@
+
+# Since eve project doesn't have a version yet!
+# bug report: https://github.com/jfalcou/eve/issues/1023
+if (false)
+    CPMAddPackage(
+            NAME eve
+            GITHUB_REPOSITORY jfalcou/eve
+    )
+endif ()
