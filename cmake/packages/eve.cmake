b8e6df9a980208b0882862a3373efdf875ab1dc8
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-10-15 06:17:41 -1000

    using github's main zip file for eve

--- cmake/packages/eve.cmake
+++ cmake/packages/eve.cmake
@@ -1,9 +1,8 @@
 
 # Since eve project doesn't have a version yet!
 # bug report: https://github.com/jfalcou/eve/issues/1023
-if (false)
-    CPMAddPackage(
-            NAME eve
-            GITHUB_REPOSITORY jfalcou/eve
-    )
-endif ()
+CPMAddPackage(
+        NAME eve
+        URL https://github.com/jfalcou/eve/archive/refs/heads/main.zip
+        VERSION 1.20.5
+)
