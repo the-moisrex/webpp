70cc9380f9cd07c984a64821b6e38c5376f63e3e
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-10-23 12:58:11 -0800

    rapidjson -march=native fix for clang; #22

--- cmake/packages/json.cmake
+++ cmake/packages/json.cmake
@@ -1,5 +1,16 @@
 
 include(CPM)
+
+
+# To fix:
+# https://github.com/Tencent/rapidjson/issues/1816
+# https://github.com/the-moisrex/webpp/issues/22
+# if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+  set(RAPIDJSON_ENABLE_INSTRUMENTATION_OPT OFF)
+  set(CMAKE_CROSS_COMPILING OFF)
+# endif()
+
+
 CPMAddPackage(
         NAME RapidJSON
         URL https://github.com/Tencent/rapidjson/archive/refs/tags/v1.1.0.zip
