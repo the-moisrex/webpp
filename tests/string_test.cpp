18fac384203e6ab08933d41e0c2792fc218c77f2
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-12-01 04:07:52 -0800

    ctre settings

--- tests/string_test.cpp
+++ tests/string_test.cpp
@@ -8,20 +8,22 @@ using namespace webpp;
 using namespace webpp::string;
 
 TEST(String, Join) {
-    std::string      one   = "one ";
-    std::pmr::string two   = "two ";
-    std::string_view three = "three";
+    stl::string      one   = "one ";
+    stl::pmr::string two   = "two ";
+    stl::string_view three = "three";
     auto const       res   = string::join(one, two, three, " four");
     static_assert(
       stl::same_as<typename decltype(res)::allocator_type, typename decltype(two)::allocator_type>);
     EXPECT_EQ(res, "one two three four");
 
-    EXPECT_EQ(string::join(std::string("one, "), 1, 2), "one, 12");
+    EXPECT_EQ(string::join(stl::string("one, "), 1, 2), "one, 12");
 }
 
 
 
 TEST(String, StringPieces) {
     string_splits<"one", "two"> pieces{"one two"};
+
+    // fixme: I don't understand this line that I wrote!!!
     EXPECT_TRUE(pieces.template view_of<"one">().empty());
 }
