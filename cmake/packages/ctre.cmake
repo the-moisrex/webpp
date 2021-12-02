e86d5c4f91974e23c93b6b4f5c65662b8386220d
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-12-02 03:58:39 -0800

    importing CTRE; #32

--- cmake/packages/ctre.cmake
+++ cmake/packages/ctre.cmake
@@ -5,3 +5,8 @@ CPMAddPackage(
         NAME ctre
         URL https://github.com/hanickadot/compile-time-regular-expressions/archive/refs/tags/v3.4.1.tar.gz
 )
+
+export(EXPORT ctre-targets
+       FILE "${CMAKE_CURRENT_BINARY_DIR}/cmake/ctre-targets.cmake"
+       NAMESPACE ctre::
+)
