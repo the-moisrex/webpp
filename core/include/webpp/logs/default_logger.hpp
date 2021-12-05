ff2faecaf8e4b425c1f25ea49bcb217b5b58a363
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2021-12-05 02:02:39 -0800

    default logger

---
+++ core/include/webpp/logs/default_logger.hpp
@@ -0,0 +1,11 @@
+#ifndef WEBPP_LOGGER_DEFAULT_HPP
+#define WEBPP_LOGGER_DEFAULT_HPP
+
+#include "./std_logger.hpp"
+
+namespace webpp {
+
+  using default_logger = stderr_loggrr;
+}
+
+#endif // WEBPP_LOGGER_DEFAULT_HPP
