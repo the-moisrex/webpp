641dab2e10b9e493051ec495b4b9338ad0bb0816
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-01-03 03:16:52 -1000

    Add errors for edge cases

--- core/include/webpp/strings/to_case.hpp
+++ core/include/webpp/strings/to_case.hpp
@@ -122,20 +122,26 @@ namespace webpp::ascii {
 
 #ifdef WEBPP_EVE
 
-        inline void eve_to_lower(auto& str) noexcept {
+        inline void eve_to_lower(istl::StringView auto& str) noexcept {
+            using value_type = typename stl::remove_cvref_t<decltype(str)>::value_type;
+            static_assert(sizeof(value_type) == sizeof(stl::uint8_t),
+                          "The specified string type's char size doesn't work with our SIMD algorithm.");
             static constexpr std::uint8_t alphabet_length = 'z' - 'a';
             static constexpr std::uint8_t a_A_offset      = 'a' - 'A';
-            std::uint8_t*                 start           = reinterpret_cast<std::uint8_t*>(str.data());
+            std::uint8_t*                 start           = reinterpret_cast<stl::uint8_t*>(str.data());
             std::uint8_t*                 finish          = start + str.size();
             eve::algo::transform_inplace(eve::algo::as_range(start, finish), [](auto c) {
                 return eve::add[(c - 'A') <= alphabet_length](c, a_A_offset);
             });
         }
 
-        inline void eve_to_upper(auto& str) noexcept {
+        inline void eve_to_upper(istl::StringView auto& str) noexcept {
+            using value_type = typename stl::remove_cvref_t<decltype(str)>::value_type;
+            static_assert(sizeof(value_type) == sizeof(stl::uint8_t),
+                          "The specified string type's char size doesn't work with our SIMD algorithm.");
             static constexpr std::uint8_t alphabet_length = 'z' - 'a';
             static constexpr std::uint8_t a_A_offset      = 'a' - 'A';
-            std::uint8_t*                 start           = reinterpret_cast<std::uint8_t*>(str.data());
+            std::uint8_t*                 start           = reinterpret_cast<stl::uint8_t*>(str.data());
             std::uint8_t*                 finish          = start + str.size();
             eve::algo::transform_inplace(eve::algo::as_range(start, finish), [](auto c) {
                 return eve::sub[(c - 'a') <= alphabet_length](c, a_A_offset);
