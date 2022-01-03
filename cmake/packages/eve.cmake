842a481a9a09f67e6881643dd4ecddb45cf866a4
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-01-03 04:15:50 -1000

    enable eve tests

--- cmake/packages/eve.cmake
+++ cmake/packages/eve.cmake
@@ -1,12 +1,13 @@
+include(CPM)
 
-# Since eve project doesn't have a version yet!
-# bug report: https://github.com/jfalcou/eve/issues/1023
+# version bug report: https://github.com/jfalcou/eve/issues/1023
+# disabling building of the tests bug report: https://github.com/jfalcou/eve/issues/1126
 CPMAddPackage(
         NAME eve
         URL https://github.com/jfalcou/eve/archive/refs/tags/v2021.10.0.zip
         VERSION 2021.10.0
         OPTIONS
-                "EVE_BUILD_TEST OFF"
+                # "EVE_BUILD_TEST OFF"
                 "EVE_BUILD_BENCHMARKS OFF"
                 "EVE_BUILD_DOCUMENTATION OFF"
 )
