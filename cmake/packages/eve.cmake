9d77ac15f87314a349091201f8ca83ef477a1ac5
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-01-07 07:04:53 -1000

    switching to develop version of eve for now

--- cmake/packages/eve.cmake
+++ cmake/packages/eve.cmake
@@ -4,10 +4,11 @@ include(CPM)
 # disabling building of the tests bug report: https://github.com/jfalcou/eve/issues/1126
 CPMAddPackage(
         NAME eve
-        URL https://github.com/jfalcou/eve/archive/refs/tags/v2021.10.0.zip
-        VERSION 2021.10.0
+        # URL https://github.com/jfalcou/eve/archive/refs/tags/v2021.10.0.zip
+        # VERSION 2021.10.0
+        URL https://github.com/jfalcou/eve/archive/refs/heads/develop.zip
         OPTIONS
-                # "EVE_BUILD_TEST OFF"
+                "EVE_BUILD_TEST OFF"
                 "EVE_BUILD_BENCHMARKS OFF"
                 "EVE_BUILD_DOCUMENTATION OFF"
 )
