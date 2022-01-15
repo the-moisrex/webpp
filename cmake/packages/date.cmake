36fcc8e8baf8660140696c4df4ea96da62651ba2
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-01-15 12:05:13 -1000

    fixing date:date-tz

--- cmake/packages/date.cmake
+++ cmake/packages/date.cmake
@@ -6,7 +6,7 @@ CPMAddPackage(
         URL https://github.com/HowardHinnant/date/archive/refs/tags/v3.0.1.tar.gz
         VERSION 3.0.1
         OPTIONS
-                "USE_SYSTEM_TZ_DB OFF"
+                "USE_SYSTEM_TZ_DB ON"
                 "USE_OS_TZDB ON"
                 "MANUAL_TZ_DB OFF"
                 "USE_TZ_DB_IN_DOT OFF"
