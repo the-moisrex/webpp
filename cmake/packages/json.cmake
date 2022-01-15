368eceab98a6f244d6d11470cdf43a5ca60dea65
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-01-15 11:21:35 -1000

    json minor updates

--- cmake/packages/json.cmake
+++ cmake/packages/json.cmake
@@ -4,10 +4,10 @@ include(CPM)
 # To fix:
 # https://github.com/Tencent/rapidjson/issues/1816
 # https://github.com/the-moisrex/webpp/issues/22
-# if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+if (IS_CLANG)
   set(RAPIDJSON_ENABLE_INSTRUMENTATION_OPT OFF CACHE BOOL "" FORCE)
   set(CMAKE_CROSS_COMPILING OFF CACHE BOOL "" FORCE)
-# endif()
+endif()
 
 # Using the master branch since the 1.1.0 is buggy
 CPMAddPackage(
