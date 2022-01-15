c675b743648e78cccf7badc86d20a740a617da74
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-01-15 10:50:16 -1000

    no local packages

--- cmake/import.cmake
+++ cmake/import.cmake
@@ -7,7 +7,7 @@
 # CPM Module examples: https://github.com/cpm-cmake/CPM.cmake/wiki/More-Snippets
 set(current_dir "${CMAKE_CURRENT_LIST_DIR}")
 set(CURRENT_CPM_VERSION "")
-set(CPM_USE_LOCAL_PACKAGES ON)
+set(CPM_USE_LOCAL_PACKAGES OFF)
 
 # this is done for find_package to be able to find the FindModule.cmake files
 list(APPEND CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake/modules)
