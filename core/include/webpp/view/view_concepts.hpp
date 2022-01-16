ed63e73b3deac57eebd3cb91069402f549526130
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-01-16 01:19:58 -1000

    view; #59

---
+++ core/include/webpp/view/view_concepts.hpp
@@ -0,0 +1,15 @@
+// Created by moisrex on 16/1/22.
+
+#ifndef WEBPP_VIEW_CONCEPTS_HPP
+#define WEBPP_VIEW_CONCEPTS_HPP
+
+namespace webpp {
+
+    template <typename T>
+    concept View = requires (T view){
+        view.page("home/page");
+    };
+
+}
+
+#endif // WEBPP_VIEW_CONCEPTS_HPP
