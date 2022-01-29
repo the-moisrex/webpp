6cb321162dc5215bea05edfcdf6a7ec85dfd1d26
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-01-29 08:23:40 -0800

    passive_cache updates

--- core/include/webpp/storage/passive_cache.hpp
+++ core/include/webpp/storage/passive_cache.hpp
@@ -1,9 +1,9 @@
 #ifndef WEBPP_CACHE_PASSIVE_CACHE
 #define WEBPP_CACHE_PASSIVE_CACHE
 
+#include "../std/string_view.hpp"
 #include "../std/type_traits.hpp"
 
-#include <string_view>
 
 namespace webpp {
 
@@ -22,22 +22,23 @@ namespace webpp {
 
       private:
       public:
-        passive_cache() noexcept;
+        constexpr passive_cache() noexcept;
 
         template <typename KeyValue, typename DataType>
-        auto get(KeyValue&& /* key */, DataType const& default_value) const noexcept {
+        constexpr auto get([[maybe_unused]] KeyValue&& key, DataType const& default_value) const noexcept {
             // yes, that's how useless this class is!
             return default_value;
         }
 
         template <typename KeyValue, typename DataType>
-        auto& set(KeyValue& /* key */, DataType&& /* value */) const noexcept {
+        constexpr auto& set([[maybe_unused]] KeyValue&  key,
+                            [[maybe_unused]] DataType&& value) const noexcept {
             // we will store nothing. that's it!
             return *this;
         }
 
         template <typename KeyValue, typename Callable>
-        auto& set(KeyValue&& /* key */, Callable&& callback) noexcept {
+        constexpr auto& set([[maybe_unused]] KeyValue&& key, Callable&& callback) noexcept {
             if constexpr (std::is_invocable_v<Callable, passive_cache>) {
                 return callback(*this);
             } else if (std::is_invocable_v<Callable>) {
