91ad2556d6ca94c6c1f92356fcbefc1505d7000e
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-02-02 09:44:43 -0800

    lru cache file added; #64

---
+++ core/include/webpp/storage/lru_cache.hpp
@@ -0,0 +1,11 @@
+#ifndef WEBPP_STORAGE_LRU_CACHE_HPP
+#define WEBPP_STORAGE_LRU_CACHE_HPP
+
+
+namespace webpp {
+
+    struct lru_cache {};
+
+} // namespace webpp
+
+#endif // WEBPP_STORAGE_LRU_CACHE_HPP
