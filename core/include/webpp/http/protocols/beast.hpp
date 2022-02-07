76552ddf430c24462672732163e2cfc450673a14
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-02-07 06:16:36 -0800

    beast; #67

--- core/include/webpp/http/protocols/beast.hpp
+++ core/include/webpp/http/protocols/beast.hpp
@@ -7,31 +7,32 @@
 #include "beast_proto/beast_server.hpp"
 #include "common/common_http_protocol.hpp"
 
-#include <boost/beast.hpp>
 
 namespace webpp::http {
 
 
-    template <Application App, ExtensionList EList = empty_extension_pack>
-    struct beast : public common_protocol<typename ServerTraitsType::traits_type, App, EList> {
-        using server_traits_type = SeverTraitsType;
-        using traits_type        = typename server_traits_type::traits_type;
-        using application_type   = App;
-        using protocol_type      = beast<traits_type, application_type, extension_list>;
-        using server_type        = beast_proto::beast_server<traits_type, request_type>;
+    template <Application App, Traits TraitsType = default_traits, ExtensionList EList = empty_extension_pack>
+    struct beast : public common_http_protocol<TraitsType, App, EList> {
+        using traits_type      = TraitsType;
+        using application_type = App;
+        using extension_list   = EList;
+        using protocol_type    = beast<application_type, traits_type, extension_list>;
+        using server_type      = beast_proto::beast_server<traits_type, extension_list>;
+        using app_wrapper_type = http_app_wrapper<traits_type, application_type>;
 
         server_type      server;
         app_wrapper_type app;
 
-        // todo: SSL options and other options go here
+      private:
+        using super = common_http_protocol<TraitsType, App, EList>;
 
-        template <typename AllocType = allocator_type>
-        beast(logger_ref logger = logger_type{}, AllocType const& alloc = AllocType{})
-          : etraits{logger, alloc},
-            server{logger, alloc},
-            app{logger, alloc} {}
+      public:
+        template <typename... Args>
+        beast(Args&&... args) : super{stl::forward<Args>(args)...},
+                                server{*this},
+                                app{*this} {}
 
-        [[nodiscard]] static constexpr bool is_ssl_available() const noexcept {
+        [[nodiscard]] static constexpr bool is_ssl_available() noexcept {
             return false; // todo: change this
         }
 
