4a1f3a40e82045e906313bb882457dca86a582aa
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-02-22 10:28:05 -0800

    using raw loops instead of resize because of copying; #67

--- core/include/webpp/http/protocols/beast_proto/beast_server.hpp
+++ core/include/webpp/http/protocols/beast_proto/beast_server.hpp
@@ -253,9 +253,10 @@ namespace webpp::http::beast_proto {
             pool{concurrency_hint - 1}, // the main thread is one thread itself
             pool_count{concurrency_hint},
             app_ref{the_app_ref} {
-
-            // initialize N http workers
-            workers.resize(pool_count, {*this, this->alloc_pack.template local_allocator<char>()});
+            for (stl::size_t index = 0ul; index != pool_count; ++index) {
+                workers.emplace_back(*this, this->alloc_pack.template local_allocator<char>());
+                workers.back().start();
+            }
         }
 
 
