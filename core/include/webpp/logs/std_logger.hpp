5ac832095decb6f3cadd4c11a2c4e078f9443a5d
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-03-05 05:39:20 -0800

    std_logger.debug; #75

--- core/include/webpp/logs/std_logger.hpp
+++ core/include/webpp/logs/std_logger.hpp
@@ -18,20 +18,24 @@ namespace webpp {
      * A logger class
      * @tparam stream_getter is a callable that gets a FILE* (like stderr, stdin, stdout)
      */
-    template <auto stream_getter>
+    template <auto stream_getter, bool IsDebug = false>
     struct std_logger {
         using logger_type = std_logger;
         using logger_ref  = logger_type const; // copy the logger, there's nothing to copy
         using logger_ptr  = logger_type*;      // there's a syntax difference, so we can't copy
 
-        static constexpr bool enabled               = true; // todo: make this configurable by the user
-        static constexpr auto default_category_name = "Default";
+        static constexpr bool enabled = true; // todo: make this configurable by the user
+#if defined(DEBUG) && DEBUG
+        static constexpr bool is_debug = true;
+#else
+        static constexpr bool is_debug = false;
+#endif
+        static constexpr auto default_category_name = is_debug ? "Debug" : "Default";
 
         enum struct logging_type : stl::uint_fast8_t { debug, info, warning, error, critical, unknown };
 
         static constexpr auto logging_type_to_string(logging_type lt) noexcept {
             switch (lt) {
-                case logging_type::debug: return "DEBUG";
                 case logging_type::info: return "INFO";
                 case logging_type::warning: return "WARNING";
                 case logging_type::error: return "ERROR";
@@ -48,78 +52,81 @@ namespace webpp {
         void log(logging_type                   lt,
                  istl::StringViewifiable auto&& category,
                  istl::StringViewifiable auto&& details) const noexcept {
+            if constexpr (!is_debug) {
 #ifdef WEBPP_FMT_LIB
-            fmt::print(stream_getter(),
-                       "[{}, {}]: {}\n",
-                       logging_type_to_string(lt),
-                       stl::forward<decltype(category)>(category),
-                       stl::forward<decltype(details)>(details));
+                fmt::print(stream_getter(),
+                           "[{}, {}]: {}\n",
+                           logging_type_to_string(lt),
+                           stl::forward<decltype(category)>(category),
+                           stl::forward<decltype(details)>(details));
 #else
-            stl::printf(stream_getter(),
-                        "[%s, %s]: %s\n",
-                        logging_type_to_string(lt),
-                        stl::forward<decltype(category)>(category),
-                        stl::forward<decltype(details)>(details));
+                stl::printf(stream_getter(),
+                            "[%s, %s]: %s\n",
+                            logging_type_to_string(lt),
+                            stl::forward<decltype(category)>(category),
+                            stl::forward<decltype(details)>(details));
 #endif
+            }
         }
 
-#define WEBPP_LOGGER_SHORTCUT(logging_name)                                                                 \
-                                                                                                            \
-    void logging_name(istl::StringViewifiable auto&& details) const noexcept {                              \
-        return log(logging_type::logging_name,                                                              \
-                   default_category_name,                                                                   \
-                   stl::forward<decltype(details)>(details));                                               \
-    }                                                                                                       \
-                                                                                                            \
-    void logging_name(istl::StringViewifiable auto&& category, istl::StringViewifiable auto&& details)      \
-      const noexcept {                                                                                      \
-        return log(logging_type::logging_name,                                                              \
-                   stl::forward<decltype(category)>(category),                                              \
-                   stl::forward<decltype(details)>(details));                                               \
-    }                                                                                                       \
-                                                                                                            \
-    void logging_name(istl::StringViewifiable auto&& category,                                              \
-                      istl::StringViewifiable auto&& details,                                               \
-                      stl::error_code const&         ec) const noexcept {                                           \
-        stl::size_t space_count =                                                                           \
-          6 + logging_type_string_size(logging_type::logging_name) + istl::string_viewify(category).size(); \
-        auto old_details = istl::string_viewify(stl::forward<decltype(details)>(details));                  \
-        auto new_details = fmt::format("{2}\n{1: >{0}}error message: {3}",                                  \
-                                       stl::move(space_count),                                              \
-                                       "",                                                                  \
-                                       old_details,                                                         \
-                                       ec.message());                                                       \
-        return log(logging_type::logging_name,                                                              \
-                   stl::forward<decltype(category)>(category),                                              \
-                   stl::move(new_details));                                                                 \
-    }                                                                                                       \
-                                                                                                            \
-    void logging_name(istl::StringViewifiable auto&& category,                                              \
-                      istl::StringViewifiable auto&& details,                                               \
-                      stl::exception const&          ex) const noexcept {                                            \
-        stl::size_t space_count =                                                                           \
-          6 + logging_type_string_size(logging_type::logging_name) + istl::string_viewify(category).size(); \
-        auto old_details = istl::string_viewify(stl::forward<decltype(details)>(details));                  \
-        auto new_details = fmt::format("{2}\n{1: >{0}}error message: {3}",                                  \
-                                       stl::move(space_count),                                              \
-                                       "",                                                                  \
-                                       old_details,                                                         \
-                                       ex.what());                                                          \
-        return log(logging_type::logging_name,                                                              \
-                   stl::forward<decltype(category)>(category),                                              \
-                   stl::move(new_details));                                                                 \
-    }                                                                                                       \
-                                                                                                            \
-    void logging_name(istl::StringViewifiable auto&& details, stl::error_code const& ec) const noexcept {   \
-        return logging_name(default_category_name, stl::forward<decltype(details)>(details), ec);           \
-    }                                                                                                       \
-                                                                                                            \
-    void logging_name(istl::StringViewifiable auto&& details, stl::exception const& ex) const noexcept {    \
-        return logging_name(default_category_name, stl::forward<decltype(details)>(details), ex);           \
+#define WEBPP_LOGGER_SHORTCUT(logging_name)                                                               \
+                                                                                                          \
+    void logging_name(istl::StringViewifiable auto&& details) const noexcept {                            \
+        log(logging_type::logging_name, default_category_name, stl::forward<decltype(details)>(details)); \
+    }                                                                                                     \
+                                                                                                          \
+    void logging_name(istl::StringViewifiable auto&& category, istl::StringViewifiable auto&& details)    \
+      const noexcept {                                                                                    \
+        log(logging_type::logging_name,                                                                   \
+            stl::forward<decltype(category)>(category),                                                   \
+            stl::forward<decltype(details)>(details));                                                    \
+    }                                                                                                     \
+                                                                                                          \
+    void logging_name(istl::StringViewifiable auto&& category,                                            \
+                      istl::StringViewifiable auto&& details,                                             \
+                      stl::error_code const&         ec) const noexcept {                                         \
+        if constexpr (!is_debug) {                                                                        \
+            stl::size_t space_count = 6 + logging_type_string_size(logging_type::logging_name) +          \
+                                      istl::string_viewify(category).size();                              \
+            auto old_details = istl::string_viewify(stl::forward<decltype(details)>(details));            \
+            auto new_details = fmt::format("{2}\n{1: >{0}}error message: {3}",                            \
+                                           stl::move(space_count),                                        \
+                                           "",                                                            \
+                                           old_details,                                                   \
+                                           ec.message());                                                 \
+            log(logging_type::logging_name,                                                               \
+                stl::forward<decltype(category)>(category),                                               \
+                stl::move(new_details));                                                                  \
+        }                                                                                                 \
+    }                                                                                                     \
+                                                                                                          \
+    void logging_name(istl::StringViewifiable auto&& category,                                            \
+                      istl::StringViewifiable auto&& details,                                             \
+                      stl::exception const&          ex) const noexcept {                                          \
+        if constexpr (!is_debug) {                                                                        \
+            stl::size_t space_count = 6 + logging_type_string_size(logging_type::logging_name) +          \
+                                      istl::string_viewify(category).size();                              \
+            auto old_details = istl::string_viewify(stl::forward<decltype(details)>(details));            \
+            auto new_details = fmt::format("{2}\n{1: >{0}}error message: {3}",                            \
+                                           stl::move(space_count),                                        \
+                                           "",                                                            \
+                                           old_details,                                                   \
+                                           ex.what());                                                    \
+            log(logging_type::logging_name,                                                               \
+                stl::forward<decltype(category)>(category),                                               \
+                stl::move(new_details));                                                                  \
+        }                                                                                                 \
+    }                                                                                                     \
+                                                                                                          \
+    void logging_name(istl::StringViewifiable auto&& details, stl::error_code const& ec) const noexcept { \
+        return logging_name(default_category_name, stl::forward<decltype(details)>(details), ec);         \
+    }                                                                                                     \
+                                                                                                          \
+    void logging_name(istl::StringViewifiable auto&& details, stl::exception const& ex) const noexcept {  \
+        return logging_name(default_category_name, stl::forward<decltype(details)>(details), ex);         \
     }
 
 
-        WEBPP_LOGGER_SHORTCUT(debug)
         WEBPP_LOGGER_SHORTCUT(info)
         WEBPP_LOGGER_SHORTCUT(warning)
         WEBPP_LOGGER_SHORTCUT(error)
@@ -127,6 +134,8 @@ namespace webpp {
         WEBPP_LOGGER_SHORTCUT(unkown)
 
 #undef WEBPP_LOGGER_SHORTCUT
+
+        [[no_unique_address]] std_logger<stream_getter, true> debug;
     };
 
     inline auto stderr_functor() noexcept {
