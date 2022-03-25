b014404fac3cf7c6f5af6efa84c463221841d494
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-03-25 03:20:37 -0700

    memory gate map type; #64

--- core/include/webpp/storage/memory_gate.hpp
+++ core/include/webpp/storage/memory_gate.hpp
@@ -5,9 +5,21 @@
 
 namespace webpp {
 
-    template <Traits TraitsType = default_traits>
     struct memory_gate {
-        using traits_type = TraitsType;
+        template <typename KeyT, typename ValueT, Traits TraitsType = default_traits>
+        struct storage_gate {
+            using key_type           = KeyT;
+            using value_type         = ValueT;
+            using traits_type        = TraitsType;
+            using map_pair_type      = stl::pair<const key_type, value_type>;
+            using map_allocator_type = traits::general_allocator<traits_type, map_pair_type>;
+            using map_type = stl::map<key_type, value_type, stl::less<key_type>, map_allocator_type>;
+
+
+            void set(key_type const& key, value_type const& value) {}
+
+            map_type map;
+        };
     };
 
 } // namespace webpp
