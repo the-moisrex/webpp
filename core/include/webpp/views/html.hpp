e5f3f8f70d376b89287ac5aae9d01323aaf9c106
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-04-12 01:39:46 -1000

    html_escape; #101

---
+++ core/include/webpp/views/html.hpp
@@ -0,0 +1,30 @@
+// Created by moisrex on 12/04/22.
+
+#ifndef WEBPP_HTML_HPP
+#define WEBPP_HTML_HPP
+
+#include "../std/string.hpp"
+#include "../std/string_view.hpp"
+
+namespace webpp {
+
+    template <istl::String StrT, istl::StringViewifiable StrVT>
+    static constexpr StrT html_escape(StrVT&& input, StrT& out) {
+        // todo: consider using SIMD if it's not being optimized
+        out.reserve(s.size() * 2);
+        for (const auto ch : input) {
+            switch (ch) {
+                case '&': out.append({'&', 'a', 'm', 'p', ';'}); break;
+                case '<': out.append({'&', 'l', 't', ';'}); break;
+                case '>': out.append({'&', 'g', 't', ';'}); break;
+                case '\"': out.append({'&', 'q', 'u', 'o', 't', ';'}); break;
+                case '\'': out.append({'&', 'a', 'p', 'o', 's', ';'}); break;
+                default: out.append(1, ch); break;
+            }
+        }
+        return out;
+    }
+
+} // namespace webpp
+
+#endif // WEBPP_HTML_HPP
