633e27c727f00a1584084ac1790a3dada574bde7
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-05-03 13:22:10 -0700

    std::span + is_span

---
+++ core/include/webpp/std/span.hpp
@@ -0,0 +1,32 @@
+#ifndef WEBPP_STD_SPAN_HPP
+#define WEBPP_STD_SPAN_HPP
+
+
+#include "./std.hpp"
+
+#include <span>
+
+
+namespace webpp::istl {
+
+    template <typename>
+    struct is_span {
+        static constexpr bool value = false;
+    };
+
+    template <template <typename, stl::size_t> typename SpanT, typename T, stl::size_t Extent>
+    struct is_span<SpanT<T, Extent>> {
+        static constexpr bool value = true;
+    };
+
+    template <typename T>
+    static constexpr bool is_span_v = is_span<T>::value;
+
+    template <typename T>
+    concept Span = is_span_v<T>;
+
+    // todo: add SpanOf types
+
+} // namespace webpp::istl
+
+#endif
