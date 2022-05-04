2095c71680f2b69a61b6e627d40035dbd7c93abc
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-05-04 13:11:29 -0700

    a little database tests

--- tests/database_test.cpp
+++ tests/database_test.cpp
@@ -4,8 +4,36 @@
 
 
 using namespace webpp;
-using namespace webpp::database;
+using namespace webpp::sql;
 
-using qbuilder = query_builder<std_traits>;
 
-TEST(Database, QueryBuilder) {}
+TEST(Database, SQLiteWrapper) {
+    database<sqlite> db; // in memory database
+    db.execute(R"sql(create table settings(
+        id integer primary key,
+        name text,
+        value text
+    ))sql");
+
+    auto stmt = db.prepare("insert into settings (name, value) values (?, ?)");
+    stmt.bind(0, "username");
+    stmt.bind(1, "moisrex");
+    stmt.execute();
+
+
+    stmt = db.prepare("select * from settings;");
+    while (stmt.step()) {
+        stl::uint64_t id    = stmt.column(0);
+        stl::string   name  = stmt.column(1);
+        stl::string   value = stmt.column(2);
+        EXPECT_EQ(name, "username");
+        EXPECT_EQ(value, "moisrex");
+    }
+
+    stmt = db.prepare("select * from settings;");
+    for (auto [id, name, value] : stmt) {
+        EXPECT_EQ(name, "username");
+        EXPECT_EQ(value, "moisrex");
+    }
+}
+
