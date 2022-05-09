9fe477421e7ad6687f6535be5c60ccc47b8f2837
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-05-09 15:12:02 -0700

    sql row init; #130

---
+++ core/include/webpp/db/sql_row.hpp
@@ -0,0 +1,11 @@
+#ifndef WEBPP_DATABASE_SQL_ROW_HPP
+#define WEBPP_DATABASE_SQL_ROW_HPP
+
+
+namespace webpp::sql {
+
+    struct sql_row {};
+
+} // namespace webpp::sql
+
+#endif // WEBPP_DATABASE_SQL_ROW_HPP
