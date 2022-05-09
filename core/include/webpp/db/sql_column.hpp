1c72532723d8d391a4c12316bcd820d27cab5629
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-05-09 15:55:17 -0700

    sql column init; #130

---
+++ core/include/webpp/db/sql_column.hpp
@@ -0,0 +1,9 @@
+#ifndef WEBPP_DATABASE_SQL_COLUMN_HPP
+#define WEBPP_DATABASE_SQL_COLUMN_HPP
+
+namespace webpp::sql {
+
+    struct sql_column {};
+} // namespace webpp::sql
+
+#endif // WEBPP_DATABASE_SQL_COLUMN_HPP
