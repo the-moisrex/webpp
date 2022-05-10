73073a89b074577aa55a206292e4ed61ef412cde
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-05-10 14:16:54 -0700

    rename column to cell; #130

---
+++ core/include/webpp/db/sql_cell.hpp
@@ -0,0 +1,14 @@
+#ifndef WEBPP_DATABASE_SQL_CELL_HPP
+#define WEBPP_DATABASE_SQL_CELL_HPP
+
+namespace webpp::sql {
+
+    struct sql_cell {
+      private:
+        statement_type* stmt;
+
+      public:
+    };
+} // namespace webpp::sql
+
+#endif // WEBPP_DATABASE_SQL_CELL_HPP
--- core/include/webpp/db/sql_column.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef WEBPP_DATABASE_SQL_COLUMN_HPP
-#define WEBPP_DATABASE_SQL_COLUMN_HPP
-
-namespace webpp::sql {
-
-    struct sql_column {};
-} // namespace webpp::sql
-
-#endif // WEBPP_DATABASE_SQL_COLUMN_HPP
