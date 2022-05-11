fc3f6207acbf8d3823d0ed2840937e8bc05bc22e
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-05-11 14:36:36 -0700

    sql row template; #130

--- core/include/webpp/db/sql_row.hpp
+++ core/include/webpp/db/sql_row.hpp
@@ -1,10 +1,21 @@
 #ifndef WEBPP_DATABASE_SQL_ROW_HPP
 #define WEBPP_DATABASE_SQL_ROW_HPP
 
+#include "sql_cell.hpp"
 
 namespace webpp::sql {
 
-    struct sql_row {};
+    template <SQLStatement SQLStmtType>
+    struct sql_row {
+        using statement_type = SQLStmtType;
+        using cell_type      = sql_cell<statement_type>;
+
+      private:
+      public:
+        cell_type operator[]() {
+            //
+        }
+    };
 
 } // namespace webpp::sql
 
