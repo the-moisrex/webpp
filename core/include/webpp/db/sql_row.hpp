491aff6803b218eb3311953137f7c4e6b89d3564
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-05-11 16:15:57 -0700

    cell iteratable; #130

--- core/include/webpp/db/sql_row.hpp
+++ core/include/webpp/db/sql_row.hpp
@@ -6,11 +6,33 @@
 namespace webpp::sql {
 
     template <SQLStatement SQLStmtType>
+    struct cell_iterator {};
+
+
+    template <SQLStatement SQLStmtType>
+    struct cell_iteratable {
+        using statement_type     = SQLStmtType;
+        using cell_iterator_type = cell_iterator<statement_type>;
+
+        cell_iterator_type begin() const noexcept {
+            return {};
+        }
+
+
+        cell_iterator_type end() const noexcept {
+            return {};
+        }
+    };
+
+
+    template <SQLStatement SQLStmtType>
     struct sql_row {
         using statement_type = SQLStmtType;
         using cell_type      = sql_cell<statement_type>;
 
       private:
+        statement_type stmt;
+
       public:
         cell_type operator[]() {
             //
