18efe6104f2b1f1abc6e5b1ab4971206f119039a
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-05-14 16:14:59 -0700

    stmt; #127

--- core/include/webpp/db/sql_statement.hpp
+++ core/include/webpp/db/sql_statement.hpp
@@ -1,24 +1,23 @@
 #ifndef WEBPP_DATABASE_SQL_STATEMENT_HPP
 #define WEBPP_DATABASE_SQL_STATEMENT_HPP
 
+#include "../common/meta.hpp"
+#include "../memory/object.hpp"
+#include "../traits/enable_traits.hpp"
 #include "sql_concepts.hpp"
 
 namespace webpp::sql {
-    template <SQLStatement StmtType>
-    struct sql_statement;
-
-    template <SQLStatement StmtType>
+    template <typename StmtType>
     struct sql_statement_binder {
-        using statement_type     = StmtType;
-        using size_type          = typename statement_type::size_type;
-        using sql_statement_type = sql_statement<statement_type>;
+        using statement_type = StmtType;
+        using size_type      = typename statement_type::size_type;
 
       private:
-        sql_statement_type& stmt;
-        size_type           index = 0; // index of the column
+        statement_type& stmt;
+        size_type       index = 0; // index of the column
 
       public:
-        constexpr sql_statement_binder(sql_statement_type& stmt_ref, size_type column_index) noexcept
+        constexpr sql_statement_binder(statement_type& stmt_ref, size_type column_index) noexcept
           : stmt{stmt_ref},
             index{column_index} {}
 
@@ -28,20 +27,28 @@ namespace webpp::sql {
             stmt.bind(index, stl::forward<T>(value));
             return *this;
         }
+
+
+        template <typename T>
+        [[nodiscard]] inline sql_statement_binder& operator<<(T&& val) noexcept {
+            stmt.bind(index++, stl::forward<T>(val));
+            return *this;
+        }
     };
 
 
     /**
      * A wrapper for the driver's statement type. This is what the user uses directly.
      */
-    template <SQLStatement StmtType>
-    struct sql_statement : StmtType {
+    template <Traits TraitsType, SQLStatement StmtType>
+    struct sql_statement : StmtType, enable_traits<TraitsType> {
+        using traits_type      = TraitsType;
         using driver_stmt_type = StmtType;
         using size_type        = typename driver_stmt_type::size_type;
-        using binder_type      = sql_statement_binder<driver_stmt_type>;
+        using binder_type      = sql_statement_binder<sql_statement>;
+        using string_type      = traits::general_string<traits_type>;
 
-      private:
-      public:
+
         /**
          * Possible values that get passed down:
          *   - string_view
@@ -49,19 +56,29 @@ namespace webpp::sql {
          */
         template <typename T>
         sql_statement& bind(size_type index, T&& val) noexcept {
-            if constexpr (requires { stmt->bind(index, stl::forward<T>(val)); }) {
-                stmt->bind(index, stl::forward<T>(val));
+            auto errmsg = object::make_general<string_type>(*this);
+            if constexpr (requires { this->bind(index, stl::forward<T>(val), errmsg); }) {
+                this->bind(index, stl::forward<T>(val), errmsg);
             } else if constexpr (istl::StringViewifiable<T>) {
                 //
             } else {
                 static_assert_false(T, "Don't know how to bind the value, unknown type specified.");
             }
+            if (!errmsg.empty()) {
+                this->logger.error("SQLStmt", errmsg);
+            }
             return *this;
         }
 
 
-        inline binder_type operator[](size_type index) noexcept {
+        [[nodiscard]] inline binder_type operator[](size_type index) noexcept {
             return {*this, index};
+        }
+
+        template <typename T>
+        [[nodiscard]] inline binder_type operator<<(T&& val) noexcept {
+            bind(0, stl::forward<T>(val));
+            return binder_type{*this, 1};
         }
     };
 
