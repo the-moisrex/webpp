7d3fb4ddea81f93ff8c52ebc8b0d21c0b4760362
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-06-01 17:12:35 -0700

    sqlite; #125

---
+++ core/include/webpp/db/sqlite/sqlite.hpp
@@ -0,0 +1,12 @@
+#ifndef WEBPP_SQL_SQLITE_SQLITE_HPP
+#define WEBPP_SQL_SQLITE_SQLITE_HPP
+
+namespace webpp::sql {
+
+    struct sqlite {
+        using grammar_type = sqlite_grammar;
+    };
+
+} // namespace webpp::sql
+
+#endif
