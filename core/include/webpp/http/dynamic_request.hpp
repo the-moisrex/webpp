5101a6d457c044d66dc5d891c53e36af2bc6f354
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-10-25 17:21:20 -0700

    Dynamic request; #180

--- core/include/webpp/http/dynamic_request.hpp
+++ core/include/webpp/http/dynamic_request.hpp
@@ -4,6 +4,7 @@
 #define WEBPP_DYNAMIC_REQUEST_HPP
 
 #include "../traits/default_traits.hpp"
+#include "./protocols/beast_proto/beast_request.hpp"
 #include "./protocols/cgi_proto/cgi_request.hpp"
 #include "./version.hpp"
 
@@ -13,20 +14,39 @@ namespace webpp::http {
 
     /**
      * This request type can hold other HTTP request types.
-     * @tparam TraitsType
      */
-    template <Traits TraitsType, typename CommonRequestParent>
-    struct dynamic_request : public CommonRequestParent {
-        using traits_type           = TraitsType;
-        using common_request_parent = CommonRequestParent;
+    template <typename CommonRequestType, template <typename> typename... RequestTypes>
+    struct basic_dynamic_request : public CommonRequestType {
+        using common_request_type = CommonRequestType;
+        using traits_type         = typename common_request_type::traits_type;
 
       private:
-        using super = common_request_parent;
+        using super = common_request_type;
+        stl::variant<RequestTypes<CommonRequestType>*...> reqvar;
+
+#define call_req(mem, ...)                                      \
+    stl::visit(                                                 \
+      [](auto* req) noexcept(noexcept(req->mem(__VA_ARGS__))) { \
+          return req->mem(__VA_ARGS__);                         \
+      },                                                        \
+      reqvar)
+
 
       public:
-        http::version version() const noexcept {
-            // todo
+        // get the HTTP version of the request
+        [[nodiscard]] http::version version() const noexcept {
+            return call_req(version);
         }
+
+#undef call_req
+    };
+
+
+    template <typename CommonRequestParent>
+    struct dynamic_request
+      : basic_dynamic_request<CommonRequestParent, cgi_request, beast_proto::beast_request> {
+        using basic_dynamic_request<CommonRequestParent, cgi_request, beast_proto::beast_request>::
+          beast_dynamic_request;
     };
 } // namespace webpp::http
 
