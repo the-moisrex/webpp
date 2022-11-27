28b87526ddc5c9ecce823efa8114a05038f2a20f
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2022-11-27 08:21:02 -0800

    embedded file instead of cfile; #211

--- core/include/webpp/storage/file.hpp
+++ core/include/webpp/storage/file.hpp
@@ -1,5 +1,5 @@
-#ifndef WEBPP_META_CFILE
-#define WEBPP_META_CFILE
+#ifndef WEBPP_STORAGE_FILE_HPP
+#define WEBPP_STORAGE_FILE_HPP
 
 #include <array>
 #include <exception>
@@ -10,29 +10,32 @@
 /**
  * @brief The file class
  * @details This class is designed for constant files that will be produced from
- * source files on build-time. This class's only purpose is to work at
- * compile-time.
+ * source files on build-time. This class's only purpose is to work at compile-time.
  */
-class const_file {
-    std::string_view _file_path;
-    std::string_view _content;
+class embedded_file {
+  public:
+    using string_view_type = std::string_view;
+
+  private:
+    string_view_type _file_path;
+    string_view_type _content;
 
   public:
-    constexpr const_file(std::string_view const& file_path, std::string_view const& content) noexcept
+    constexpr embedded_file(string_view_type file_path, string_view_type content) noexcept
       : _file_path(file_path),
         _content(content) {}
 
-    [[nodiscard]] constexpr auto const& path() const noexcept {
+    [[nodiscard]] constexpr string_view_type path() const noexcept {
         return _file_path;
     }
 
-    [[nodiscard]] constexpr auto const& content() const noexcept {
+    [[nodiscard]] constexpr string_view_type content() const noexcept {
         return _content;
     }
 
-    [[nodiscard]] static constexpr std::optional<const_file> search(std::string_view const& path) noexcept {
-        constexpr std::array<const_file, 2> files{const_file("file.json", "{\"value\": 10}"),
-                                                  const_file("config.json", "{}")};
+    [[nodiscard]] static constexpr std::optional<embedded_file> search(string_view_type path) noexcept {
+        constexpr std::array<embedded_file, 2> files{embedded_file("file.json", "{\"value\": 10}"),
+                                                     embedded_file("config.json", "{}")};
 
         for (auto const& f : files)
             if (f.path() == path)
@@ -42,4 +45,4 @@ class const_file {
     }
 };
 
-#endif // WEBPP_META_CFILE
+#endif // WEBPP_STORAGE_FILE_HPP
