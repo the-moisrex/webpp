c95bc7e8c61e27f9dbfd380787a909a10ffb5ba3
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-01-09 01:59:53 -0800

    reformat script added

---
+++ reformat.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if [ -f /usr/bin/clang-format ]; then
+    formatter="/usr/bin/clang-format";
+else
+    formatter="/usr/bin/env clang-format";
+fi
+
+git ls-files | grep -E "\.(hpp|cpp)$"  | xargs $formatter -i --verbose $@
