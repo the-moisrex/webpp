4a443c59dcb5bd528ea807bd107bdbd09f4be672
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-03-24 08:20:43 -0800

    Removing CTRE and eve's bloats

--- tests/configs_test.cpp
+++
@@ -1,9 +0,0 @@
-// Created by moisrex on 16/1/22.
-
-#include "../webpp/configs/configs.hpp"
-
-#include "common_pch.hpp"
-
-using namespace webpp;
-
-TEST(ConfigsTests, ConfigCreation) {}
--- webpp/webpp.hpp
+++ webpp/webpp.hpp
@@ -1,19 +1,19 @@
-#ifndef WEBPP_WEBPP_H
-#define WEBPP_WEBPP_H
+#ifndef WEBPP_WEBPP_HPP
+#define WEBPP_WEBPP_HPP
 
-#include "webpp/http/bodies/string.hpp"
-#include "webpp/http/header_fields.hpp"
-#include "webpp/http/http.hpp"
-#include "webpp/http/request.hpp"
-#include "webpp/http/response.hpp"
-#include "webpp/http/response_body.hpp"
-#include "webpp/http/routes/context.hpp"
-#include "webpp/http/routes/methods.hpp"
-#include "webpp/http/routes/path.hpp"
-#include "webpp/http/routes/route.hpp"
-#include "webpp/http/routes/router.hpp"
-#include "webpp/http/routes/tpath.hpp"
+#include "http/bodies/string.hpp"
+#include "http/header_fields.hpp"
+#include "http/http.hpp"
+#include "http/request.hpp"
+#include "http/response.hpp"
+#include "http/response_body.hpp"
+#include "http/routes/context.hpp"
+#include "http/routes/methods.hpp"
+#include "http/routes/path.hpp"
+#include "http/routes/route.hpp"
+#include "http/routes/router.hpp"
+#include "http/routes/tpath.hpp"
 
 // todo: optimize the includes and also include others as well
 
-#endif // WEBPP_WEBPP_H
+#endif // WEBPP_WEBPP_HPP
