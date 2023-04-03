08832ddb580533792a7ec4a88e1c981626157100
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-04-02 16:00:47 -0800

    Extracting dynamic route to its own file; #266

--- webpp/http/routes/dynamic_router.hpp
+++ webpp/http/routes/dynamic_router.hpp
@@ -11,85 +11,11 @@
 #include "../request.hpp"
 #include "../response.hpp"
 #include "../status_code.hpp"
-#include "./context.hpp"
-#include "valves.hpp"
+#include "dynamic_route.hpp"
 
 #include <any>
 
 namespace webpp::http {
-
-
-    template <Traits>
-    struct basic_dynamic_router;
-
-
-    template <Traits TraitsType, typename Callable = void>
-    struct dynamic_route final : dynamic_route<TraitsType, void> {
-        using traits_type   = TraitsType;
-        using string_type   = traits::general_string<traits_type>;
-        using context_type  = basic_context<traits_type>;
-        using callable_type = stl::remove_cvref_t<Callable>;
-        using router_type   = basic_dynamic_router<traits_type>;
-
-      private:
-        callable_type callable;
-
-      public:
-        dynamic_route(Callable&& new_callable) noexcept : callable(stl::move(new_callable)) {}
-        dynamic_route(Callable const& new_callable) noexcept(
-          stl::is_nothrow_copy_constructible_v<callable_type>)
-          : callable(new_callable) {}
-        dynamic_route(dynamic_route const&)                     = delete;
-        dynamic_route(dynamic_route&&) noexcept                 = default;
-        dynamic_route& operator=(dynamic_route const&) noexcept = delete;
-        dynamic_route& operator=(dynamic_route&&) noexcept      = default;
-        ~dynamic_route() final                                  = default;
-
-        void operator()(context_type& ctx) final {
-            callable(ctx);
-        }
-
-        void operator()(context_type& ctx, [[maybe_unused]] router_type& router) final {
-            if constexpr (stl::is_invocable_v<callable_type, context_type&, router_type&>) {
-                callable(ctx, router);
-            } else {
-                callable(ctx);
-            }
-        }
-
-        void to_string(string_type& out) const final {
-            valve_to_string(out, callable);
-        }
-
-
-        void setup([[maybe_unused]] router_type& router) final {
-            if constexpr (ValveRequiresSetup<router_type, callable_type>) {
-                callable.setup(router);
-            }
-        }
-    };
-
-
-    template <Traits TraitsType>
-    struct dynamic_route<TraitsType, void> {
-        using traits_type  = TraitsType;
-        using string_type  = traits::general_string<traits_type>;
-        using context_type = basic_context<traits_type>;
-        using router_type  = basic_dynamic_router<traits_type>;
-
-        dynamic_route()                                         = default;
-        dynamic_route(dynamic_route const&)                     = default;
-        dynamic_route(dynamic_route&&) noexcept                 = default;
-        dynamic_route& operator=(dynamic_route const&) noexcept = default;
-        dynamic_route& operator=(dynamic_route&&) noexcept      = default;
-
-        virtual ~dynamic_route() = default;
-
-        virtual void operator()(context_type& ctx, [[maybe_unused]] router_type& router) = 0;
-        virtual void operator()(context_type& ctx)                                       = 0;
-        virtual void to_string(string_type& out) const                                   = 0;
-        virtual void setup(router_type& out)                                             = 0;
-    };
 
 
     /**
