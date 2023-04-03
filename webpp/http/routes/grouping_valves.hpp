f741b19c8ba8e4940ab0b1357aa7018191a1e358
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-04-03 13:21:56 -0800

    Append method; #280

--- webpp/http/routes/grouping_valves.hpp
+++ webpp/http/routes/grouping_valves.hpp
@@ -163,6 +163,124 @@ namespace webpp::http {
         }
     };
 
+
+
+
+    template <typename... Pres, typename... Posts, typename... Manglers, typename... Routes>
+    struct valves_group<prerouting_valve<Pres...>,
+                        postrouting_valve<Posts...>,
+                        mangler_valve<Manglers...>,
+                        forward_valve<Routes...>> : valve<valves_group<prerouting_valve<Pres...>,
+                                                                       postrouting_valve<Posts...>,
+                                                                       mangler_valve<Manglers...>,
+                                                                       forward_valve<Routes...>>> {
+
+        using valve_type   = valve<valves_group>;
+        using pre_type     = prerouting_valve<Pres...>;
+        using post_type    = postrouting_valve<Posts...>;
+        using mangler_type = mangler_valve<Manglers...>;
+        using route_type   = forward_valve<Routes...>;
+
+      private:
+        [[no_unique_address]] pre_type     pres;
+        [[no_unique_address]] post_type    posts;
+        [[no_unique_address]] mangler_type manglers;
+        [[no_unique_address]] route_type   routes;
+
+      public:
+        template <istl::cvref_as<pre_type>     PreT,
+                  istl::cvref_as<post_type>    PostT,
+                  istl::cvref_as<mangler_type> ManglerT,
+                  istl::cvref_as<route_type>   RouteT>
+        constexpr valves_group(PreT&& inp_pre, PostT&& inp_post, ManglerT&& inp_mangler, RouteT&& inp_route)
+          : pres{stl::forward<PreT>(inp_pre)},
+            posts{stl::forward<PostT>(inp_post)},
+            manglers{stl::forward<ManglerT>(inp_mangler)},
+            routes{stl::forward<RouteT>(inp_route)} {}
+
+        constexpr valves_group(valves_group const&)                = default;
+        constexpr valves_group(valves_group&&) noexcept            = default;
+        constexpr valves_group& operator=(valves_group const&)     = default;
+        constexpr valves_group& operator=(valves_group&&) noexcept = default;
+        constexpr ~valves_group()                                  = default;
+
+
+        using valve_type::operator();
+
+        template <Traits TraitsType>
+        constexpr void operator()(basic_context<TraitsType>& ctx) {
+            using context_type   = basic_context<TraitsType>;
+            using pre_traits     = valve_traits<pre_type, context_type>;
+            using post_traits    = valve_traits<post_type, context_type>;
+            using mangler_traits = valve_traits<mangler_type, context_type>;
+            using route_traits   = valve_traits<route_type, context_type>;
+            pre_traits::call_set(pres, ctx);
+            route_traits::call_set(routes, ctx);
+            post_traits::call_set(posts, ctx);
+        }
+
+
+        template <typename RouterT>
+        constexpr void setup(RouterT& router) {
+            if constexpr (ValveRequiresSetup<RouterT, pre_type>) {
+                pres.setup(router);
+            }
+            if constexpr (ValveRequiresSetup<RouterT, mangler_type>) {
+                manglers.setup(router);
+            }
+            if constexpr (ValveRequiresSetup<RouterT, route_type>) {
+                routes.setup(router);
+            }
+            if constexpr (ValveRequiresSetup<RouterT, post_type>) {
+                posts.setup(router);
+            }
+        }
+
+        template <typename Callable>
+        [[nodiscard]] constexpr auto append_preroute(Callable&& callable) {
+            using callable_type = stl::remove_cvref_t<Callable>;
+            using new_pre_type  = prerouting_valve<Pres..., Callable>;
+            return valves_group<new_pre_type, post_type, mangler_type, route_type>{
+              new_pre_type{stl::tuple_cat(pres.as_tuple(), stl::forward_as_tuple<Callable>(callable))},
+              posts,
+              manglers,
+              routes};
+        }
+
+        template <typename Callable>
+        [[nodiscard]] constexpr auto append_postroute(Callable&& callable) {
+            using callable_type = stl::remove_cvref_t<Callable>;
+            using new_post_type = postrouting_valve<Posts..., Callable>;
+            return valves_group<pre_type, new_post_type, mangler_type, route_type>{
+              pres,
+              new_post_type{stl::tuple_cat(posts.as_tuple(), stl::forward_as_tuple<Callable>(callable))},
+              manglers,
+              routes};
+        }
+
+        template <typename Callable>
+        [[nodiscard]] constexpr auto append_route(Callable&& callable) {
+            using callable_type  = stl::remove_cvref_t<Callable>;
+            using new_route_type = forward_valve<Routes..., Callable>;
+            return valves_group<pre_type, post_type, mangler_type, new_route_type>{
+              pres,
+              posts,
+              manglers,
+              new_post_type{stl::tuple_cat(routes.as_tuple(), stl::forward_as_tuple<Callable>(callable))}};
+        }
+
+
+        constexpr void to_string(istl::String auto& out) const {
+            out.append("- (");
+            valve_to_string(out, pres);
+            out.append(")");
+            valve_to_string(out, routes);
+            out.append("+ (");
+            valve_to_string(out, posts);
+            out.append(")");
+        }
+    };
+
 } // namespace webpp::http
 
 #endif // WEBPP_GROUPING_VALVES_HPP
