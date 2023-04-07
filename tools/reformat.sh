416ce088ff0bfba150af584b1854ca8b12c88430
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-04-07 13:54:10 -0800

    reformat --staged

--- tools/reformat.sh
+++ tools/reformat.sh
@@ -6,4 +6,22 @@ else
     formatter="/usr/bin/env clang-format";
 fi
 
-git ls-files $@ | grep -E "\.(hpp|cpp)$"  | xargs "$formatter" -i --verbose --Wno-error=unknown
+formatter="$formatter git --no-pager diff --name-only --staged"
+
+function reformat {
+  git ls-files $@ | grep -E "\.(hpp|cpp)$"  | xargs "$formatter"
+}
+
+function reformat_staged {
+  git --no-pager diff --name-only --staged | grep -E "\.(hpp|cpp)$"  | xargs "$formatter"
+}
+
+case $1 in
+  --staged)
+    shift;
+    reformat_staged
+    ;;
+  *)
+    reformat;
+    ;;
+esac
