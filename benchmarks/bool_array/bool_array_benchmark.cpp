c828782406c09e168d11902b1ea2a934b3936902
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-04-26 05:23:08 -0800

    bool-array vs char-array bench

---
+++ benchmarks/bool_array/bool_array_benchmark.cpp
@@ -0,0 +1,65 @@
+#include "../benchmark.hpp"
+
+#include <array>
+
+using namespace std;
+
+static constexpr const char token_char_map[]{
+  "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+  "\0\1\0\1\1\1\1\1\0\0\1\1\0\1\1\0\1\1\1\1\1\1\1\1\1\1\0\0\0\0\0\0"
+  "\0\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\0\0\0\1\1"
+  "\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\0\1\0\1\0"
+  "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+  "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+  "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
+  "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"};
+
+
+static constexpr bool is_token(char item) {
+    return static_cast<bool>(token_char_map[static_cast<unsigned char>(item)]);
+}
+
+static constexpr array<bool, 256> bool_token_char_map{
+  {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+   false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+   false, false, false, true,  false, true,  true,  true,  true,  true,  false, false, true,  true,  false,
+   true,  true,  false, true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  false, false,
+   false, false, false, false, false, true,  true,  true,  true,  true,  true,  true,  true,  true,  true,
+   true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,
+   true,  false, false, false, true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,
+   true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,  true,
+   true,  true,  true,  false, true,  false, true,  false, false, false, false, false, false, false, false,
+   false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+   false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+   false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+   false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+   false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+   false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+   false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+   false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+   false}};
+
+
+static constexpr bool bool_is_token(char item) {
+    return token_char_map[static_cast<unsigned char>(item)];
+}
+
+
+static void BoolTokenMap(benchmark::State& state) {
+    auto str = str_generator();
+    for (auto _ : state) {
+        for (auto ch : str)
+            benchmark::DoNotOptimize(bool_is_token(ch));
+    }
+}
+BENCHMARK(BoolTokenMap);
+
+
+static void CharTokenMap(benchmark::State& state) {
+    auto str = str_generator();
+    for (auto _ : state) {
+        for (auto ch : str)
+            benchmark::DoNotOptimize(is_token(ch));
+    }
+}
+BENCHMARK(CharTokenMap);
