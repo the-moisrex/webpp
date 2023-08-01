37681c569636536309269e0040be015dcbb2e4fd
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-07-31 22:19:36 -0800

    renaming

--- webpp/views/file_view.hpp
+++ webpp/views/file_view.hpp
@@ -1,7 +1,7 @@
 // Created by moisrex on 14/04/22.
 
-#ifndef WEBPP_HTML_VIEW_HPP
-#define WEBPP_HTML_VIEW_HPP
+#ifndef WEBPP_VIEWS_FILE_VIEW_HPP
+#define WEBPP_VIEWS_FILE_VIEW_HPP
 
 #include "../std/string.hpp"
 #include "view_concepts.hpp"
@@ -74,4 +74,4 @@ namespace webpp::views {
 
 } // namespace webpp::views
 
-#endif // WEBPP_HTML_VIEW_HPP
+#endif // WEBPP_VIEWS_FILE_VIEW_HPP
