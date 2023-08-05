6ebc313d7afe0e3634a620863dfb074bee3e8348
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-08-05 05:10:19 -0800

    self host changes

--- webpp/hub/self_hosted.hpp
+++ webpp/hub/self_hosted.hpp
@@ -7,8 +7,6 @@
 #include "../http/protocol/common_http_protocol.hpp"
 #include "../http/request.hpp"
 #include "../http/response.hpp"
-#include "../server/default_server_traits.hpp"
-#include "../server/server_concepts.hpp"
 #include "../std/string_view.hpp"
 #include "self_hosted_request.hpp"
 #include "self_hosted_session_manager.hpp"
@@ -17,27 +15,22 @@ namespace webpp {
 
     template <Application App, Traits TraitsType = default_traits>
     struct host : public common_http_protocol<TraitsType, App> {
-
-        using server_traits_type = ServerTraitsType;
-        using traits_type        = typename server_traits_type::traits_type;
+        using traits_type = TraitsType;
 
       private:
         using super = common_http_protocol<traits_type, App>;
 
       public:
         using app_wrapper_type = typename super::app_wrapper_type;
-        using connection_type  = istl::nothing_type; // fixme: just a placeholder
         using request_type     = typename super::request_type;
-        using server_type      = typename server_traits_type::template server_type<
-          shosted::self_hosted_session_manager<traits_type, app_wrapper_type, request_type, connection_type>>;
 
         server_type server;
 
         // todo: SSL options and other options go here
 
         template <typename... Args>
-        self_hosted(Args&&... args) : super{stl::forward<Args>(args)...},
-                                      server{this->app} {}
+        host(Args&&... args) : super{stl::forward<Args>(args)...},
+                               server{this->app} {}
 
         [[nodiscard]] static constexpr bool is_ssl_available() noexcept {
             return false; // todo: change this
