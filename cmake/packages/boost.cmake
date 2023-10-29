a770d224b26496e1094d2c1b816ec4e1bddde41c
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-10-29 00:30:38 -0800

    updating boost to 83.0.0

--- cmake/packages/boost.cmake
+++ cmake/packages/boost.cmake
@@ -1,31 +1,14 @@
-set(Boost_USE_MULTITHREAD ON)
-find_package(Boost REQUIRED COMPONENTS container)
+set(Boost_USE_STATIC_LIBS OFF)
+set(Boost_USE_MULTITHREADED ON)
+set(Boost_USE_STATIC_RUNTIME OFF)
+find_package(Boost COMPONENTS)
 
-if (Boost_FOUND)
+if (Boost_FOUND AND TARGET Boost::boost)
     add_library(Boost::asio ALIAS Boost::boost)
 else()
     include(CPM)
 
-
-    # Get the download links from here: https://www.boost.org/users/download/
-    # This is not a good way since the archive file is about 125MiB
-    #if (UNIX)
-    #    CPMAddPackage(
-    #            NAME boost
-    #            URL https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.gz
-    #            SHA256_HASH 5347464af5b14ac54bb945dc68f1dd7c56f0dad7262816b956138fc53bcc0131
-    #            VERSION 1.77.0
-    #    )
-    #elseif (WIN32)
-    #    CPMAddPackage(
-    #            NAME boost
-    #            URL https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.zip
-    #            SHA265_HASH d2886ceff60c35fc6dc9120e8faa960c1e9535f2d7ce447469eae9836110ea77
-    #            VERSION 1.77.0
-    #    )
-    #endif ()
-
-    set(boost_version 1.88.0)
+    set(boost_version 1.83.0)
     set(boost_components "asio")
 
     ## In order to calculate the dependencies:
@@ -40,11 +23,31 @@ else()
     ##   7. goto step 3 if there's still errors
 
     # todo: add container to the list
-    set(boost_deps "algorithm;align;array;assert;atomic;bind;chrono;concept_check;config;container;container_hash;context;conversion;core;coroutine;date_time;detail;exception;function;function_types;fusion;integer;intrusive;io;iterator;lexical_cast;move;mpl;numeric_conversion;optional;pool;predef;preprocessor;range;ratio;rational;regex;smart_ptr;static_assert;system;thread;throw_exception;tokenizer;tuple;type_index;typeof;type_traits;unordered;utility;winapi;lambda")
+    set(boost_deps "algorithm;align;array;assert;atomic;bind;chrono;concept_check;config;container;container_hash;context;conversion;core;coroutine;date_time;detail;exception;function;function_types;fusion;integer;intrusive;io;iterator;lexical_cast;move;mpl;numeric_conversion;optional;pool;predef;preprocessor;range;ratio;rational;regex;smart_ptr;static_assert;system;thread;throw_exception;tokenizer;tuple;type_index;typeof;type_traits;unordered;utility;winapi;lambda;beast;mp11;describe;functional;variant2")
+
+
+    # Get the download links from here: https://www.boost.org/users/download/
+    # This is not a good way since the archive file is about 125MiB
+    # if (UNIX)
+    #     CPMAddPackage(
+    #             NAME boost
+    #             URL https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.gz
+    #             SHA256_HASH 5347464af5b14ac54bb945dc68f1dd7c56f0dad7262816b956138fc53bcc0131
+    #             VERSION 1.77.0
+    #     )
+    # elseif (WIN32)
+    #     CPMAddPackage(
+    #             NAME boost
+    #             URL https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.zip
+    #             SHA265_HASH d2886ceff60c35fc6dc9120e8faa960c1e9535f2d7ce447469eae9836110ea77
+    #             VERSION 1.77.0
+    #     )
+    # endif ()
 
     # Since Boost's support for CMake is still experimental, it's intentionally disabled by default. To enable it
     # link: https://github.com/boostorg/cmake/issues/6#issuecomment-626353520
     set(BOOST_ENABLE_CMAKE ON)
+    set(BOOST_ENABLE_MPI ON)
 
 
     # Boost CMake support (which is said to not work officially)
@@ -73,12 +76,12 @@ else()
                 NAMESPACE Boost::${component_name}
                 FILE "${CMAKE_CURRENT_BINARY_DIR}/${component_name}InternalConfig.cmake"
         )
-        #        install(
-        #                EXPORT "${component_name}Config"
-        #                FILE "${component_name}Config.cmake"
-        #                NAMESPACE Boost::
-        #                DESTINATION "${CMAKE_INSTALL_DATADIR}/boost"
-        #        )
+        # install(
+        #         EXPORT "${component_name}Config"
+        #         FILE "${component_name}Config.cmake"
+        #         NAMESPACE Boost::
+        #         DESTINATION "${CMAKE_INSTALL_DATADIR}/boost"
+        # )
     endfunction()
 
     # Separating these because they're special
@@ -96,12 +99,13 @@ else()
     # The reason for mp11 being excluded is explained in the issue that I opened here:
     # link: https://github.com/boostorg/mp11/issues/67
     # The issue will be fixed what I hope in boost 78.0.0
-    CPMAddPackage(
-            NAME boost_mp11
-            URL https://github.com/boostorg/mp11/archive/refs/tags/boost-${boost_version}.zip
-            VERSION ${boost_version}
-    )
-    boost_export(boost_mp11)
+    # As of 83.0.0, it's fixed and if you uncomment this, it'll break.
+    # CPMAddPackage(
+    #         NAME boost_mp11
+    #         URL https://github.com/boostorg/mp11/archive/refs/tags/boost-${boost_version}.zip
+    #         VERSION ${boost_version}
+    # )
+    # boost_export(boost_mp11)
 
     include(BoostInstall)
     # include(BoostFetch)
--- webpp/uri/fragment.hpp
+++ webpp/uri/fragment.hpp
@@ -18,6 +18,7 @@ namespace webpp::uri {
 
         if constexpr (ctx_type::is_modifiable) {
             auto&      output = ctx.out.get_fragment_ref();
+            // todo: this is encode, not decode
             bool const is_valid =
               decode_uri_component(ctx.whole(), output, uri::details::FRAGMENT_ENCODE_SET);
             if (!is_valid) {
