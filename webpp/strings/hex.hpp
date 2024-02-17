86bcfee096c26c7a5a5bd2341db503859ff80f5a
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2024-02-17 14:10:39 -0800

    Fixing hex

--- webpp/strings/hex.hpp
+++ webpp/strings/hex.hpp
@@ -279,7 +279,7 @@ namespace webpp::ascii {
         /// 0123456789abcdef
         template <typename CharT = char>
         static constexpr CharT
-          hex_chars[16]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', '\0'};
+          hex_chars[17]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', '\0'};
 
 
         template <typename CharT = char>
