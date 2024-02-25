cd5f977ea4cb5499eccd06711952c391977934e3
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2024-02-24 20:19:19 -0800

    Fuzz test for uri update; #463

--- tests/uri_fuzz.cpp
+++ tests/uri_fuzz.cpp
@@ -15,7 +15,22 @@ void uri_fuzz(stl::string_view data) {
     url.path(data);
     url.queries(data);
     url.fragment(data);
-    // ASSERT_NE(url.size(), 0);
+    auto const res1         = url.as_string();
+    auto const scheme_res   = url.scheme().as_string();
+    auto const username_res = url.username().as_string();
+    auto const password_res = url.password().as_string();
+    auto const hostname_res = url.hostname().as_string();
+    auto const port_res     = url.port().as_string();
+    auto const path_res     = url.path().as_string();
+    auto const queries_res  = url.queries().as_string();
+    auto const fragment_res = url.fragment().as_string();
+
+    if (url.has_value()) {
+        auto const url_size =
+          res1.size() + scheme_res.size() + username_res.size() + password_res.size() + hostname_res.size() +
+          port_res.size() + path_res.size() + queries_res.size() + fragment_res.size();
+        ASSERT_NE(url_size, 0);
+    }
 }
 
 register_fuzz(uri_fuzz);
