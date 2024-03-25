0c81c1acf24edf2c9bc3da8e9d6fbde40b85dfee
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2024-03-25 15:23:44 -0700

    Test added for percent decoding in domains

--- tests/uri_test.cpp
+++ tests/uri_test.cpp
@@ -594,6 +594,21 @@ TYPED_TEST(URITests, LastEmptySegment) {
     }
 }
 
+TYPED_TEST(URITests, PercentDecodingInDomains) {
+    constexpr stl::string_view str = "https://www.ex%21ample.com/";
+
+    auto context = this->template get_context<TypeParam>(str);
+    uri::parse_uri(context);
+    EXPECT_TRUE(uri::is_valid(context.status));
+    EXPECT_EQ(uri::get_value(context.status), uri::uri_status::valid)
+      << to_string(uri::get_value(context.status));
+    if constexpr (TypeParam::is_modifiable || TypeParam::is_segregated) {
+        EXPECT_EQ(context.out.get_hostname(), "www.ex!ample.com");
+    } else {
+        EXPECT_EQ(context.out.get_hostname(), "www.ex%21ample.com");
+    }
+}
+
 TYPED_TEST(URITests, PathDotNormalizedABunchWithNewLines) {
     constexpr stl::string_view str =
       "https://127.0.0.1/.\r.//./one/%2\nE./%\n2e/two/./.\n/\n././%2e\n/%2e/.././three/f\nour/\r%2e%\r2e/"
