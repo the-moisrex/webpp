41bd378cfbedc0176e12cb2a76da266d4eeee7cb
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2025-02-18 00:14:04 -1000

    Fix set_opaque impl; #589

--- webpp/uri/parser/uri_components.hpp
+++ webpp/uri/parser/uri_components.hpp
@@ -1150,18 +1150,8 @@ namespace webpp::uri {
 
     template <ParsingURIContext CtxT>
     constexpr void set_opaque(CtxT& ctx, bool const is_opaque_path) noexcept {
-        using ctx_type = CtxT;
-
-        if constexpr (requires {
-                          ctx_type::component;
-                          ctx.out->set_opaque(true);
-                      })
-        {
-            // works for strings only
-            ctx.out->set_opaque(is_opaque_path);
-        } else if constexpr (requires { istl::deptr(ctx.out).path().set_opaque(true); }) {
-            istl::deptr(ctx).path().set_opaque(is_opaque_path);
-        }
+        auto& path_comp = get_component<components::path>(ctx);
+        path_comp.set_opaque(is_opaque_path);
     }
 
 } // namespace webpp::uri
