254cfc2574b9083c7b38811a4a39f7b739e4c48c
M. Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2025-03-26 14:28:52 -1000

    Added test; #622

--- tests/unicode_test.cpp
+++ tests/unicode_test.cpp
@@ -7048,13 +7048,15 @@ TEST(Unicode, BidiMost) {
     using webpp::unicode::direction;
     using webpp::unicode::direction_of;
 
+    EXPECT_EQ(direction_of(U'\x9'), direction::L);
     EXPECT_EQ(direction_of(U'\x0041'), direction::L);
     EXPECT_EQ(direction_of(U'\x0600'), direction::AN);
     EXPECT_EQ(direction_of(U'\x0610'), direction::NSM);
     EXPECT_EQ(direction_of(U'\x1F4A9'), direction::ON);
 
     for (char32_t cp = 0; cp < 0x10'fffdU + 10; cp += 3) {
-        EXPECT_EQ(direction_of(cp), webpp::unicode::tests::find_direction(cp)) << static_cast<int>(cp);
+        EXPECT_EQ(direction_of(cp), webpp::unicode::tests::find_direction(cp))
+          << "Code Point: " << static_cast<int>(cp);
         // EXPECT_EQ(webpp::unicode::tests::find_direction_map(cp), webpp::unicode::tests::find_direction(cp))
         // << static_cast<int>(cp); EXPECT_EQ(webpp::unicode::tests::find_direction_map(cp), direction_of(cp))
         // << static_cast<int>(cp);
