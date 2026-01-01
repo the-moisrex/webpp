31266c8c8789cdf0dfb91f55972eb1fc1d713837
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2025-12-31 20:05:12 -1000

    Make domain non-owning; #737

--- webpp/uri/domain.hpp
+++ webpp/uri/domain.hpp
@@ -4,29 +4,19 @@
 #define WEBPP_URI_DOMAIN_HPP
 
 #include "../std/string_like.hpp"
-#include "./parser/constants.hpp"
-#include "./parser/iiequals.hpp"
 #include "./parser/parse_domain.hpp"
-
-#include <compare>
 
 namespace webpp::uri {
 
     /**
      * Structured Domain Name
      */
-    template <istl::StringLike StorageT = stl::string_view>
-    struct basic_domain {
-        using string_type      = StorageT;
-        using char_type        = typename string_type::value_type;
-        using storage_type     = StorageT;
-        using string_view_type = istl::string_view_type_of<string_type>;
-
-        static constexpr bool is_modifiable = istl::ModifiableString<string_type>;
-        static constexpr bool is_nothrow    = !is_modifiable;
+    template <typename CharT>
+    struct basic_domain : stl::basic_string_view<CharT> {
+        using char_type        = CharT;
+        using string_view_type = stl::basic_string_view<CharT>;
 
       private:
-        storage_type       storage;
         domain_name_status status = domain_name_status::unparsed;
 
       public:
@@ -36,10 +26,10 @@ namespace webpp::uri {
         basic_domain& operator=(basic_domain&&) noexcept = default;
         ~basic_domain()                                  = default;
 
-        template <typename... Args>
-        explicit constexpr basic_domain(Args&&... args) noexcept(is_nothrow)
-          : storage{stl::forward<Args>(args)...},
-            status{parse_domain_name(storage.begin(), storage.end())} {}
+        explicit constexpr basic_domain(stl::basic_string_view<CharT> input,
+                                        domain_name_status const      inp_status) noexcept
+          : stl::basic_string_view<CharT>{input},
+            status{inp_status} {}
 
         [[nodiscard]] constexpr bool is_valid() const noexcept {
             using enum domain_name_status;
@@ -54,26 +44,15 @@ namespace webpp::uri {
             return is_valid();
         }
 
-        template <istl::StringViewifiable NStrT = string_view_type>
-        [[nodiscard]] constexpr bool operator==(NStrT&& inp_str) const noexcept {
-            return iiequals_afl(storage, stl::forward<NStrT>(inp_str));
-        }
-
-        [[nodiscard]] constexpr bool operator==(basic_domain const& other) const noexcept {
-            return storage == other.storage && status == other.status;
-        }
-
-        [[nodiscard]] constexpr stl::strong_ordering operator<=>(basic_domain const& other) const noexcept = default;
-
         /// Top-Level-Domain
         [[nodiscard]] constexpr string_view_type tld() const noexcept {
             // Domain Separators can be Unicode Code Points as well, but if the transition was not successful, then
             // we'll have an error anyway.
             // https://www.unicode.org/reports/tr46/#Notation
-            if (auto const pos = storage.rfind('.'); pos != string_view_type::npos) {
-                return storage.substr(pos + 1);
+            if (auto const pos = this->rfind('.'); pos != string_view_type::npos) {
+                return this->substr(pos + 1);
             }
-            return storage; // the whole thing is a TLD
+            return *this; // the whole thing is a TLD
         }
     };
 
