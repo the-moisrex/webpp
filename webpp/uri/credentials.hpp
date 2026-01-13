831429cda9622ddd2a160ad74d2e9b6c4ee8e7e2
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2026-01-13 02:59:03 -1000

    Remove basic_username and basic_password; #720
    std::basic_string and string view are enough.

--- webpp/uri/credentials.hpp
+++ webpp/uri/credentials.hpp
@@ -3,257 +3,32 @@
 #ifndef WEBPP_URI_CREDENTIALS_HPP
 #define WEBPP_URI_CREDENTIALS_HPP
 
-#include "../std/string_like.hpp"
+#include "../std/string.hpp"
 #include "../std/string_view.hpp"
-#include "./parser/parse_credentials.hpp"
-#include "./parser/uri_components.hpp"
-#include "uri_status.hpp"
 
 namespace webpp::uri {
 
     /// Serialize username
-    template <istl::StringLike StorageStrT, istl::StringLike StrT>
-    static constexpr void render_username(StorageStrT const& storage, StrT& out)
-      noexcept(!istl::ModifiableString<StrT>) {
+    template <typename CharT, typename AllocT>
+    static constexpr void render_username(stl::basic_string_view<CharT> storage, stl::basic_string<CharT, AllocT>& out)
+      noexcept(false) {
         // https://url.spec.whatwg.org/#url-serializing
         if (storage.empty()) {
             return;
         }
-        istl::append(out, storage);
+        out.append(storage);
     }
 
     /// Serialize password
-    template <istl::StringLike StorageStrT, istl::StringLike StrT>
-    static constexpr void render_password(StorageStrT const& storage, StrT& out)
-      noexcept(!istl::ModifiableString<StrT>) {
+    template <typename CharT>
+    static constexpr void render_password(stl::basic_string_view<CharT> storage, stl::basic_string<CharT>& out)
+      noexcept(false) {
         // https://url.spec.whatwg.org/#url-serializing
         if (storage.empty()) {
             return;
         }
-        istl::append(out, storage);
+        out.append(storage);
     }
-
-    /**
-     * @brief Basic structured username
-     * @tparam StringType Storage Type
-     */
-    template <istl::StringLike StringType = stl::string_view>
-    struct basic_username {
-        using string_type      = StringType;
-        using char_type        = typename string_type::value_type;
-        using iterator         = typename string_type::iterator;
-        using size_type        = typename string_type::size_type;
-        using string_view_type = istl::string_view_type_of<string_type>;
-
-        static constexpr bool is_modifiable   = istl::ModifiableString<string_type>;
-        static constexpr bool is_nothrow      = !is_modifiable;
-        static constexpr bool needs_allocator = requires { typename string_type::allocator_type; };
-
-      private:
-        string_type storage;
-
-      public:
-        template <uri_options Options , typename Iter = iterator>
-        constexpr uri_status_type parse(Iter beg, Iter end) noexcept(is_nothrow) {
-            parsing_uri_component_context<components::username, string_type*, stl::remove_cvref_t<Iter>> ctx{};
-            ctx.beg = beg;
-            ctx.pos = beg;
-            ctx.end = end;
-            ctx.out = stl::addressof(storage);
-            parse_username<Options>(ctx);
-            return ctx.status;
-        }
-
-        template <Allocator AllocT = allocator_type_from_t<string_type>>
-            requires needs_allocator
-        explicit constexpr basic_username(AllocT const& alloc = {}) noexcept : storage{alloc} {}
-
-        template <Allocator AllocT = allocator_type_from_t<string_type>>
-            requires(!needs_allocator)
-        explicit constexpr basic_username([[maybe_unused]] AllocT const& alloc = {}) noexcept {}
-
-        template <istl::StringLike InpStr = string_view_type>
-        explicit constexpr basic_username(InpStr const& inp_str) noexcept(is_nothrow) {
-            parse(inp_str.begin(), inp_str.end());
-        }
-
-        template <istl::StringLike InpStr = string_view_type>
-        constexpr basic_username& operator=(InpStr const& inp_str) noexcept(is_nothrow) {
-            parse(inp_str.begin(), inp_str.end());
-            return *this;
-        }
-
-        /**
-         * @brief check if we have value
-         * @return false if we don't have anything
-         */
-        [[nodiscard]] constexpr bool has_value() const noexcept {
-            return !storage.empty();
-        }
-
-        [[nodiscard]] constexpr size_type size() const noexcept {
-            return storage.size();
-        }
-
-        constexpr void clear() {
-            istl::clear(storage);
-        }
-
-        constexpr void assign(iterator beg, iterator end) noexcept(is_nothrow) {
-            istl::assign(storage, beg, end);
-        }
-
-        [[nodiscard]] constexpr string_view_type view() const noexcept {
-            return string_view_type{storage.data(), storage.size()};
-        }
-
-        template <istl::StringLike NStrT = string_view_type>
-        constexpr void to_string(NStrT& out) const noexcept(!istl::ModifiableString<NStrT>) {
-            render_username(storage, out);
-        }
-
-        template <istl::StringLike NStrT = string_view_type, typename... Args>
-        [[nodiscard]] constexpr NStrT as_string(Args&&... args) const noexcept(!istl::ModifiableString<NStrT>) {
-            NStrT out{stl::forward<Args>(args)...};
-            to_string(out);
-            return out;
-        }
-
-        [[nodiscard]] constexpr decltype(auto) get_allocator() const noexcept {
-            return storage.get_allocator();
-        }
-
-        [[nodiscard]] constexpr auto& storage_ref() noexcept {
-            return storage;
-        }
-
-        [[nodiscard]] constexpr auto const& storage_ref() const noexcept {
-            return storage;
-        }
-
-        /// Equality check
-        /// Attention: this function doesn't parse your input
-        template <istl::StringViewifiable NStrT = string_view_type>
-        [[nodiscard]] constexpr bool operator==(NStrT&& inp_str) const noexcept {
-            return storage == stl::forward<NStrT>(inp_str);
-        }
-
-        [[nodiscard]] constexpr bool operator==(basic_username const& other) const noexcept {
-            return storage == other.storage_ref();
-        }
-    };
-
-    /**
-     * @brief Basic structured password
-     * @tparam StringType Storage type
-     */
-    template <istl::StringLike StringType = stl::string_view>
-    struct basic_password {
-        using string_type      = StringType;
-        using char_type        = typename string_type::value_type;
-        using iterator         = typename string_type::iterator;
-        using size_type        = typename string_type::size_type;
-        using string_view_type = istl::string_view_type_of<string_type>;
-
-        static constexpr bool is_modifiable   = istl::ModifiableString<string_type>;
-        static constexpr bool is_nothrow      = !is_modifiable;
-        static constexpr bool needs_allocator = requires { typename string_type::allocator_type; };
-
-
-      private:
-        string_type storage;
-
-      public:
-        template <uri_options Options , typename Iter = iterator>
-        constexpr uri_status_type parse(Iter beg, Iter end) noexcept(is_nothrow) {
-            parsing_uri_component_context<components::password, string_type*, stl::remove_cvref_t<Iter>> ctx{};
-            ctx.beg = beg;
-            ctx.pos = beg;
-            ctx.end = end;
-            ctx.out = stl::addressof(storage);
-            parse_password<Options>(ctx);
-            return ctx.status;
-        }
-
-        template <Allocator AllocT = allocator_type_from_t<string_type>>
-            requires needs_allocator
-        explicit constexpr basic_password(AllocT const& alloc = {}) noexcept : storage{alloc} {}
-
-        template <Allocator AllocT = allocator_type_from_t<string_type>>
-            requires(!needs_allocator)
-        explicit constexpr basic_password([[maybe_unused]] AllocT const& alloc = {}) noexcept {}
-
-        template <istl::StringLike InpStr = string_view_type>
-        explicit constexpr basic_password(InpStr const& inp_str) noexcept(is_nothrow) {
-            parse(inp_str.begin(), inp_str.end());
-        }
-
-        template <istl::StringLike InpStr = string_view_type>
-        constexpr basic_password& operator=(InpStr const& inp_str) noexcept(is_nothrow) {
-            parse(inp_str.begin(), inp_str.end());
-            return *this;
-        }
-
-        [[nodiscard]] constexpr size_type size() const noexcept {
-            return storage.size();
-        }
-
-        constexpr void clear() {
-            istl::clear(storage);
-        }
-
-        constexpr void assign(iterator beg, iterator end) noexcept(!is_modifiable) {
-            istl::assign(storage, beg, end);
-        }
-
-        template <istl::StringView StrVT = string_view_type>
-        [[nodiscard]] constexpr StrVT view() const noexcept {
-            return StrVT{storage.data(), storage.size()};
-        }
-
-        template <istl::StringLike NStrT = string_view_type>
-        constexpr void to_string(NStrT& out) const noexcept(!istl::ModifiableString<NStrT>) {
-            render_password(storage, out);
-        }
-
-        template <istl::StringLike NStrT = string_view_type, typename... Args>
-        [[nodiscard]] constexpr NStrT as_string(Args&&... args) const noexcept(!istl::ModifiableString<NStrT>) {
-            NStrT out{stl::forward<Args>(args)...};
-            to_string(out);
-            return out;
-        }
-
-        /**
-         * @brief check if we have value
-         * @return false if we don't have anything
-         */
-        [[nodiscard]] constexpr bool has_value() const noexcept {
-            return !storage.empty();
-        }
-
-        [[nodiscard]] constexpr decltype(auto) get_allocator() const noexcept {
-            return storage.get_allocator();
-        }
-
-        [[nodiscard]] constexpr auto& storage_ref() noexcept {
-            return storage;
-        }
-
-        [[nodiscard]] constexpr auto const& storage_ref() const noexcept {
-            return storage;
-        }
-
-        /// Equality check
-        /// Attention: this function doesn't parse your input
-        template <istl::StringViewifiable NStrT = string_view_type>
-        [[nodiscard]] constexpr bool operator==(NStrT&& inp_str) const noexcept {
-            return storage == stl::forward<NStrT>(inp_str);
-        }
-
-        [[nodiscard]] constexpr bool operator==(basic_password const& other) const noexcept {
-            return storage == other.storage_ref();
-        }
-    };
 
 } // namespace webpp::uri
 
