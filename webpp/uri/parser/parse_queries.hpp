e21f7524402034ba4d6a2647b250cd912452bda6
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2026-01-16 02:34:20 -1000

    String-based queries; #720

--- webpp/uri/parser/parse_queries.hpp
+++ webpp/uri/parser/parse_queries.hpp
@@ -13,16 +13,14 @@ namespace webpp::uri {
     namespace details {
 
         template <URIContext CtxT, CtxBufferOf<CtxT> BufT>
-        static constexpr void set_query_name(CtxT& ctx, BufT& buffer, typename CtxT::iterator seg_beg)
-          noexcept(CtxT::is_nothrow) {
+        static constexpr void set_query_name(CtxT& ctx, BufT& buffer) noexcept(CtxT::is_nothrow) {
             if constexpr (CtxNonModifiableBuffer<BufT, CtxT>) {
                 istl::assign(buffer, seg_beg, ctx.pos);
             }
         }
 
         template <URIContext CtxT, CtxBufferOf<CtxT> BufT>
-        static constexpr void set_query_value(CtxT& ctx, BufT& buffer, typename CtxT::iterator& seg_beg)
-          noexcept(CtxT::is_nothrow) {
+        static constexpr void set_query_value(CtxT& ctx, BufT& buffer) noexcept(CtxT::is_nothrow) {
             if constexpr (CtxNonModifiableBuffer<BufT, CtxT>) {
                 istl::assign(buffer, seg_beg, ctx.pos);
                 seg_beg = ctx.pos + 1;
@@ -30,8 +28,7 @@ namespace webpp::uri {
         }
 
         template <URIContext CtxT, CtxBufferOf<CtxT> BufT>
-        static constexpr void
-        append_query_value(CtxT& ctx, BufT& buffer, diff_type_of<CtxT> count, typename CtxT::iterator seg_beg)
+        static constexpr void append_query_value(CtxT& ctx, BufT& buffer, diff_type_of<CtxT> count)
           noexcept(CtxT::is_nothrow) {
             if constexpr (CtxMappedBuffer<BufT, CtxT>) {
                 if constexpr (CtxNonModifiableBuffer<BufT, CtxT>) {
@@ -47,8 +44,7 @@ namespace webpp::uri {
         }
 
         template <URIContext CtxT, CtxBufferOf<CtxT> BufT>
-        static constexpr void
-        next_query([[maybe_unused]] CtxT& ctx, BufT& key_buffer, BufT& value_buffer, typename CtxT::iterator& seg_beg)
+        static constexpr void next_query([[maybe_unused]] CtxT& ctx, BufT& key_buffer, BufT& value_buffer)
           noexcept(CtxT::is_nothrow) {
             if constexpr (CtxT::is_segregated && istl::StringLike<BufT>) {
                 if (!key_buffer.empty() || !value_buffer.empty()) {
@@ -68,6 +64,79 @@ namespace webpp::uri {
         set(ctx.status, ctx.pos == ctx.end ? valid : invalid_queries_character);
     }
 
+    /// Parse into string-based queries components
+    template <uri_options Options, URIContext CtxT>
+        requires(Options.parse_queries && !URIStructuredComponents<typename CtxT::component_type>)
+    static constexpr void parse_queries(CtxT& ctx) noexcept(CtxT::is_nothrow) {
+        // https://url.spec.whatwg.org/#query-state
+
+        using enum uri_status;
+        using details::append_query_value;
+        using details::ascii_bitmap;
+        using details::encode_or_validate_map;
+        using details::set_query_name;
+        using details::set_query_value;
+        using details::skip_separator;
+        using details::validate_percent_encode;
+
+        if (ctx.pos == ctx.end) {
+            set(ctx.status, valid);
+            return;
+        }
+
+        webpp_static_constexpr auto interesting_characters =
+          Options.parse_fragment && !Options.state_override ? ascii_bitmap('%', '#') : ascii_bitmap('%');
+
+        auto const query_percent_encode_set =
+          is_special_scheme(ctx.status) ? details::SPECIAL_QUERIES_ENCODE_SET : details::QUERIES_ENCODE_SET;
+        auto buffer = create_buffer(ctx);
+
+        // find the end of the queries
+        while (!encode_or_validate(ctx, query_percent_encode_set, interesting_characters, buffer)) {
+            switch (*ctx.pos) {
+                case '#':
+                    if constexpr (Options.parse_fragment && !Options.state_override) {
+                        clear_fragment(ctx.out);
+                        ++ctx.pos;
+                        set(ctx.status, valid_fragment);
+                        return;
+                    } else {
+                        stl::unreachable();
+                    }
+                    break;
+                case '%':
+                    if (!validate_percent_encode(ctx, buffer)) {
+                        if constexpr (Options.allow_invalid_characters) {
+                            set_warning(ctx.status, invalid_character);
+                        } else {
+                            set(ctx.status, invalid_queries_character);
+                            return;
+                        }
+                    }
+                    continue;
+                default: {
+                    if constexpr (Options.allow_invalid_characters) {
+                        set_warning(ctx.status, invalid_character);
+                    } else {
+                        set(ctx.status, invalid_queries_character);
+                        return;
+                    }
+                    skip_separator(ctx, buffer);
+                    // invalid characters are not errors
+                    continue;
+                }
+            }
+            break;
+        }
+        end_segment(ctx, buffer);
+        set_queries(ctx.out, buffer);
+
+        if (ctx.pos == ctx.end) {
+            set(ctx.status, valid);
+        }
+    }
+
+    /// Parse into a Structured queries (usually a vector<pair<string, string>>)
     template <uri_options Options, URIContext CtxT>
         requires(Options.parse_queries)
     static constexpr void parse_queries(CtxT& ctx) noexcept(CtxT::is_nothrow) {
@@ -78,7 +147,6 @@ namespace webpp::uri {
         using details::ascii_bitmap;
         using details::encode_or_validate_map;
         using details::next_query;
-        using details::reset_begin;
         using details::set_query_name;
         using details::set_query_value;
         using details::skip_separator;
@@ -102,7 +170,6 @@ namespace webpp::uri {
         auto  key_buffer   = create_buffer(ctx);
         auto  value_buffer = create_buffer(ctx);
         auto& out          = get_storage<components::queries>(ctx);
-        auto  seg_beg      = ctx.pos;
 
         // find the end of the queries
         while (!encode_or_validate_map(
@@ -133,22 +200,22 @@ namespace webpp::uri {
                 case '=':
                     if (!in_value) {
                         if constexpr (CtxT::is_segregated) {
-                            set_query_name(ctx, key_buffer, seg_beg);
+                            set_query_name(ctx, key_buffer);
                         }
                         skip_separator(ctx, out);
                         reset_begin(ctx, seg_beg);
                     } else {
-                        append_query_value(ctx, value_buffer, 1, seg_beg);
+                        append_query_value(ctx, value_buffer, 1);
                     }
                     in_value = true;
                     continue;
                 case '&':
                     if constexpr (CtxT::is_segregated) {
-                        set_query_value(ctx, value_buffer, seg_beg);
+                        set_query_value(ctx, value_buffer);
                         in_value = false;
                     }
                     skip_separator(ctx, out);
-                    next_query(ctx, key_buffer, value_buffer, seg_beg);
+                    next_query(ctx, key_buffer, value_buffer);
                     continue;
                 default: {
                     if constexpr (Options.allow_invalid_characters) {
