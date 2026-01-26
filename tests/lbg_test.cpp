0e93fc7a5e8ce1714c46451cc94b6e280899d192
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2026-01-26 08:32:30 -1000

    Nested test; #720

--- tests/lbg_test.cpp
+++ tests/lbg_test.cpp
@@ -25,3 +25,25 @@ TEST(LBGTest, Basic) {
     EXPECT_EQ(context.req(), "req1");
     EXPECT_EQ(context.res(), "res1");
 }
+
+TEST(LBGTest, Nested) {
+    // layer 1:
+    {
+        context_type ctx1{.request = "req1", .response = "res1"};
+        lbg_scope    scope1{context, ctx1};
+        EXPECT_EQ(context.req(), "req1");
+        EXPECT_EQ(context.res(), "res1");
+
+        // layer 2:
+        {
+            context_type ctx2{.request = "req2", .response = "res2"};
+            lbg_scope    scope2{context, ctx2};
+            EXPECT_EQ(context.req(), "req2");
+            EXPECT_EQ(context.res(), "res2");
+        }
+
+        // back to layer 1:
+        EXPECT_EQ(context.req(), "req1");
+        EXPECT_EQ(context.res(), "res1");
+    }
+}
