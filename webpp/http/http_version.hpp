a3e3a0828466b7b60afffda82e725b9c7acbc2eb
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2026-04-05 20:18:24 -1000

    safety even when not needed

--- webpp/http/http_version.hpp
+++ webpp/http/http_version.hpp
@@ -15,15 +15,22 @@ namespace webpp::http {
     // Wrapper for an HTTP (major,minor) version pair.
     struct [[nodiscard]] version {
       private:
-        static constexpr auto uint_16_bits     = sizeof(stl::uint16_t) * CHAR_BIT;
-        static constexpr auto minor_value_mask = 0xFFFFU;
+        static constexpr auto          uint_16_bits     = sizeof(stl::uint16_t) * CHAR_BIT;
+        static constexpr stl::uint32_t minor_value_mask = 0xFFFFU;
 
         // parse version from string
         [[nodiscard]] static constexpr stl::uint32_t parse_string(stl::string_view const str) noexcept {
-            auto dot   = str.find('.');
+            auto dot = str.find('.');
+            if (dot == stl::string_view::npos) [[unlikely]] {
+                return 0;
+            }
             auto major = to_uint16(str.substr(0, dot));
-            auto minor = to_uint16(str.substr(dot + 1, str.size()));
-            return static_cast<stl::uint32_t>(static_cast<stl::uint32_t>(major << uint_16_bits) | minor);
+            auto minor = to_uint16(str.substr(dot + 1));
+            if (!major.has_value() || !minor.has_value()) {
+                return 0;
+            }
+            return static_cast<stl::uint32_t>(
+              static_cast<stl::uint32_t>((static_cast<stl::uint32_t>(major) << uint_16_bits)) | minor);
         }
 
 
@@ -42,7 +49,8 @@ namespace webpp::http {
 
         // Build from unsigned major/minor pair.
         constexpr version(stl::uint16_t const major, stl::uint16_t const minor) noexcept
-          : value(static_cast<stl::uint32_t>(static_cast<stl::uint32_t>(major << uint_16_bits) | minor)) {}
+          : value(static_cast<stl::uint32_t>(
+              static_cast<stl::uint32_t>((static_cast<stl::uint32_t>(major) << uint_16_bits)) | minor)) {}
 
         // Major version number.
         [[nodiscard]] constexpr stl::uint16_t major_value() const noexcept {
@@ -117,7 +125,10 @@ namespace webpp::http {
         // todo: use <=> operator
 
       private:
-        uint32_t value = 0UL; // Packed as <major>:<minor>
+        // Packed like this:
+        // [ major | minor ]
+        //  16 bits  16 bits
+        stl::uint32_t value = 0U;
     };
 
     /**
@@ -130,7 +141,7 @@ namespace webpp::http {
         template <typename... T>
         explicit constexpr version_list(T&&... versions) noexcept : array_type{stl::forward<T>(versions)...} {}
 
-        [[nodiscard]] constexpr bool include_version(version ver) noexcept {
+        [[nodiscard]] constexpr bool include_version(version ver) const noexcept {
             for (auto const& cur : *this) {
                 if (ver == cur) {
                     return true;
