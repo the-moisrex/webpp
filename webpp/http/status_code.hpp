9961992c7f124dc47863982121adc7ae806e0e3c
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2026-05-25 13:18:14 -1000

    string view

--- webpp/http/status_code.hpp
+++ webpp/http/status_code.hpp
@@ -10,6 +10,7 @@
 #include "../std/std.hpp"
 
 #include <cstdint>
+#include <string_view>
 
 // This file contains the list of HTTP status codes. Taken from IANA HTTP Status Code Registry.
 // https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
@@ -180,87 +181,87 @@ namespace webpp::http {
         network_connect_timeout_error   = 599
     };
 
-    [[nodiscard]] static constexpr auto status_code_reason_phrase(status_code_type const code) noexcept {
+    [[nodiscard]] static constexpr stl::string_view status_code_reason_phrase(status_code_type const code) noexcept {
         switch (code) {
-            case 100: return "Continue";
-            case 101: return "Switching Protocols";
-            case 102:                       // WebDav
-                return "Processing";
-            case 103: return "Early Hints"; // rfc8297.html
+            case 100: return {"Continue"};
+            case 101: return {"Switching Protocols"};
+            case 102:                         // WebDav
+                return {"Processing"};
+            case 103: return {"Early Hints"}; // rfc8297.html
 
-            case 200: return "OK";
-            case 201: return "Created";
-            case 202: return "Accepted";
-            case 203: return "Non-Authoritative Information";
-            case 204: return "No Content";
-            case 205: return "Reset Content";
-            case 206: return "Partial Content";
+            case 200: return {"OK"};
+            case 201: return {"Created"};
+            case 202: return {"Accepted"};
+            case 203: return {"Non-Authoritative Information"};
+            case 204: return {"No Content"};
+            case 205: return {"Reset Content"};
+            case 206: return {"Partial Content"};
             case 207: // WebDav
-                return "Multi-Status";
+                return {"Multi-Status"};
             case 208: // WebDav
-                return "Already Reported";
-            case 226: return "IM Used";
+                return {"Already Reported"};
+            case 226: return {"IM Used"};
 
-            case 300: return "Multiple Choices";
-            case 301: return "Moved Permanently";
+            case 300: return {"Multiple Choices"};
+            case 301: return {"Moved Permanently"};
             case 302: // Previously named "Moved temporarily"
-                return "Found";
-            case 303: return "See Other";
-            case 304: return "Not Modified";
-            case 305: return "Use Proxy";
-            case 306: return "Switch Proxy";
-            case 307: return "Temporary Redirect";
-            case 308: return "Permanent Redirect";
+                return {"Found"};
+            case 303: return {"See Other"};
+            case 304: return {"Not Modified"};
+            case 305: return {"Use Proxy"};
+            case 306: return {"Switch Proxy"};
+            case 307: return {"Temporary Redirect"};
+            case 308: return {"Permanent Redirect"};
 
-            case 400: return "Bad Request";
-            case 401: return "Unauthorized";
-            case 402: return "Payment Required";
-            case 403: return "Forbidden";
+            case 400: return {"Bad Request"};
+            case 401: return {"Unauthorized"};
+            case 402: return {"Payment Required"};
+            case 403: return {"Forbidden"};
             case 404: // Joke Not Found :)
-                return "Not Found";
-            case 405: return "Method Not Allowed";
-            case 406: return "Not Acceptable";
-            case 407: return "Proxy Authentication Required";
-            case 408: return "Request Timeout";
-            case 409: return "Conflict";
-            case 410: return "Gone";
-            case 411: return "Length Required";
-            case 412: return "Precondition Failed";
-            case 413: return "Payload Too Large";
-            case 414: return "URI Too Long";
-            case 415: return "Unsupported Media Type";
-            case 416: return "Range Not Satisfiable";
-            case 417: return "Expectation Failed";
+                return {"Not Found"};
+            case 405: return {"Method Not Allowed"};
+            case 406: return {"Not Acceptable"};
+            case 407: return {"Proxy Authentication Required"};
+            case 408: return {"Request Timeout"};
+            case 409: return {"Conflict"};
+            case 410: return {"Gone"};
+            case 411: return {"Length Required"};
+            case 412: return {"Precondition Failed"};
+            case 413: return {"Payload Too Large"};
+            case 414: return {"URI Too Long"};
+            case 415: return {"Unsupported Media Type"};
+            case 416: return {"Range Not Satisfiable"};
+            case 417: return {"Expectation Failed"};
             case 418: // We should make jokes about this one!
-                return "I'm a teapot";
-            case 421: return "Misdirected Request";
+                return {"I'm a teapot"};
+            case 421: return {"Misdirected Request"};
             case 422: // WebDav
-                return "Unprocessable Entity";
+                return {"Unprocessable Entity"};
             case 423: // WebDav
-                return "Locked";
+                return {"Locked"};
             case 424: // WebDav
-                return "Failed Dependency";
-            case 425: return "Too Early";
-            case 426: return "Upgrade Required";
-            case 428: return "Precondition Required";
-            case 429: return "Too Many Requests";
-            case 431: return "Request Header Fields Too Large";
-            case 451: return "Unavailable For Legal Reasons";
+                return {"Failed Dependency"};
+            case 425: return {"Too Early"};
+            case 426: return {"Upgrade Required"};
+            case 428: return {"Precondition Required"};
+            case 429: return {"Too Many Requests"};
+            case 431: return {"Request Header Fields Too Large"};
+            case 451: return {"Unavailable For Legal Reasons"};
 
-            case 500: return "Internal Server Error";
-            case 501: return "Not Implemented";
-            case 502: return "Bad Gateway";
-            case 503: return "Service Unavailable";
-            case 504: return "Gateway Timeout";
-            case 505: return "HTTP Version Not Supported";
-            case 506: return "Variant Also Negotiates";
+            case 500: return {"Internal Server Error"};
+            case 501: return {"Not Implemented"};
+            case 502: return {"Bad Gateway"};
+            case 503: return {"Service Unavailable"};
+            case 504: return {"Gateway Timeout"};
+            case 505: return {"HTTP Version Not Supported"};
+            case 506: return {"Variant Also Negotiates"};
             case 507: // WebDav
-                return "Insufficient Storage";
+                return {"Insufficient Storage"};
             case 508: // WebDav
-                return "Loop Detected";
-            case 510: return "Not Extended";
+                return {"Loop Detected"};
+            case 510: return {"Not Extended"};
             case 511:
-                return "Network Authentication Required";
+                return {"Network Authentication Required"};
 
                 // Unofficial codes:
                 // case 103:
@@ -299,11 +300,11 @@ namespace webpp::http {
             default:
                 // Some Microsoft clients behave badly if the reason string is left empty.
                 // Thus, return a non-empty string here.
-                return "-";
+                return {"-"};
         }
     }
 
-    [[nodiscard]] static constexpr auto status_code_reason_phrase(status_code code) noexcept {
+    [[nodiscard]] static constexpr stl::string_view status_code_reason_phrase(status_code code) noexcept {
         return status_code_reason_phrase(static_cast<status_code_type>(code));
     }
 
