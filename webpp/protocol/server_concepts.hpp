3fcae879ae37d81c4f08cd9536aa869cc8b87599
The Moisrex <12122474+the-moisrex@users.noreply.github.com>
2026-05-30 11:28:01 -1000

    Server concepts update

--- webpp/protocol/server_concepts.hpp
+++ webpp/protocol/server_concepts.hpp
@@ -1,21 +1,28 @@
 #ifndef WEBPP_PROTOCOL_SERVER_CONCEPTS_HPP
 #define WEBPP_PROTOCOL_SERVER_CONCEPTS_HPP
 
+#include "../io/io_concepts.hpp"
 #include "../ip/endpoint.hpp"
 
 namespace webpp {
 
     template <typename T>
     concept Server = requires(T& server) {
+        requires io::FullIOBackend<typename T::io_backend>;
         requires Endpoint<typename T::endpoint_type>;
 
         requires requires(typename T::endpoint_type endpoint) {
             // start listening on the specified endpoint
             listen(server, endpoint);
-
-            // start the server
-            run(server);
         };
+
+        // Check if ssl supported
+        { is_encrypted(server) } noexcept -> stl::same_as<bool>;
+
+        // start/stop the server
+        start(server);
+        stop(server);
+        { is_running(server) } noexcept -> stl::same_as<bool>;
     };
 
 } // namespace webpp
